711a87f88ab53e9d2583ad41e5198de75811d375
Nils Strelow <nils.strelow@student.hpi.de>
2018-01-04 08:37:20 +0100

    Use PropTypes from prop-types package (#11)

--- frontend/lib/js/form-components/EditableText.js
+++ frontend/lib/js/form-components/EditableText.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React                from 'react';
+import PropTypes            from 'prop-types';
 
 import { SelectableLabel }  from '../selectable-label';
 import EditableTextForm     from './EditableTextForm';
--- frontend/lib/js/query-group-modal/QueryGroupModal.js
+++ frontend/lib/js/query-group-modal/QueryGroupModal.js
@@ -1,6 +1,7 @@
 // @flow
 
-import React, { PropTypes } from 'react';
+import React                from 'react';
+import PropTypes            from 'prop-types';
 import classnames           from 'classnames';
 import T                    from 'i18n-react';
 import { connect }          from 'react-redux';
