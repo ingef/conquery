8cb70da7c64e143401ea93b0c670c14ddd701e3c
Nils Strelow <nils.strelow@student.hpi.de>
2018-01-18 15:20:40 +0100

    Remove function creating config for now

--- frontend/app/webpack.production.config.js
+++ frontend/app/webpack.production.config.js
@@ -5,57 +5,54 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 
 const commonConfig = require('./webpack.common.config.js');
 
-module.exports = env => {
-  const useBundleAnalyzer = env && env.stats;
-  return {
-    ...commonConfig,
-    entry: {
-      main: path.join(__dirname, 'src/js/main.js')
-    },
-    output: {
-      path: path.join(__dirname, '/dist/'),
-      filename: '[name]-[hash].min.js'
-    },
-    plugins: [
-      ...commonConfig.plugins,
-      new ExtractTextPlugin({filename: '[name]-[hash].min.css', allChunks: true}),
-      new webpack.optimize.UglifyJsPlugin({
-        compressor: {
-          warnings: false,
-          screw_ie8: true
-        }
-      }),
-      new BundleAnalyzerPlugin({
-        generateStatsFile: true,
-        analyzerMode: 'disabled' // Set to 'server' to analyze
-      }),
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      })
-    ],
-    module: {
-      ...commonConfig.module,
-      rules: [
-        ...commonConfig.module.rules,
-        {
-          test: /\.sass$/,
-          loader: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              'css-loader',
-              'postcss-loader',
-              'resolve-url-loader',
-              {
-                loader: 'sass-loader',
-                options: {
-                  indentedSyntax: true,
-                  sourceMap: true, // Necessary for resolve-url
-                }
+module.exports = {
+  ...commonConfig,
+  entry: {
+    main: path.join(__dirname, 'src/js/main.js')
+  },
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name]-[hash].min.js'
+  },
+  plugins: [
+    ...commonConfig.plugins,
+    new ExtractTextPlugin({filename: '[name]-[hash].min.css', allChunks: true}),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      }
+    }),
+    new BundleAnalyzerPlugin({
+      generateStatsFile: true,
+      analyzerMode: 'disabled' // Set to 'server' to analyze
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ],
+  module: {
+    ...commonConfig.module,
+    rules: [
+      ...commonConfig.module.rules,
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'postcss-loader',
+            'resolve-url-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                indentedSyntax: true,
+                sourceMap: true, // Necessary for resolve-url
               }
-            ]
-          })
-        }
-      ]
-    }
+            }
+          ]
+        })
+      }
+    ]
   }
 };
