7a7d156202d96613638d7ca9c73787f54175d481
Nils Strelow <nils.strelow@student.hpi.de>
2018-02-01 19:18:14 +0100

    Add version to Header component (#34)

--- frontend/lib/js/header/Header.js
+++ frontend/lib/js/header/Header.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import T     from 'i18n-react';
+import React        from 'react';
+import PropTypes    from 'prop-types';
+import T            from 'i18n-react';
+import { connect }  from "react-redux";
 
 class Header extends React.Component {
   render() {
@@ -10,9 +12,20 @@ class Header extends React.Component {
         />
         <span className="header__spacer" />
         <h1 className="header__headline">{T.translate('headline')}</h1>
+        {this.props.development && <h1 className="header__version">{this.props.version}</h1>}
       </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  version: PropTypes.string,
+  development: PropTypes.bool
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  version: '1.6-RC4',
+  development: true,
+});
+
+export default connect(mapStateToProps)(Header);
