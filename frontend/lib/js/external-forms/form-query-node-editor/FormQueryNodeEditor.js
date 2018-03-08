5c2bd9ccc5384f2b2014ca7751469d60a15a4024
Robert Schmid <r.schmid@outlook.com>
2018-03-08 15:29:47 +0100

    Re-add Form Query Node Editor (#74)
    * Add form components for Query and Concept fields, using FormQueryNodeEditor
    
    * Fix prop type validation, add some padding between form elements
    
    * Don't destroy form state on unmount, style fixes
    
    * Fix code style

---
+++ frontend/lib/js/external-forms/form-query-node-editor/FormQueryNodeEditor.js
@@ -0,0 +1,105 @@
+// @flow
+
+import { createConnectedQueryNodeEditor }  from '../../query-node-editor';
+
+import {
+  selectFormState,
+  selectReduxFormState,
+  selectEditedConceptPosition,
+  selectEditedConcept,
+  selectSuggestions,
+}                                          from '../../external-forms/stateSelectors';
+
+import { toUpperCaseUnderscore }           from '../../common/helpers';
+
+import { createFormSuggestionActions }     from '../form-suggestions/actions';
+
+export type PropsType = {
+  name: string,
+  node: ?Object,
+  showTables: boolean,
+  andIdx: number,
+  orIdx: number,
+  suggestions: ?Object,
+  datasetId: number,
+  onCloseModal: Function,
+  onToggleTable: Function,
+  onSetFilterValue: Function,
+  onSwitchFilterMode: Function,
+  onResetAllFilters: Function
+};
+
+export const createConnectedFormQueryNodeEditor = (formType: string, fieldName: string) => {
+  const mapStateToProps = (state, ownProps) => {
+    const reduxFormState = selectReduxFormState(state);
+    const conceptPosition = selectEditedConceptPosition(
+      reduxFormState,
+      formType,
+      fieldName
+    );
+
+    const node = conceptPosition
+      ? selectEditedConcept(reduxFormState, formType, fieldName, conceptPosition)
+      : null;
+
+    const { andIdx, orIdx } = conceptPosition || {};
+
+    const showTables = node && node.tables && (
+      node.tables.length > 1 ||
+      node.tables.some(table => table.filters && table.filters.length > 0)
+    );
+
+    const formState = selectFormState(state, formType);
+    const suggestions = conceptPosition
+      ? selectSuggestions(formState, fieldName, conceptPosition)
+      : null;
+
+    return {
+      node,
+      andIdx,
+      orIdx,
+      editorState: formState[fieldName],
+      isExcludeTimestampsPossible: false,
+      showTables,
+      suggestions,
+
+      onToggleTimestamps: () => {},
+      onCloseModal: () => ownProps.onCloseModal(andIdx, orIdx),
+      onToggleTable: (...props) => ownProps.onToggleTable(andIdx, orIdx, ...props),
+      onSetFilterValue: (...props) => ownProps.onSetFilterValue(andIdx, orIdx, ...props),
+      onSwitchFilterMode: (...props) => ownProps.onSwitchFilterMode(andIdx, orIdx, ...props),
+      onResetAllFilters: () => ownProps.onResetAllFilters(andIdx, orIdx),
+    };
+  }
+
+  const mapDispatchToProps = (dispatch) => {
+    const {
+      loadFormFilterSuggestions
+    } = createFormSuggestionActions(formType, fieldName);
+
+    return {
+      onLoadFilterSuggestions: (...params) =>
+        dispatch(loadFormFilterSuggestions(formType, fieldName, ...params))
+    };
+  };
+
+  const mergeProps = (stateProps, dispatchProps, ownProps) => ({
+    ...ownProps,
+    ...stateProps,
+    ...dispatchProps,
+    onLoadFilterSuggestions: (datasetId, ...rest) =>
+      dispatchProps.onLoadFilterSuggestions(
+        datasetId,
+        stateProps.andIdx,
+        stateProps.orIdx,
+        ...rest
+      ),
+  });
+
+  return createConnectedQueryNodeEditor(
+    `${formType}_${toUpperCaseUnderscore(fieldName)}`,
+    mapStateToProps,
+    mapDispatchToProps,
+    mergeProps
+  );
+}
