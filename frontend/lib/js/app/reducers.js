64164bf757468094c057cffc4c98f2ce935982c2
Robert Schmid <r.schmid@outlook.com>
2018-06-28 12:12:13 +0200

    Remove external forms (#123)
    * Remove external forms
    
    * Keep react components off the redux state

--- frontend/lib/js/app/reducers.js
+++ frontend/lib/js/app/reducers.js
@@ -23,22 +23,18 @@ import {
 } from '../tooltip';
 
 import {
-  reducer as panes,
   type StateType as PanesStateType
 } from '../pane';
 
-import { reducer as query }                    from '../standard-query-editor';
+import { buildPanesReducer }                   from '../pane/reducer';
 import { reducer as queryGroupModal }          from '../query-group-modal';
 import { reducer as previousQueries }          from '../previous-queries/list';
 import { reducer as previousQueriesSearch }    from '../previous-queries/search';
 import { reducer as previousQueriesFilter }    from '../previous-queries/filter';
 import { reducer as uploadQueryResults }       from '../previous-queries/upload';
 import { reducer as deletePreviousQueryModal } from '../previous-queries/delete-modal';
-import { reducer as timebasedQuery }           from '../timebased-query-editor';
-import { buildExternalFormsReducer }           from '../external-forms';
 import { reducer as uploadConceptListModal }   from '../upload-concept-list-modal';
 
-import { createQueryRunnerReducer }            from '../query-runner';
 import { createQueryNodeEditorReducer }        from '../query-node-editor';
 
 export type StateType = {
@@ -49,25 +45,20 @@ export type StateType = {
   version: VersionStateType,
 };
 
-const buildAppReducer = (availableForms) => combineReducers({
+const buildAppReducer = (tabs) => combineReducers({
   categoryTrees,
-  query,
   uploadConceptListModal,
-  standardQueryRunner: createQueryRunnerReducer('standard'),
-  timebasedQueryRunner: createQueryRunnerReducer('timebased'),
   queryNodeEditor: createQueryNodeEditorReducer('standard'),
   queryGroupModal,
   datasets,
   tooltip,
-  panes,
+  panes: buildPanesReducer(tabs),
   previousQueries,
   previousQueriesSearch,
   previousQueriesFilter,
   uploadQueryResults,
   deletePreviousQueryModal,
-  timebasedQuery,
   version,
-  externalForms: buildExternalFormsReducer(availableForms),
 });
 
 export default buildAppReducer;
