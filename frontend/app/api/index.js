1b38e133cabe20c192438d076ad36bcad0bb906d
Robert Schmid <robert.schmid@bakdata.com>
2018-06-28 15:48:03 +0200

    Add place of birth concept to demo

--- frontend/app/api/index.js
+++ frontend/app/api/index.js
@@ -226,23 +226,23 @@ module.exports = function (app, port) {
   /*
     SEARCH
   */
-  app.post('/api/datasets/:datasetId/concepts/search',
-    function response (req, res) {
-      setTimeout(() => {
-        res.setHeader('Content-Type', 'application/json');
+  app.post('/api/datasets/:datasetId/concepts/search', (req, res) => {
+    setTimeout(() => {
+      res.setHeader('Content-Type', 'application/json');
 
-        const result = [];
-        const awards = require('./concepts/awards');
-        const movieAppearance = require('./concepts/movie_appearances');
+      const result = [];
+      const awards = require('./concepts/awards');
+      const movieAppearance = require('./concepts/movie_appearances');
+      const placeOfBirth = require('./concepts/place_of_birth');
 
-        result.push(...findConcepts(awards, req.body.query))
-        result.push(...findConcepts(movieAppearance, req.body.query))
+      result.push(...findConcepts(awards, req.body.query));
+      result.push(...findConcepts(movieAppearance, req.body.query));
+      result.push(...findConcepts(placeOfBirth, req.body.query));
 
-        // see type SearchResult
-        res.send({ result: result, limit: 20, size: result.length });
-      }, 500);
-    }
-  );
+      // see type SearchResult
+      res.send({ result: result, limit: 20, size: result.length });
+    }, 10);
+  });
 };
 
 const findConcepts = (concepts, query) => {
