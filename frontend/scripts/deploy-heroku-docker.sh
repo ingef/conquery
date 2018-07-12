e37b970cc9f734846ecca4e2d42fd0f7171c57fe
Marcus Baitz <marcus.baitz@bakdata.com>
2018-07-12 13:39:20 +0200

    fix heroku deployment

--- frontend/scripts/deploy-heroku-docker.sh
+++ frontend/scripts/deploy-heroku-docker.sh
@@ -2,5 +2,6 @@
 APP_NAME=$1
 cd ${TRAVIS_BUILD_DIR}/frontend/
 docker login -u _ -p "$HEROKU_TOKEN" registry.heroku.com
+heroku -v
 heroku container:push web -a $APP_NAME
 heroku container:release web -a $APP_NAME
