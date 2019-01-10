7e1c98edf8925dd6d2628c7bd78892568fc26ebd
thoniTUB <12283268+thoniTUB@users.noreply.github.com>
2019-01-10 17:03:16 +0100

    changes default credentials

--- backend/src/main/java/com/bakdata/conquery/models/auth/DefaultTokenExtractor.java
+++ backend/src/main/java/com/bakdata/conquery/models/auth/DefaultTokenExtractor.java
@@ -3,7 +3,7 @@ package com.bakdata.conquery.models.auth;
 import javax.ws.rs.container.ContainerRequestContext;
 
 public class DefaultTokenExtractor implements TokenExtractor {
-	private static final String DEFAULT_CREDENTIALS = "injected_credentials";
+	private static final String DEFAULT_CREDENTIALS = "DEFAULT_CREDENTIALS";
 	@Override
 	public ConqueryToken extract(ContainerRequestContext requestContext) {
 		return new ConqueryToken(DEFAULT_CREDENTIALS);
