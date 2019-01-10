9bef00e4f63074883d46d23353d68079c1daf9dc
thoniTUB <12283268+thoniTUB@users.noreply.github.com>
2019-01-10 09:59:34 +0100

    further defines abilities

--- backend/src/main/java/com/bakdata/conquery/models/auth/permissions/AbilitySets.java
+++ backend/src/main/java/com/bakdata/conquery/models/auth/permissions/AbilitySets.java
@@ -7,8 +7,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum AbilitySets {
-	READER(EnumSet.of(Ability.READ)),
-	CREATOR(EnumSet.of(Ability.READ, Ability.DELETE, Ability.SHARE));
+	QUERY_EXECUTOR(EnumSet.of(
+			Ability.READ,
+			Ability.CANCEL)),
+	QUERY_CREATOR(EnumSet.of(
+			Ability.READ,
+			Ability.DELETE,
+			Ability.SHARE,
+			Ability.TAG,
+			Ability.CANCEL,
+			Ability.LABEL));
 
 	private final EnumSet<Ability> set;
 }
