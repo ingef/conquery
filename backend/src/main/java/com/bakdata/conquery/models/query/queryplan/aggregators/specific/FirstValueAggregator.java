f6893a67b33e6257c781e7948fb832d352c0a762
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2019-01-23 13:00:45 +0100

    implementation of new aggregators for specific sampling.

---
+++ backend/src/main/java/com/bakdata/conquery/models/query/queryplan/aggregators/specific/FirstValueAggregator.java
@@ -0,0 +1,52 @@
+package com.bakdata.conquery.models.query.queryplan.aggregators.specific;
+
+import com.bakdata.conquery.models.datasets.Column;
+import com.bakdata.conquery.models.datasets.Table;
+import com.bakdata.conquery.models.events.Block;
+import com.bakdata.conquery.models.query.QueryContext;
+import com.bakdata.conquery.models.query.queryplan.aggregators.SingleColumnAggregator;
+
+/**
+ * Entity is included when the number of values for a specified column are
+ * within a given range.
+ */
+public class FirstValueAggregator extends SingleColumnAggregator<Object> {
+
+	private Object value;
+	private int date;
+
+	private Column validityDateColumn;
+
+	public FirstValueAggregator(Column column) {
+		super(column);
+	}
+
+	@Override
+	public void nextTable(QueryContext ctx, Table currentTable) {
+		validityDateColumn = ctx.getValidityDateColumn();
+	}
+
+	@Override
+	public void aggregateEvent(Block block, int event) {
+		if (!block.has(event, getColumn())) {
+			return;
+		}
+
+		int next = block.getDate(event, validityDateColumn);
+
+		if (next < date) {
+			date = next;
+			value = block.getAsObject(event, getColumn());
+		}
+	}
+
+	@Override
+	public Object getAggregationResult() {
+		return value;
+	}
+
+	@Override
+	public FirstValueAggregator clone() {
+		return new FirstValueAggregator(getColumn());
+	}
+}
