e27aec430448b4b2f4e9bc783e69cf400ba5e678
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2019-01-30 16:06:06 +0100

    missing package info.

---
+++ backend/src/main/java/com/bakdata/conquery/models/query/temporal/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Logic for Temporal Queries.
+ *
+ * Main class is {@link com.bakdata.conquery.models.query.temporal.TemporalQueryNode}.
+ */
+package com.bakdata.conquery.models.query.temporal;
