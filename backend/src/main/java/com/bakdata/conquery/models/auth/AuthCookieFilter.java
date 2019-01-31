2d13e5ea9c9f5c4e4ee9da95c44138ef3e0c1f93
Marcus Baitz <marcus.baitz@bakdata.com>
2019-01-31 17:50:40 +0100

    Cookie handling for Authentication in Admin-UI

--- backend/src/main/java/com/bakdata/conquery/models/auth/AuthCookieFilter.java
+++ backend/src/main/java/com/bakdata/conquery/models/auth/AuthCookieFilter.java
@@ -7,21 +7,21 @@ import com.bakdata.conquery.io.jersey.AuthCookie;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.Cookie;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.NewCookie;
 import org.eclipse.jetty.http.HttpHeader;
 
 @Provider
 @AuthCookie
 public class AuthCookieFilter implements ContainerResponseFilter {
+    
+    private static final String ACCESS_TOKEN = "access_token";
 
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-        MultivaluedMap<String, String> pathParameters = request.getUriInfo().getQueryParameters();
-        Cookie accessTokenCookie = new NewCookie("access_token", pathParameters.getFirst("access_token"));
+        Cookie cookie = request.getCookies().get(ACCESS_TOKEN);
+        String token = request.getUriInfo().getQueryParameters().getFirst(ACCESS_TOKEN);
         
-        response.getHeaders().add(HttpHeader.COOKIE.toString(), accessTokenCookie);
+        if(cookie == null)        
+            response.getHeaders().add(HttpHeader.SET_COOKIE.toString(), new NewCookie(ACCESS_TOKEN, token));
     }
-
-    
 }
