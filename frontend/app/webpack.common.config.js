1c4c363424a1b44f8f6fdd3924074ceddaea1df1
Marcus Baitz <marcus.baitz@bakdata.com>
2019-02-03 15:42:25 +0100

    removed CopyWebpackPlugin

--- frontend/app/webpack.common.config.js
+++ frontend/app/webpack.common.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = Object.assign({}, ...['en', 'de'].map(lang => ({
   [lang]: {
