8496e5fd63ba53984b47e586957d12c62949c72f
Marcus Baitz <marcus.baitz@bakdata.com>
2019-02-06 09:20:33 +0100

    reorganized imports

--- backend/src/main/java/com/bakdata/conquery/models/query/concept/specific/CQConcept.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/concept/specific/CQConcept.java
@@ -23,7 +23,6 @@ import com.bakdata.conquery.models.query.concept.filter.FilterValue;
 import com.bakdata.conquery.models.query.concept.filter.FilterValue.CQSelectFilter;
 import com.bakdata.conquery.models.query.queryplan.QPNode;
 import com.bakdata.conquery.models.query.queryplan.QueryPlan;
-import com.bakdata.conquery.models.query.queryplan.aggregators.specific.SpecialDateUnion;
 import com.bakdata.conquery.models.query.queryplan.filter.FilterNode;
 import com.bakdata.conquery.models.query.queryplan.specific.AggregatorNode;
 import com.bakdata.conquery.models.query.queryplan.specific.AndNode;
