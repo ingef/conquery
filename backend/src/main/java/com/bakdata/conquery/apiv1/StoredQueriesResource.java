c9b710b7fab7b99563a18f1309a677f33b2ca5f7
Marcus Baitz <marcus.baitz@bakdata.com>
2019-02-11 16:44:37 +0100

    fix merge confict

--- backend/src/main/java/com/bakdata/conquery/apiv1/StoredQueriesResource.java
+++ backend/src/main/java/com/bakdata/conquery/apiv1/StoredQueriesResource.java
@@ -3,7 +3,6 @@ package com.bakdata.conquery.apiv1;
 import com.bakdata.conquery.models.auth.permissions.QueryPermission;
 import com.bakdata.conquery.models.auth.permissions.Ability;
 import com.bakdata.conquery.models.auth.subjects.User;
-import com.bakdata.conquery.models.exceptions.QueryTranslationException;
 import com.bakdata.conquery.models.identifiable.ids.specific.DatasetId;
 import com.bakdata.conquery.models.identifiable.ids.specific.ManagedQueryId;
 import com.bakdata.conquery.models.worker.Namespaces;
@@ -27,21 +26,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import java.util.stream.Collectors;
-import java.util.List;
-
-import com.bakdata.conquery.models.auth.permissions.Ability;
-import com.bakdata.conquery.models.auth.subjects.User;
 import com.bakdata.conquery.models.datasets.Dataset;
 import com.bakdata.conquery.models.exceptions.JSONException;
-import com.bakdata.conquery.models.identifiable.ids.specific.DatasetId;
-import com.bakdata.conquery.models.identifiable.ids.specific.ManagedQueryId;
-import com.bakdata.conquery.models.query.ManagedQuery;
-import com.bakdata.conquery.models.worker.Namespaces;
-import com.bakdata.conquery.util.ResourceUtil;
-import com.fasterxml.jackson.databind.JsonNode;
-
-import io.dropwizard.auth.Auth;
-import io.dropwizard.jersey.PATCH;
 
 @Path("datasets/{" + DATASET + "}/stored-queries")
 @Consumes(AdditionalMediaTypes.JSON)
@@ -49,22 +35,22 @@ import io.dropwizard.jersey.PATCH;
 @PermitAll
 public class StoredQueriesResource {
 
-    private final StoredQueriesProcessor processor;
-    private final ResourceUtil dsUtil;
+	private final StoredQueriesProcessor processor;
+	private final ResourceUtil dsUtil;
 
-    public StoredQueriesResource(Namespaces namespaces) {
-        this.processor = new StoredQueriesProcessor(namespaces);
-        this.dsUtil = new ResourceUtil(namespaces);
-    }
+	public StoredQueriesResource(Namespaces namespaces) {
+		this.processor = new StoredQueriesProcessor(namespaces);
+		this.dsUtil = new ResourceUtil(namespaces);
+	}
 
-    @GET
-    public List<SQStatus> getAllQueries(@Auth User user, @PathParam(DATASET) DatasetId datasetId, @Context HttpServletRequest req) {
-        authorize(user, datasetId, Ability.READ);
+	@GET
+	public List<SQStatus> getAllQueries(@Auth User user, @PathParam(DATASET) DatasetId datasetId, @Context HttpServletRequest req) {
+		authorize(user, datasetId, Ability.READ);
 
-        return processor.getAllQueries(dsUtil.getDataset(datasetId), req).stream()
-                .filter(status -> user.isPermitted(new QueryPermission(user.getId(), Ability.READ.AS_SET, status.getId())))
-                .collect(Collectors.toList());
-    }
+		return processor.getAllQueries(dsUtil.getDataset(datasetId), req).stream()
+				.filter(status -> user.isPermitted(new QueryPermission(user.getId(), Ability.READ.AS_SET, status.getId())))
+				.collect(Collectors.toList());
+	}
 
 	@GET
 	@Path("{" + QUERY + "}")
@@ -73,14 +59,14 @@ public class StoredQueriesResource {
 		authorize(user, datasetId, Ability.READ);
 		authorize(user, queryId, Ability.READ);
 
-        return processor.getQueryWithSource(dsUtil.getDataset(datasetId), queryId);
-    }
+		return processor.getQueryWithSource(dsUtil.getDataset(datasetId), queryId);
+	}
 
 	@PATCH
 	@Path("{" + QUERY + "}")
 	public SQStatus patchQuery(@Auth User user, @PathParam(DATASET) DatasetId datasetId, @PathParam(QUERY) ManagedQueryId queryId, JsonNode patch) throws JSONException {
 		authorize(user, datasetId, Ability.READ);
-		
+
 		Dataset dataset = dsUtil.getDataset(datasetId);
 
 		processor.patchQuery(user, dataset, queryId, patch);
@@ -93,12 +79,7 @@ public class StoredQueriesResource {
 
 		authorize(user, datasetId, Ability.READ);
 		authorize(user, queryId, Ability.DELETE);
-		
-		Dataset dataset = dsUtil.getDataset(datasetId);
-		ManagedQuery query = dsUtil.getManagedQuery(datasetId, queryId);
-		processor.deleteQuery(dataset, query);
-	}
 
-        processor.deleteQuery(dsUtil.getDataset(datasetId), dsUtil.getManagedQuery(datasetId, queryId));
-    }
+		processor.deleteQuery(dsUtil.getDataset(datasetId), dsUtil.getManagedQuery(datasetId, queryId));
+	}
 }
