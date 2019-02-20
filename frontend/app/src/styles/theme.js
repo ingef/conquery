0827c22964c7bc26b8ff98cbb6f6ae93c7524a78
Kai Rollmann <kai.rollmann@gmail.com>
2019-02-20 15:51:40 +0100

    Add react-emotion as a better way to style

---
+++ frontend/app/src/styles/theme.js
@@ -0,0 +1,33 @@
+export default {
+  col: {
+    bg: "#f9f9f9",
+    black: "#444",
+    gray: "#999",
+    grayMediumLight: "#aaa",
+    grayLight: "#dadada",
+    grayVeryLight: "#eee",
+    red: "#b22125",
+    blueGray: "#72757C",
+    blueGrayDark: "#2C8437",
+    blueGrayLight: "#84C68C",
+    green: "#36971C",
+    orange: "#E9711C"
+  },
+  img: {
+    logo: "../images/conquery_logo.svg",
+    logoWidth: "167px",
+    logoBackgroundSize: "167px 50px",
+    spinner: "../images/spinner.png"
+  },
+  font: {
+    huge: "30px",
+    lg: "24px",
+    md: "18px",
+    sm: "14px",
+    xs: "12px",
+    tiny: "11px"
+  },
+  maxWidth: "1024px",
+  borderRadius: "2px",
+  transitionTime: "0.2s"
+};
