3bcb6d99d2950d6d317b82162d86a18200968ff9
thoniTUB <12283268+thoniTUB@users.noreply.github.com>
2019-02-25 13:44:19 +0100

    fixes checking of inherited permissions

--- backend/src/main/java/com/bakdata/conquery/models/auth/subjects/User.java
+++ backend/src/main/java/com/bakdata/conquery/models/auth/subjects/User.java
@@ -102,4 +102,13 @@ public class User extends PermissionOwner<UserId> implements Principal{
 	public String getName() {
 		return email;
 	}
+	
+	@Override
+	public Set<ConqueryPermission> getPermissions(){
+		Set<ConqueryPermission> permissions = super.getPermissions();
+		for (Mandator mandator : roles) {
+			permissions.addAll(mandator.getPermissions());
+		}
+		return permissions;
+	}
 }
