d9de37f03a21471b2378b06a25ddb9f7055b1193
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2019-02-26 14:05:10 +0100

    fixed not requiring wrapped aggs required columns.

--- backend/src/main/java/com/bakdata/conquery/models/query/queryplan/aggregators/DistinctValuesWrapperAggregatorNode.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/queryplan/aggregators/DistinctValuesWrapperAggregatorNode.java
@@ -1,29 +1,30 @@
 package com.bakdata.conquery.models.query.queryplan.aggregators;
 
-import com.bakdata.conquery.models.datasets.Column;
-import com.bakdata.conquery.models.events.Block;
-import lombok.Getter;
-
 import java.util.HashSet;
 import java.util.Set;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import com.bakdata.conquery.models.datasets.Column;
+import com.bakdata.conquery.models.events.Block;
+
+import lombok.Getter;
 
 /**
  * Event is included only, when the value has not been seen before.
  */
 public class DistinctValuesWrapperAggregatorNode<VALUE> extends ColumnAggregator<VALUE> {
 
-
-	private final Aggregator<VALUE> aggregator;
+	private final ColumnAggregator<VALUE> aggregator;
 	private Set<Object> observed = new HashSet<>();
 
 	@Getter
 	private final Column column;
 
-	public DistinctValuesWrapperAggregatorNode(Aggregator<VALUE> aggregator, Column column) {
+	public DistinctValuesWrapperAggregatorNode(ColumnAggregator<VALUE> aggregator, Column column) {
 		this.column = column;
 		this.aggregator = aggregator;
 	}
-
 
 	@Override
 	public VALUE getAggregationResult() {
@@ -32,8 +33,7 @@ public class DistinctValuesWrapperAggregatorNode<VALUE> extends ColumnAggregator
 
 	@Override
 	public Column[] getRequiredColumns() {
-		//TODO wrapped aggregator is ignored.
-		return new Column[]{getColumn()};
+		return ArrayUtils.add(aggregator.getRequiredColumns(), getColumn());
 	}
 
 	@Override
