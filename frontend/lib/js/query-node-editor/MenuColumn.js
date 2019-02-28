9dc04684dd97cff9cdbd5590fdfd7e9e160930d1
Kai Rollmann <kai.rollmann@gmail.com>
2019-02-28 10:01:35 +0100

    Update menu column active style

--- frontend/lib/js/query-node-editor/MenuColumn.js
+++ frontend/lib/js/query-node-editor/MenuColumn.js
@@ -44,10 +44,10 @@ const StyledButton = styled(TransparentHeaderButton)`
   line-height: 21px;
 
   background-color: ${({ theme, active }) =>
-    active ? theme.col.blueGrayLight : "initial"};
+    active ? theme.col.blueGrayVeryLight : "initial"};
   &:hover {
     background-color: ${({ theme, active }) =>
-      active ? theme.col.blueGrayLight : "initial"};
+      active ? theme.col.blueGrayVeryLight : "initial"};
   }
 `;
 
