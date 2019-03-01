e42a6d0ee3fad5300982ee63375b282d5feb8e32
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2019-03-01 14:39:18 +0100

    all values aggregator

--- backend/src/main/java/com/bakdata/conquery/models/query/queryplan/aggregators/specific/value/AllValuesAggregator.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/queryplan/aggregators/specific/value/AllValuesAggregator.java
@@ -7,7 +7,10 @@ import com.bakdata.conquery.models.datasets.Column;
 import com.bakdata.conquery.models.events.Block;
 import com.bakdata.conquery.models.query.queryplan.aggregators.SingleColumnAggregator;
 
-
+/**
+ * Gathers all distinct values for a specific column and returns them as a {@code Set<VALUE>}
+ * @param <VALUE>
+ */
 public class AllValuesAggregator<VALUE> extends SingleColumnAggregator<Set<VALUE>> {
 
 	private final Set<VALUE> entries = new HashSet<>();
