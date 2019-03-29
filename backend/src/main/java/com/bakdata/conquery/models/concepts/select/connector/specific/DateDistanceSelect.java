4733329d03ad1dfd4222af87a49d5e2e7690aba8
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2019-03-29 17:06:32 +0100

    cleanup.

--- backend/src/main/java/com/bakdata/conquery/models/concepts/select/connector/specific/DateDistanceSelect.java
+++ backend/src/main/java/com/bakdata/conquery/models/concepts/select/connector/specific/DateDistanceSelect.java
@@ -2,34 +2,28 @@ package com.bakdata.conquery.models.concepts.select.connector.specific;
 
 import java.time.temporal.ChronoUnit;
 
-import javax.validation.constraints.NotNull;
-
 import com.bakdata.conquery.io.cps.CPSType;
-import com.bakdata.conquery.io.jackson.serializer.NsIdRef;
 import com.bakdata.conquery.models.concepts.select.Select;
 import com.bakdata.conquery.models.concepts.select.connector.SingleColumnSelect;
 import com.bakdata.conquery.models.datasets.Column;
 import com.bakdata.conquery.models.query.queryplan.aggregators.Aggregator;
-import com.bakdata.conquery.models.query.queryplan.aggregators.DistinctValuesWrapperAggregator;
-import com.bakdata.conquery.models.query.queryplan.aggregators.specific.CountAggregator;
 import com.bakdata.conquery.models.query.queryplan.aggregators.specific.DateDistanceAggregatorNode;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @CPSType(id = "DATE_DISTANCE", base = Select.class)
 @Getter @Setter
 public class DateDistanceSelect extends SingleColumnSelect {
 
-	private ChronoUnit unit = ChronoUnit.YEARS;
+	private ChronoUnit timeUnit = ChronoUnit.YEARS;
 
-	public DateDistanceSelect(@NsIdRef Column column) {
+	public DateDistanceSelect(Column column) {
 		super(column);
 	}
 
 	@Override
 	public Aggregator<?> createAggregator() {
-		return new DateDistanceAggregatorNode(getColumn(), getUnit());
+		return new DateDistanceAggregatorNode(getColumn(), getTimeUnit());
 	}
 }
