0225245de2a3440c6fa7843ba63969f81f8cce16
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2019-04-16 13:40:09 +0200

    code style issues

--- backend/src/main/java/com/bakdata/conquery/models/concepts/select/connector/specific/DateDistanceSelect.java
+++ backend/src/main/java/com/bakdata/conquery/models/concepts/select/connector/specific/DateDistanceSelect.java
@@ -3,10 +3,8 @@ package com.bakdata.conquery.models.concepts.select.connector.specific;
 import java.time.temporal.ChronoUnit;
 
 import com.bakdata.conquery.io.cps.CPSType;
-import com.bakdata.conquery.io.jackson.serializer.NsIdRef;
 import com.bakdata.conquery.models.concepts.select.Select;
 import com.bakdata.conquery.models.concepts.select.connector.SingleColumnSelect;
-import com.bakdata.conquery.models.datasets.Column;
 import com.bakdata.conquery.models.query.queryplan.aggregators.Aggregator;
 import com.bakdata.conquery.models.query.queryplan.aggregators.specific.DateDistanceAggregatorNode;
 import com.fasterxml.jackson.annotation.JsonCreator;
