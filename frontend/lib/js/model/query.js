1559176ce4d7d7c7b683096f0d7d27c78e9e2519
Kai Rollmann <kai.rollmann@gmail.com>
2019-04-24 13:13:49 +0200

    Validate external query, don't allow to expand them

--- frontend/lib/js/model/query.js
+++ frontend/lib/js/model/query.js
@@ -3,12 +3,27 @@
 import type { ConceptQueryNodeType } from "../standard-query-editor/types";
 import { TIMEBASED_OPERATOR_TYPES } from "../common/constants/timebasedQueryOperatorTypes";
 
+function isTimebasedQuery(node) {
+  const queryString = JSON.stringify(node.query);
+
+  Object.values(TIMEBASED_OPERATOR_TYPES).some(
+    op => queryString.indexOf(op) === -1
+  );
+}
+
+// A little weird that it's nested so deeply, but well, you can't expand an external query
+function isExternalQuery(node) {
+  return (
+    node.query.type === "CONCEPT_QUERY" &&
+    node.query.root &&
+    node.query.root.children &&
+    node.query.root.children.length > 0 &&
+    node.query.root.children[0].type === "EXTERNAL"
+  );
+}
+
 export function isQueryExpandable(node: ConceptQueryNodeType) {
   if (!node.isPreviousQuery || !node.query) return false;
 
-  const queryString = JSON.stringify(node.query);
-
-  return Object.values(TIMEBASED_OPERATOR_TYPES).every(
-    op => queryString.indexOf(op) === -1
-  );
+  return !isTimebasedQuery(node) && !isExternalQuery(node);
 }
