6a921722c84ced668e9e44a0cc17286f21f76d24
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2019-04-24 11:56:44 +0200

    fixes to cpstype annotations.

--- backend/src/main/java/com/bakdata/conquery/models/messages/namespaces/specific/UpdateMatchingStatsMessage.java
+++ backend/src/main/java/com/bakdata/conquery/models/messages/namespaces/specific/UpdateMatchingStatsMessage.java
@@ -2,11 +2,12 @@ package com.bakdata.conquery.models.messages.namespaces.specific;
 
 import com.bakdata.conquery.io.cps.CPSType;
 import com.bakdata.conquery.models.jobs.UpdateMatchingStats;
+import com.bakdata.conquery.models.messages.namespaces.NamespacedMessage;
 import com.bakdata.conquery.models.messages.namespaces.WorkerMessage;
 import com.bakdata.conquery.models.worker.Worker;
 
-@CPSType(id = "UPDATE_MATCHING_STATS", base = WorkerMessage.class)
-public class UpdateMatchingStatsMessage extends WorkerMessage {
+@CPSType(id = "UPDATE_MATCHING_STATS", base = NamespacedMessage.class)
+public class UpdateMatchingStatsMessage extends WorkerMessage.Slow {
 
 	@Override
 	public void react(Worker context) throws Exception {
