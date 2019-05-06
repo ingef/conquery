254e11181e2ba3f040cc80c7f015bfdba284d870
Kai Rollmann <kai.rollmann@gmail.com>
2019-05-06 16:28:52 +0200

    Use new styles and handleClickOutside for modal

--- frontend/lib/js/modal/Modal.js
+++ frontend/lib/js/modal/Modal.js
@@ -5,16 +5,33 @@ import styled from "@emotion/styled";
 import T from "i18n-react";
 
 import TransparentButton from "../button/TransparentButton";
-
 import EscAble from "../common/components/EscAble";
 
+import ModalContent from "./ModalContent";
+
 type PropsType = {
+  className?: string,
   children?: React.Node,
   headline?: React.Node,
   closeModal: Function,
   doneButton: boolean,
   tabIndex: number
 };
+
+const StyledEscAble = styled(EscAble)`
+  position: fixed;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  width: 100%;
+  max-width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(255, 255, 255, 0.5);
+  cursor: pointer;
+`;
 
 const Headline = styled("h3")`
   margin-top: 20px;
@@ -31,41 +48,26 @@ const StyledTransparentButton = styled(TransparentButton)`
 class Modal extends React.Component {
   props: PropsType;
 
-  closeMaybe = (e: Object) => {
-    if (
-      typeof e.nativeEvent.target.className === "string" && // When SVG Fa Icon is clicked
-      e.nativeEvent.target.className.indexOf("modal__center") !== -1
-    )
-      this.props.closeModal();
-  };
-
   render() {
     return (
-      <EscAble
-        className="modal"
-        onClick={this.closeMaybe}
+      <StyledEscAble
+        className={this.props.className}
         onEscPressed={this.props.closeModal}
       >
-        <div className="modal__center">
-          <div className="modal__content">
-            <>
-              {this.props.doneButton && (
-                <StyledTransparentButton
-                  small
-                  onClick={this.props.closeModal}
-                  tabIndex={this.props.tabIndex || 0}
-                >
-                  {T.translate("common.done")}
-                </StyledTransparentButton>
-              )}
-              {this.props.headline && (
-                <Headline>{this.props.headline}</Headline>
-              )}
-              {this.props.children}
-            </>
-          </div>
-        </div>
-      </EscAble>
+        <ModalContent onClickOutside={this.props.closeModal}>
+          {this.props.doneButton && (
+            <StyledTransparentButton
+              small
+              onClick={this.props.closeModal}
+              tabIndex={this.props.tabIndex || 0}
+            >
+              {T.translate("common.done")}
+            </StyledTransparentButton>
+          )}
+          {this.props.headline && <Headline>{this.props.headline}</Headline>}
+          {this.props.children}
+        </ModalContent>
+      </StyledEscAble>
     );
   }
 }
