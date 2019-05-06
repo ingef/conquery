9b5d0fe7d4af53f4a99a748e3c4fc1f36c5687c2
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2019-05-06 13:02:56 +0200

    fixes contructor

--- backend/src/main/java/com/bakdata/conquery/models/concepts/select/connector/specific/DateDistanceSelect.java
+++ backend/src/main/java/com/bakdata/conquery/models/concepts/select/connector/specific/DateDistanceSelect.java
@@ -3,8 +3,10 @@ package com.bakdata.conquery.models.concepts.select.connector.specific;
 import java.time.temporal.ChronoUnit;
 
 import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.io.jackson.serializer.NsIdRef;
 import com.bakdata.conquery.models.concepts.select.Select;
 import com.bakdata.conquery.models.concepts.select.connector.SingleColumnSelect;
+import com.bakdata.conquery.models.datasets.Column;
 import com.bakdata.conquery.models.query.queryplan.aggregators.Aggregator;
 import com.bakdata.conquery.models.query.queryplan.aggregators.specific.DateDistanceAggregatorNode;
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -17,8 +19,8 @@ import lombok.Setter;
 public class DateDistanceSelect extends SingleColumnSelect {
 
 	@JsonCreator
-	public DateDistanceSelect() {
-		super(null);
+	public DateDistanceSelect(@NsIdRef Column column) {
+		super(column);
 	}
 
 	private ChronoUnit timeUnit = ChronoUnit.YEARS;
