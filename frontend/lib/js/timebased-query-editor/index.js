0bc294929df643f92e3fb070b1c3e6c6517e204d
Kai Rollmann <kai.rollmann@gmail.com>
2019-06-03 11:26:21 +0200

    Refactor right pane state, less nesting, more flat structure

--- frontend/lib/js/timebased-query-editor/index.js
+++ frontend/lib/js/timebased-query-editor/index.js
@@ -1,25 +1,22 @@
+import { combineReducers } from "redux";
+
+import type { TabType } from "../common/types/tabs";
 import { createQueryRunnerReducer } from "../query-runner";
 import { default as timebasedQueryReducer } from "./reducer";
 import TimebasedQueryEditorTab from "./TimebasedQueryEditorTab";
 
 const timebasedQueryRunnerReducer = createQueryRunnerReducer("timebased");
 
-const timebasedQueryEditorTabDescription = {
-  key: "timebasedQueryEditor",
-  label: "rightPane.timebasedQueryEditor"
-};
-
 export * as actions from "./actions";
 
-export default {
-  description: timebasedQueryEditorTabDescription,
-  reducer: (state = timebasedQueryEditorTabDescription, action) => ({
-    ...state,
-    timebasedQuery: timebasedQueryReducer(state.timebasedQuery, action),
-    timebasedQueryRunner: timebasedQueryRunnerReducer(
-      state.timebasedQueryRunner,
-      action
-    )
+const Tab: TabType = {
+  key: "timebasedQueryEditor",
+  label: "rightPane.timebasedQueryEditor",
+  reducer: combineReducers({
+    timebasedQuery: timebasedQueryReducer,
+    timebasedQueryRunner: timebasedQueryRunnerReducer
   }),
   component: TimebasedQueryEditorTab
 };
+
+export default Tab;
