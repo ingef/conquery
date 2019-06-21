f24ed7469d982d6309672a4c75b64b310be7ed82
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2019-06-21 16:08:07 +0200

    fixed exception

--- eva-backend/src/main/java/com/bakdata/eva/forms/queries/RelativeFormQueryPlan.java
+++ eva-backend/src/main/java/com/bakdata/eva/forms/queries/RelativeFormQueryPlan.java
@@ -64,7 +64,7 @@ public class RelativeFormQueryPlan implements QueryPlan {
 		return
 			featurePlan.isOfInterest(bucket)
 			|
-			featurePlan.isOfInterest(bucket);
+			outcomePlan.isOfInterest(bucket);
 	}
 	
 	@Override
