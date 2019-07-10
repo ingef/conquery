d2720182056d220761bfddac4cf4765a7d3e7de4
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2019-07-10 14:02:34 +0200

    testing with trace logging

--- eva-backend/src/test/java/com/bakdata/conquery/integration/EvaIntegrationTests.java
+++ eva-backend/src/test/java/com/bakdata/conquery/integration/EvaIntegrationTests.java
@@ -2,11 +2,11 @@ package com.bakdata.conquery.integration;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DynamicNode;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestFactory;
+import org.slf4j.LoggerFactory;
 
 import com.bakdata.conquery.TestTags;
 import com.bakdata.conquery.models.auth.subjects.Mandator;
@@ -16,6 +16,9 @@ import com.bakdata.conquery.util.support.ConfigOverride;
 import com.bakdata.conquery.util.support.TestAuth;
 import com.bakdata.eva.idmapping.IngefIdMappingConfig;
 import com.bakdata.eva.models.auth.IngefAuthConfig;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 
 public class EvaIntegrationTests extends IntegrationTests implements ConfigOverride {
 
@@ -42,7 +45,10 @@ public class EvaIntegrationTests extends IntegrationTests implements ConfigOverr
 		config.setAuthentication(AUTH_CONFIG);
 		config.setAdditionalFormats(ADDITIONAL_DATE_FORMATS);
 		config.setIdMapping(new IngefIdMappingConfig());
-	}
+		
+		((Logger)LoggerFactory.getILoggerFactory().getLogger("ROOT")).setLevel(Level.TRACE);
+		((Logger)LoggerFactory.getILoggerFactory().getLogger("com.bakdata")).setLevel(Level.TRACE);
+    }
 
 	@Override
 	@TestFactory @Tag(TestTags.INTEGRATION_JSON)
