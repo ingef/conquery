5a01b891d13a5a5d1829fa776a82d27ef1cc9cfc
Fabian Kovacs <Fabian.Kovacs@ingef.de>
2019-10-02 14:57:38 +0200

    removed unused imports.

--- backend/src/main/java/com/bakdata/conquery/models/messages/network/specific/AddSlave.java
+++ backend/src/main/java/com/bakdata/conquery/models/messages/network/specific/AddSlave.java
@@ -2,27 +2,14 @@ package com.bakdata.conquery.models.messages.network.specific;
 
 import com.bakdata.conquery.io.cps.CPSType;
 import com.bakdata.conquery.io.mina.NetworkSession;
-import com.bakdata.conquery.io.xodus.WorkerStorage;
-import com.bakdata.conquery.io.xodus.WorkerStorageImpl;
-import com.bakdata.conquery.models.config.ConqueryConfig;
-import com.bakdata.conquery.models.datasets.Dataset;
 import com.bakdata.conquery.models.messages.network.MasterMessage;
 import com.bakdata.conquery.models.messages.network.NetworkMessage;
 import com.bakdata.conquery.models.messages.network.NetworkMessageContext;
-import com.bakdata.conquery.models.messages.network.NetworkMessageContext.Slave;
-import com.bakdata.conquery.models.messages.network.SlaveMessage;
-import com.bakdata.conquery.models.query.QueryExecutor;
 import com.bakdata.conquery.models.worker.SlaveInformation;
-import com.bakdata.conquery.models.worker.Worker;
-import com.bakdata.conquery.models.worker.WorkerInformation;
 import com.fasterxml.jackson.annotation.JsonCreator;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.File;
-import java.util.UUID;
 
 /**
  * Dummy message that is sent to Master, to authenticate the connection as a Slave connection.
