82b89deaa9e394be381ab13aa90dbe420927f195
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2019-11-15 13:52:12 +0100

    remove double synchronization

--- backend/src/main/java/com/bakdata/conquery/models/auth/entities/Group.java
+++ backend/src/main/java/com/bakdata/conquery/models/auth/entities/Group.java
@@ -19,30 +19,40 @@ import lombok.NonNull;
 import lombok.Setter;
 
 /**
- * A group consists of users and permissions. The permissions held by the group are effective for all
- * users in the group. In Conquery, when a user shares a query it is currently shared with all groups
- * a user is in.
+ * A group consists of users and permissions. The permissions held by the group
+ * are effective for all users in the group. In Conquery, when a user shares a
+ * query it is currently shared with all groups a user is in.
  *
  */
 public class Group extends PermissionOwner<GroupId> {
 
-	@Getter @Setter @NonNull @NotNull @NotEmpty
+	@Getter
+	@Setter
+	@NonNull
+	@NotNull
+	@NotEmpty
 	private String name;
-	@Getter @Setter @NonNull @NotNull @NotEmpty
+	@Getter
+	@Setter
+	@NonNull
+	@NotNull
+	@NotEmpty
 	private String label;
-	
-	@Getter(value = AccessLevel.PUBLIC, onMethod = @__({@Deprecated})) @Setter @MetaIdRefCollection
-	Set<User> members = Collections.synchronizedSet( new HashSet<>());
-	
+
+	@Getter(value = AccessLevel.PUBLIC, onMethod = @__({ @Deprecated }))
+	@Setter
+	@MetaIdRefCollection
+	Set<User> members = Collections.synchronizedSet(new HashSet<>());
+
 	public Group(String name, String label) {
 		this.name = name;
 		this.label = label;
 	}
-	
+
 	@Override
 	protected void updateStorage(MasterMetaStorage storage) throws JSONException {
 		storage.updateGroup(this);
-		
+
 	}
 
 	@Override
@@ -51,26 +61,20 @@ public class Group extends PermissionOwner<GroupId> {
 	}
 
 	public void addMember(MasterMetaStorage storage, User user) throws JSONException {
-		synchronized (members) {
-			members.add(user);
-			updateStorage(storage);
-		}
+		members.add(user);
+		updateStorage(storage);
 	}
-	
+
 	public void removeMember(MasterMetaStorage storage, User user) throws JSONException {
-		synchronized (members) {
-			members.remove(user);
-			updateStorage(storage);
-		}
+		members.remove(user);
+		updateStorage(storage);
 	}
-	
+
 	public boolean containsMember(User user) {
-		synchronized (members) {
-			return members.contains(user);
-		}
+		return members.contains(user);
 	}
-	
-	public Set<User> copyMembers(){
+
+	public Set<User> copyMembers() {
 		return new HashSet<>(members);
 	}
 
