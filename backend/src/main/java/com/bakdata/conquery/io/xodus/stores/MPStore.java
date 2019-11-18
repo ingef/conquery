7d7501a08fe96569686f26ca4be789fb99b51d8d
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2019-11-18 15:38:20 +0100

    small cleanup refactoring

--- backend/src/main/java/com/bakdata/conquery/io/xodus/stores/MPStore.java
+++ backend/src/main/java/com/bakdata/conquery/io/xodus/stores/MPStore.java
@@ -1,12 +1,5 @@
 package com.bakdata.conquery.io.xodus.stores;
 
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Objects;
-import java.util.function.Consumer;
-
-import javax.validation.Validator;
-
 import com.bakdata.conquery.io.jackson.Injectable;
 import com.bakdata.conquery.io.jackson.InternalOnly;
 import com.bakdata.conquery.io.jackson.Jackson;
@@ -17,12 +10,24 @@ import com.bakdata.conquery.models.exceptions.ValidatorHelper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.ObjectWriter;
-
 import jetbrains.exodus.ArrayByteIterable;
 import jetbrains.exodus.ByteIterable;
 import jetbrains.exodus.env.Environment;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.validation.Validator;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+/**
+ * MessagePacked key-value-store from {@link KEY} type values to {@link VALUE} values. ACID consistent, stored on disk using {@link jetbrains.exodus.env.Store} via {@link XodusStore}.
+ *
+ * Values are (de-)serialized using {@link Jackson::BINARY_MAPPER}.
+ * @param <KEY> type of keys
+ * @param <VALUE> type of values.
+ */
 @Slf4j
 public class MPStore <KEY, VALUE> implements Store<KEY, VALUE> {
 
@@ -79,9 +84,9 @@ public class MPStore <KEY, VALUE> implements Store<KEY, VALUE> {
 
 	@Override
 	public void forEach(Consumer<StoreEntry<KEY, VALUE>> consumer) {
-		final StoreEntry<KEY, VALUE> entry = new StoreEntry<>();
 		store.forEach((k,v) -> {
 			try {
+				final StoreEntry<KEY, VALUE> entry = new StoreEntry<>();
 				entry.setKey(readKey(k));
 				entry.setByteSize(v.getLength());
 				try {
--- backend/src/main/java/com/bakdata/conquery/resources/admin/rest/AdminConceptsResource.java
+++ backend/src/main/java/com/bakdata/conquery/resources/admin/rest/AdminConceptsResource.java
@@ -1,20 +1,19 @@
 package com.bakdata.conquery.resources.admin.rest;
 
-import static com.bakdata.conquery.resources.ResourceConstants.CONCEPT_NAME;
-import static com.bakdata.conquery.resources.ResourceConstants.DATASET_NAME;
+import com.bakdata.conquery.io.jersey.ExtraMimeTypes;
+import com.bakdata.conquery.models.jobs.SimpleJob;
+import com.bakdata.conquery.models.messages.namespaces.specific.RemoveConcept;
+import com.bakdata.conquery.resources.hierarchies.HConcepts;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-import com.bakdata.conquery.io.jersey.ExtraMimeTypes;
-import com.bakdata.conquery.models.jobs.SimpleJob;
-import com.bakdata.conquery.models.messages.namespaces.specific.RemoveConcept;
-import com.bakdata.conquery.resources.hierarchies.HConcepts;
-
-import lombok.Getter;
-import lombok.Setter;
+import static com.bakdata.conquery.resources.ResourceConstants.CONCEPT_NAME;
+import static com.bakdata.conquery.resources.ResourceConstants.DATASET_NAME;
 
 @Produces({ExtraMimeTypes.JSON_STRING, ExtraMimeTypes.SMILE_STRING})
 @Consumes({ExtraMimeTypes.JSON_STRING, ExtraMimeTypes.SMILE_STRING})
@@ -27,7 +26,8 @@ public class AdminConceptsResource extends HConcepts {
 	public void removeConcept() {
 		processor
 			.getJobManager()
-			.addSlowJob(new SimpleJob("Adding concept " + conceptId, () -> namespace.getStorage().removeConcept(conceptId)));
+			.addSlowJob(new SimpleJob("Removing concept " + conceptId, () -> namespace.getStorage().removeConcept(conceptId)));
+
 		processor
 			.getJobManager()
 			.addSlowJob(new SimpleJob("sendToAll: remove " + conceptId, () -> namespace.sendToAll(new RemoveConcept(conceptId))));
