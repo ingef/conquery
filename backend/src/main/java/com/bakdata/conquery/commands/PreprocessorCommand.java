37459744c1de9267232b45da10d1c6dbd88814b8
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2019-12-10 16:56:20 +0100

    comments and logging. And allow config from environment variables

--- backend/src/main/java/com/bakdata/conquery/commands/PreprocessorCommand.java
+++ backend/src/main/java/com/bakdata/conquery/commands/PreprocessorCommand.java
@@ -1,8 +1,9 @@
 package com.bakdata.conquery.commands;
 
+import javax.validation.Validator;
+
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -10,8 +11,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
-
-import javax.validation.Validator;
 
 import com.bakdata.conquery.ConqueryConstants;
 import com.bakdata.conquery.models.config.ConqueryConfig;
@@ -24,12 +23,10 @@ import com.bakdata.conquery.util.io.ConqueryMDC;
 import com.bakdata.conquery.util.io.LogUtil;
 import com.bakdata.conquery.util.io.ProgressBar;
 import com.jakewharton.byteunits.BinaryByteUnit;
-
 import io.dropwizard.setup.Environment;
 import lombok.extern.slf4j.Slf4j;
 import net.sourceforge.argparse4j.impl.type.FileArgumentType;
 import net.sourceforge.argparse4j.inf.ArgumentGroup;
-import net.sourceforge.argparse4j.inf.ArgumentType;
 import net.sourceforge.argparse4j.inf.Namespace;
 import net.sourceforge.argparse4j.inf.Subparser;
 
@@ -47,20 +44,28 @@ public class PreprocessorCommand extends ConqueryCommand {
 		this.pool = pool;
 	}
 
+	/**
+	 * Configure CLI based configuration for preprocessor.
+	 */
 	@Override
 	public void configure(Subparser subparser) {
 		super.configure(subparser);
 
-		final ArgumentGroup group = subparser.addArgumentGroup("Preprocessing CLI Config");
+		final ArgumentGroup group = subparser.addArgumentGroup("Preprocessing CLI Config").description("Optional arguments to do a single import step by hand. Overrides json configuration.");
 
 		group.addArgument("--in").required(false)
-			 .type(new FileArgumentType().verifyIsDirectory().verifyCanRead());
+			 .type(new FileArgumentType().verifyIsDirectory().verifyCanRead())
+			 .help("Directory containing the input files (in csv or gzipped csv format).");
 
 		group.addArgument("--out").required(false)
-			 .type(new FileArgumentType().verifyIsDirectory().verifyCanCreate().verifyCanWrite());
+			 .type(new FileArgumentType().verifyIsDirectory().verifyCanCreate().verifyCanWrite())
+			 .help("Directory to write the output cqpp files to.");
+
 
 		group.addArgument("--desc").required(false)
-			 .type(new FileArgumentType().verifyIsDirectory().verifyCanRead());
+			 .type(new FileArgumentType().verifyIsDirectory().verifyCanRead())
+			 .help("Directory containing the import description files (*.import.json).");
+
 	}
 
 	@Override
@@ -72,12 +77,15 @@ public class PreprocessorCommand extends ConqueryCommand {
 		Collection<Preprocessor> jobs = null;
 
 		if (namespace.get("in") != null && namespace.get("desc") != null && namespace.get("out") != null) {
+			log.info("Preprocessing from command line config.");
 			jobs = findPreprocessingJobs(config, environment.getValidator(), new PreprocessingDirectories[]{
 					new PreprocessingDirectories(namespace.get("in"), namespace.get("desc"), namespace.get("out"))
 			});
 		}
-		else
+		else {
+			log.info("Preprocessing from config.json");
 			jobs = findPreprocessingJobs(config, environment.getValidator(), config.getPreprocessor().getDirectories());
+		}
 
 
 		jobs.removeIf(Predicate.not(Preprocessor::requiresProcessing));
@@ -103,20 +111,22 @@ public class PreprocessorCommand extends ConqueryCommand {
 		pool.awaitTermination(24, TimeUnit.HOURS);
 	}
 
-	public static List<Preprocessor> findPreprocessingJobs(ConqueryConfig config, Validator validator, PreprocessingDirectories[] directories1) throws IOException, JSONException {
+	public static List<Preprocessor> findPreprocessingJobs(ConqueryConfig config, Validator validator, PreprocessingDirectories[] directories) throws IOException, JSONException {
 		List<Preprocessor> l = new ArrayList<>();
-		for (PreprocessingDirectories directories : directories1) {
-			File in = directories.getDescriptions().getAbsoluteFile();
+		for (PreprocessingDirectories description : directories) {
+			File in = description.getDescriptions().getAbsoluteFile();
 			for (File descriptionFile : in.listFiles()) {
-				if (descriptionFile.getName().endsWith(ConqueryConstants.EXTENSION_DESCRIPTION)) {
-					InputFile file = InputFile.fromDescriptionFile(descriptionFile, directories);
-					try {
-						ImportDescriptor descr = file.readDescriptor(validator);
-						descr.setInputFile(file);
-						l.add(new Preprocessor(config, descr));
-					} catch (Exception e) {
-						log.error("Failed to process " + LogUtil.printPath(descriptionFile), e);
-					}
+				if (!descriptionFile.getName().endsWith(ConqueryConstants.EXTENSION_DESCRIPTION)) {
+					continue;
+				}
+
+				InputFile file = InputFile.fromDescriptionFile(descriptionFile, description);
+				try {
+					ImportDescriptor descr = file.readDescriptor(validator);
+					descr.setInputFile(file);
+					l.add(new Preprocessor(config, descr));
+				} catch (Exception e) {
+					log.error("Failed to process " + LogUtil.printPath(descriptionFile), e);
 				}
 			}
 		}
