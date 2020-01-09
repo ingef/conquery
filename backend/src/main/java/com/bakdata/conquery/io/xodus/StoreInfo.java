5610fdff6506dfce4b3ff8d9a8a179a9ed9faefe
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-01-09 16:02:16 +0100

    more documentation

--- backend/src/main/java/com/bakdata/conquery/io/xodus/StoreInfo.java
+++ backend/src/main/java/com/bakdata/conquery/io/xodus/StoreInfo.java
@@ -25,6 +25,7 @@ import com.bakdata.conquery.models.events.CBlock;
 import com.bakdata.conquery.models.execution.ManagedExecution;
 import com.bakdata.conquery.models.identifiable.CentralRegistry;
 import com.bakdata.conquery.models.identifiable.Identifiable;
+import com.bakdata.conquery.models.identifiable.ids.IId;
 import com.bakdata.conquery.models.identifiable.ids.specific.BucketId;
 import com.bakdata.conquery.models.identifiable.ids.specific.CBlockId;
 import com.bakdata.conquery.models.identifiable.ids.specific.ConceptId;
@@ -36,6 +37,7 @@ import com.bakdata.conquery.models.identifiable.ids.specific.RoleId;
 import com.bakdata.conquery.models.identifiable.ids.specific.UserId;
 import com.bakdata.conquery.models.identifiable.mapping.PersistentIdMap;
 import com.bakdata.conquery.models.worker.Namespaces;
+import com.bakdata.conquery.models.worker.SingletonNamespaceCollection;
 import com.bakdata.conquery.models.worker.SlaveInformation;
 import com.bakdata.conquery.models.worker.WorkerInformation;
 import jetbrains.exodus.env.Environment;
@@ -44,28 +46,29 @@ import lombok.RequiredArgsConstructor;
 
 /**
  * Enums and helper methods to create stores of a certain kind.
- *
+ * <p>
  * Boolean is used as a placeholder value/class for singleton stores.
  */
-@RequiredArgsConstructor @Getter
+@RequiredArgsConstructor
+@Getter
 public enum StoreInfo implements IStoreInfo {
-	DATASET			(Dataset.class,				Boolean.class),
-	ID_MAPPING		(PersistentIdMap.class,		Boolean.class),
-	NAMESPACES		(Namespaces.class,			Boolean.class),
-	SLAVE			(SlaveInformation.class,		Boolean.class),
-	DICTIONARIES	(Dictionary.class,			DictionaryId.class),
-	IMPORTS			(Import.class,				ImportId.class),
-	CONCEPTS		(Concept.class,				ConceptId.class),
-	BUCKETS			(Bucket.class,				BucketId.class),
-	C_BLOCKS		(CBlock.class,				CBlockId.class),
-	WORKER			(WorkerInformation.class,	Boolean.class),
-	EXECUTIONS		(ManagedExecution.class,		ManagedExecutionId.class),
-	AUTH_ROLE	(Role.class,				RoleId.class),
-	AUTH_USER		(User.class,					UserId.class),
-	AUTH_GROUP		(Group.class,					GroupId.class),
-	STRUCTURE		(StructureNode[].class,		Boolean.class),
+	DATASET(Dataset.class, Boolean.class),
+	ID_MAPPING(PersistentIdMap.class, Boolean.class),
+	NAMESPACES(Namespaces.class, Boolean.class),
+	SLAVE(SlaveInformation.class, Boolean.class),
+	DICTIONARIES(Dictionary.class, DictionaryId.class),
+	IMPORTS(Import.class, ImportId.class),
+	CONCEPTS(Concept.class, ConceptId.class),
+	BUCKETS(Bucket.class, BucketId.class),
+	C_BLOCKS(CBlock.class, CBlockId.class),
+	WORKER(WorkerInformation.class, Boolean.class),
+	EXECUTIONS(ManagedExecution.class, ManagedExecutionId.class),
+	AUTH_ROLE(Role.class, RoleId.class),
+	AUTH_USER(User.class, UserId.class),
+	AUTH_GROUP(Group.class, GroupId.class),
+	STRUCTURE(StructureNode[].class, Boolean.class),
 	;
-	
+
 	private final Class<?> valueType;
 	private final Class<?> keyType;
 
@@ -73,21 +76,23 @@ public enum StoreInfo implements IStoreInfo {
 	 * Store for identifiable values, with injectors. Store is also cached.
 	 */
 	public <T extends Identifiable<?>> IdentifiableStore<T> identifiable(Environment environment, Validator validator, CentralRegistry centralRegistry, Injectable... injectables) {
-		return new IdentifiableStore<>(
-				centralRegistry,
-				cached(environment, validator),
-				injectables
-		);
+
+		final CachedStore<IId<T>, T> store = cached(environment, validator);
+
+		for (Injectable injectable : injectables) {
+			store.inject(injectable);
+		}
+
+		store.inject(centralRegistry);
+
+		return new IdentifiableStore<>(centralRegistry, store);
 	}
 
 	/**
 	 * Store for identifiable values, without injectors. Store is also cached.
 	 */
 	public <T extends Identifiable<?>> IdentifiableStore<T> identifiable(Environment environment, Validator validator, CentralRegistry centralRegistry) {
-		return new IdentifiableStore<>(
-				centralRegistry,
-				cached(environment, validator)
-		);
+		return identifiable(environment, validator, centralRegistry, new SingletonNamespaceCollection(centralRegistry));
 	}
 
 	/**
@@ -98,7 +103,8 @@ public enum StoreInfo implements IStoreInfo {
 				new SerializingStore<>(
 						new XodusStore(environment, this),
 						validator,
-						this)
+						this
+				)
 		);
 	}
 
--- backend/src/main/java/com/bakdata/conquery/io/xodus/stores/WeakCachedStore.java
+++ backend/src/main/java/com/bakdata/conquery/io/xodus/stores/WeakCachedStore.java
@@ -14,6 +14,9 @@ import com.google.common.cache.LoadingCache;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Weakly cached store, using {@link LoadingCache} to maintain values. Is a wrapper around the supplied {@link Store}.
+ */
 @RequiredArgsConstructor @Slf4j
 public class WeakCachedStore<KEY, VALUE> implements Store<KEY, VALUE> {
 
@@ -26,7 +29,7 @@ public class WeakCachedStore<KEY, VALUE> implements Store<KEY, VALUE> {
 		.build(new CacheLoader<KEY, Optional<VALUE>>() {
 			@Override
 			public Optional<VALUE> load(KEY key) throws Exception {
-				log.debug("Needing to load entry "+key+" in "+this);
+				log.trace("Needing to load entry "+key+" in "+this);
 				return Optional.ofNullable(store.get(key));
 			}
 		});
@@ -42,7 +45,7 @@ public class WeakCachedStore<KEY, VALUE> implements Store<KEY, VALUE> {
 		try {
 			Optional<VALUE> old = cache.get(key);
 			if(old.isPresent()) {
-				throw new IllegalStateException("The id "+key+" is alread part of this store");
+				throw new IllegalStateException("The id "+key+" is already part of this store");
 			}
 			cache.put(key, Optional.of(value));
 			store.add(key, value);
