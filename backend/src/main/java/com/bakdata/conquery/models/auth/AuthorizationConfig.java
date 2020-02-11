e5d2942e2ab8b6154882723f2ec620b3224ba890
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-02-11 10:55:22 +0100

    adds doku

--- backend/src/main/java/com/bakdata/conquery/models/auth/AuthorizationConfig.java
+++ backend/src/main/java/com/bakdata/conquery/models/auth/AuthorizationConfig.java
@@ -4,24 +4,27 @@ import java.util.List;
 
 import com.bakdata.conquery.apiv1.auth.ProtoUser;
 import com.bakdata.conquery.io.cps.CPSBase;
-import com.bakdata.conquery.models.config.ConqueryConfig;
+import com.bakdata.conquery.models.auth.permissions.QueryPermission;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 /**
- * TODO rework doku
- * Conquery's authentication and authorization system uses this
- * interface to retrieve necessary objects and other auth related informations
- * for system for different configurations.
- * 
- * A custom authentication mechanism must implement this interface and register
- * a JSON type from this interface, before it is added to the base configuration
- * {@link ConqueryConfig}.
+ * Configurations of this type define the initial users with their permissions
+ * and optional credentials that might be registered by realm that are
+ * {@link UserManageable}.
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.CUSTOM, property = "type")
 @CPSBase
 public interface AuthorizationConfig {
 
+	/**
+	 * A list of initial users that are loaded on start of the application.
+	 * Users that are defined in the configuration will override the users in the storage if they have the same id.
+	 */
 	List<ProtoUser> getInitialUsers();
 
+	/**
+	 * A list of permission scopes/domains that should be used to generate the permission overview as an CSV.
+	 * Usually {@link QueryPermission} are not included, since they add little information to the overview.
+	 */
 	List<String> getOverviewScope();
 }
