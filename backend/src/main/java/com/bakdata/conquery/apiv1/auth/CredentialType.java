3767ab8bc85c14bb9f58f33dc9a05c44d1c2431f
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-02-12 16:51:56 +0100

    fixes serialization of password credential

--- backend/src/main/java/com/bakdata/conquery/apiv1/auth/CredentialType.java
+++ backend/src/main/java/com/bakdata/conquery/apiv1/auth/CredentialType.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 /**
  * A specific type of credential (e.g. a password, hashed password) that can be used by realms
  * that are {@link UserManageable} for user registration.
+ * Make sure that any subtype is serializable (see SerializationTests).
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.CUSTOM, property = "type")
 @CPSBase
--- backend/src/main/java/com/bakdata/conquery/apiv1/auth/PasswordCredential.java
+++ backend/src/main/java/com/bakdata/conquery/apiv1/auth/PasswordCredential.java
@@ -2,7 +2,10 @@ package com.bakdata.conquery.apiv1.auth;
 
 import com.bakdata.conquery.io.cps.CPSType;
 import com.bakdata.conquery.models.auth.basic.LocalAuthenticationRealm;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 /**
  * Container for holding a password. This credential type is used by the
@@ -10,7 +13,9 @@ import lombok.Data;
  */
 @CPSType(base = CredentialType.class, id = "PASSWORD")
 @Data
+@RequiredArgsConstructor(onConstructor = @__({@JsonCreator}))
+@AllArgsConstructor
 public class PasswordCredential implements CredentialType {
 
-	private final char[] password;
+	private char[] password;
 }
