91ebced799092470d29fd9eb1cc5d6d65913de2b
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-03-16 15:39:44 +0100

    Merge remote-tracking branch 'origin/develop' into feature/delete-old-queries
    # Conflicts:
    #	backend/src/main/java/com/bakdata/conquery/models/execution/ManagedExecution.java

--- backend/src/main/java/com/bakdata/conquery/tasks/QueryCleanupTask.java
+++ backend/src/main/java/com/bakdata/conquery/tasks/QueryCleanupTask.java
@@ -7,23 +7,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.bakdata.conquery.io.xodus.MasterMetaStorage;
+import com.bakdata.conquery.models.config.ConqueryConfig;
 import com.bakdata.conquery.models.execution.ManagedExecution;
+import com.bakdata.conquery.models.forms.managed.ManagedForm;
 import com.bakdata.conquery.models.identifiable.ids.specific.ManagedExecutionId;
 import com.bakdata.conquery.models.query.ManagedQuery;
 import com.bakdata.conquery.models.query.concept.specific.CQReusedQuery;
 import com.bakdata.conquery.util.QueryUtils;
 import com.google.common.collect.ImmutableMultimap;
 import io.dropwizard.servlets.tasks.Task;
+import io.dropwizard.util.Duration;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * Dropwizard Task deleting queries that are not used anymore. Defined as:
- * 	- not named
- * 	- older than 30 days
- * 	- is not shared
- * 	- no tags
- * 	- not referenced by other queries
+ * - not named
+ * - older than 30 days
+ * - is not shared
+ * - no tags
+ * - not referenced by other queries
  */
 @Slf4j
 public class QueryCleanupTask extends Task {
@@ -38,37 +41,64 @@ public class QueryCleanupTask extends Task {
 	@Override
 	public void execute(ImmutableMultimap<String, String> parameters, PrintWriter output) throws Exception {
 
+		final Duration oldQueriesTime = ConqueryConfig.getInstance().getQueries().getOldQueriesTime();
+
+
 		// Iterate for as long as no changes are needed (this is because queries can be referenced by other queries)
 		while (true) {
 			final QueryUtils.AllReusedFinder reusedChecker = new QueryUtils.AllReusedFinder();
 			List<ManagedExecutionId> toDelete = new ArrayList<>();
 
-			for (ManagedExecution execution : storage.getAllExecutions()) {
+			for (ManagedExecution<?> execution : storage.getAllExecutions()) {
 
+				// Gather all referenced queries via reused checker.
 				if (execution instanceof ManagedQuery) {
 					((ManagedQuery) execution).getQuery().visit(reusedChecker);
-
-
-					if (((ManagedQuery) execution).isShared())
-						continue;
-
-					if (ArrayUtils.isNotEmpty(((ManagedQuery) execution).getTags()))
-						continue;
+				}
+				else if (execution instanceof ManagedForm) {
+					((ManagedForm) execution).getFlatSubQueries().values()
+											 .forEach(q -> q.getQuery().visit(reusedChecker));
 				}
 
-				if (execution.getLabel() != null)
+				if (execution.isShared()) {
 					continue;
+				}
+				else {
+					log.trace("{} is not shared", execution.getId());
+				}
 
-				if (execution.getCreationTime().toLocalDate().isAfter(LocalDate.now().minusDays(30)))
+
+				if (ArrayUtils.isNotEmpty(execution.getTags())) {
 					continue;
+				}
+				else {
+					log.trace("{} has no tags", execution.getId());
+				}
 
+				if (execution.getLabel() != null) {
+					continue;
+				}
+				else {
+					log.trace("{} has no label", execution.getId());
+				}
+
+				if(execution.getCreationTime().until(LocalDate.now(), oldQueriesTime.getUnit().toChronoUnit()) < oldQueriesTime.getQuantity()) {
+					continue;
+				}
+				else {
+					log.trace("{} is not older than {}.", execution.getId(), oldQueriesTime);
+				}
 
 				toDelete.add(execution.getId());
 			}
 
-			toDelete.removeAll(reusedChecker.getReusedElements().stream().map(CQReusedQuery::getQuery).collect(Collectors.toList()));
+			// remove all queries referenced in reused queries.
+			toDelete.removeAll(
+					reusedChecker.getReusedElements().stream()
+								 .map(CQReusedQuery::getQuery)
+								 .collect(Collectors.toList()));
 
-			if(toDelete.isEmpty()) {
+			if (toDelete.isEmpty()) {
 				break;
 			}
 
