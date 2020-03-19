88a6b7479a456657c2844e75f9a6142b1d3763c9
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-03-19 18:30:58 +0100

    use constants for better documentation

--- backend/src/main/java/com/bakdata/conquery/models/forms/managed/RelativeFormQueryPlan.java
+++ backend/src/main/java/com/bakdata/conquery/models/forms/managed/RelativeFormQueryPlan.java
@@ -24,10 +24,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @Getter @RequiredArgsConstructor
 public class RelativeFormQueryPlan implements QueryPlan {
+
+	private static final int DATE_RANGE_SUB_RESULT = 3;
+	private static final int EVENTDATE = 2;
+	private static final int FEATURE_DATE_RANGE = 3;
+	private static final int OUTCOME_DATE_RANGE = 4;
 
 	private final ConceptQueryPlan query;
 	private final ArrayConceptQueryPlan featurePlan;
@@ -76,10 +80,7 @@ public class RelativeFormQueryPlan implements QueryPlan {
 		int size = featureLength + outcomeLength - 3/*= [RESOLUTION], [INDEX], [EVENTDATE]*/;
 
 		int resultStartIndex = 0;
-		if(contexts.size()>=2
-			&& contexts.get(0).getSubdivisionMode().equals(DateContextMode.COMPLETE)
-			&& contexts.get(1).getSubdivisionMode().equals(DateContextMode.COMPLETE)
-			&& !contexts.get(0).getFeatureGroup().equals(contexts.get(1).getFeatureGroup())) {
+		if(hasCompleteDateContexts(contexts)) {
 			// merge a line for the complete daterange, when two dateContext were generated that don't target the same feature group,
 			// which would be a mistake by the generation
 			// Since the DateContexts are primarily ordered by their coarseness and COMPLETE is the coarsed resolution it must be at the first
@@ -105,6 +106,13 @@ public class RelativeFormQueryPlan implements QueryPlan {
 		return EntityResult.multilineOf(entity.getId(), values);
 	}
 
+	private boolean hasCompleteDateContexts(List<DateContext> contexts) {
+		return contexts.size()>=2
+			&& contexts.get(0).getSubdivisionMode().equals(DateContextMode.COMPLETE)
+			&& contexts.get(1).getSubdivisionMode().equals(DateContextMode.COMPLETE)
+			&& !contexts.get(0).getFeatureGroup().equals(contexts.get(1).getFeatureGroup());
+	}
+
 	private SubResult executeSubQuery(QueryExecutionContext ctx, FeatureGroup featureGroup, Entity entity, List<DateContext> contexts) {
 		List<DateContext> list = new ArrayList<>(contexts);
 		list.removeIf(dctx -> dctx.getFeatureGroup() != featureGroup);
@@ -114,25 +122,25 @@ public class RelativeFormQueryPlan implements QueryPlan {
 		FormQueryPlan sub = new FormQueryPlan(list,subPlan);
 		return new SubResult((MultilineContainedEntityResult) sub.execute(ctx, entity));
 	}
-
+	
 	private void setFeatureValues(Object[] result, Object[] value) {
 		// copy everything up to including index
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i <= EVENTDATE; i++) {
 			result[i] = value[i];
 		}
 		// copy daterange
-		result[3] = value[3];
-		System.arraycopy(value, 4, result, 5, value.length - 4);
+		result[FEATURE_DATE_RANGE] = value[DATE_RANGE_SUB_RESULT];
+		System.arraycopy(value, DATE_RANGE_SUB_RESULT+1, result, FEATURE_DATE_RANGE + 1, value.length - (DATE_RANGE_SUB_RESULT+1));
 	}
 	
 	private void setOutcomeValues(Object[] result, Object[] value, int featureLength) {
 		// copy everything up to including index
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i <= EVENTDATE; i++) {
 			result[i] = value[i];
 		}
 		// copy daterange
-		result[4] = value[3];
-		System.arraycopy(value, 4, result, 1 + featureLength, value.length - 4);
+		result[OUTCOME_DATE_RANGE] = value[DATE_RANGE_SUB_RESULT];
+		System.arraycopy(value, DATE_RANGE_SUB_RESULT+1, result, 1 + featureLength, value.length - (DATE_RANGE_SUB_RESULT+1));
 	}
 
 	@Override
@@ -164,4 +172,5 @@ public class RelativeFormQueryPlan implements QueryPlan {
 			return result.getValues();
 		}
 	}
+
 }
