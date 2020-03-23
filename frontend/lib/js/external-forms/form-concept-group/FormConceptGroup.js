b307e9eab6cb38dede4351b137138ea314a1c830
Kai Rollmann <kai.rollmann@gmail.com>
2020-03-23 13:17:10 +0100

    Make copy from button transparent

--- frontend/lib/js/external-forms/form-concept-group/FormConceptGroup.js
+++ frontend/lib/js/external-forms/form-concept-group/FormConceptGroup.js
@@ -15,7 +15,6 @@ import {
   FORM_CONCEPT_NODE
 } from "../../common/constants/dndTypes";
 import DropzoneWithFileInput from "../../form-components/DropzoneWithFileInput";
-import BasicButton from "../../button/BasicButton";
 import UploadConceptListModal from "../../upload-concept-list-modal/UploadConceptListModal";
 
 import {
@@ -51,6 +50,7 @@ import {
   selectFormConfig,
   useAllowExtendedCopying
 } from "../stateSelectors";
+import TransparentButton from "../../button/TransparentButton";
 
 type PropsType = FieldProps & {
   fieldName: string,
@@ -425,7 +425,7 @@ const DropzoneListItem = styled("div")`
   flex-wrap: wrap;
 `;
 
-const SxBasicButton = styled(BasicButton)`
+const SxTransparentButton = styled(TransparentButton)`
   margin-left: 10px;
 `;
 
@@ -509,9 +509,12 @@ const FormConceptGroup = (props: PropsType) => {
           <>
             {props.label}
             {allowExtendedCopying && (
-              <SxBasicButton tiny onClick={() => setIsCopyModalOpen(true)}>
+              <SxTransparentButton
+                tiny
+                onClick={() => setIsCopyModalOpen(true)}
+              >
                 {T.translate("externalForms.common.concept.copyFrom")}
-              </SxBasicButton>
+              </SxTransparentButton>
             )}
           </>
         }
