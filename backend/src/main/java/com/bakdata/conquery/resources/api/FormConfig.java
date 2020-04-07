0181aea1a28b20cbc2a14846fd0fbcec709267d7
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-04-07 13:06:47 +0200

    WIP form config

---
+++ backend/src/main/java/com/bakdata/conquery/resources/api/FormConfig.java
@@ -0,0 +1,16 @@
+package com.bakdata.conquery.resources.api;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import com.bakdata.conquery.resources.hierarchies.HDatasets;
+
+@Path("form-configs")
+public class FormConfig extends HDatasets{
+	
+	@POST
+	public void postConfig() {
+		
+	}
+
+}
