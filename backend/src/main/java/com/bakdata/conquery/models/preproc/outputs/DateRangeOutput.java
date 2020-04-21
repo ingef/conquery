afa650605a2bf69184aeaa8749d4ddf26d18db18
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-04-21 17:42:09 +0200

    reimplement DateRangeOutput as Epoch dates were a stupid idea to begin with.

---
+++ backend/src/main/java/com/bakdata/conquery/models/preproc/outputs/DateRangeOutput.java
@@ -0,0 +1,65 @@
+package com.bakdata.conquery.models.preproc.outputs;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotNull;
+
+import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.models.common.daterange.CDateRange;
+import com.bakdata.conquery.models.exceptions.ParsingException;
+import com.bakdata.conquery.models.types.MajorTypeId;
+import com.bakdata.conquery.models.types.parser.Parser;
+import com.bakdata.conquery.util.DateFormats;
+import it.unimi.dsi.fastutil.objects.Object2IntArrayMap;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * Parse input columns as {@link CDateRange}. Input values must be {@link com.bakdata.conquery.models.common.CDate} based ints.
+ */
+@Data
+@ToString(of = {"startColumn", "endColumn"})
+@CPSType(id = "DATE_RANGE", base = OutputDescription.class)
+public class DateRangeOutput extends OutputDescription {
+
+	private static final long serialVersionUID = 1L;
+
+	@NotNull
+	private String startColumn, endColumn;
+
+	/**
+	 * Parse null values as open date-range if true.
+	 */
+	public boolean allowOpen = false;
+
+	@Override
+	public Output createForHeaders(Object2IntArrayMap<String> headers) {
+		assertRequiredHeaders(headers, startColumn, endColumn);
+
+		final int startIndex = headers.getInt(startColumn);
+		final int endIndex = headers.getInt(endColumn);
+
+		return new Output() {
+			@Override
+			protected Object parseLine(String[] row, Parser<?> type, long sourceLine) throws ParsingException {
+				if (!allowOpen && (row[startIndex] == null || row[endIndex] == null)) {
+					throw new IllegalArgumentException("Open Ranges are not allowed.");
+				}
+
+				if (row[startIndex] == null && row[endIndex] == null) {
+					return null;
+				}
+
+				LocalDate start = DateFormats.parseToLocalDate(row[startIndex]);
+				LocalDate end = DateFormats.parseToLocalDate(row[endIndex]);
+
+				return CDateRange.of(start, end);
+			}
+		};
+	}
+
+	@Override
+	public MajorTypeId getResultType() {
+		return MajorTypeId.DATE_RANGE;
+	}
+}
