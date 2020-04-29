25e67335c1c25ee0ed20daa09775d7740b00f325
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-04-29 18:00:29 +0200

    SimpleResultInfo cleanup

--- backend/src/main/java/com/bakdata/conquery/models/query/resultinfo/SimpleResultInfo.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/resultinfo/SimpleResultInfo.java
@@ -1,14 +1,12 @@
 package com.bakdata.conquery.models.query.resultinfo;
 
 import com.bakdata.conquery.models.externalservice.ResultType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class SimpleResultInfo extends ResultInfo {
-	public SimpleResultInfo(String name, ResultType type) {
-		this.name = name;
-		this.type = type;
-	}
 
 	private final String name;
 	private final ResultType type;
