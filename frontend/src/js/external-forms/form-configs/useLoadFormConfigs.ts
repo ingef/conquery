ea9379aaf8219a704e4b3f1517eab3cde3cdcab5
Kai Rollmann <kai.rollmann@gmail.com>
2020-05-25 17:28:42 +0200

    Use custom hook to load form configs from anywhere

---
+++ frontend/src/js/external-forms/form-configs/useLoadFormConfigs.ts
@@ -0,0 +1,27 @@
+import { useState, useCallback } from "react";
+import { getFormConfigs } from "../../api/api";
+import { loadFormConfigsSuccess, loadFormConfigsError } from "./actions";
+import { DatasetIdT } from "js/api/types";
+import { useDispatch } from "react-redux";
+
+export const useLoadFormConfigs = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const dispatch = useDispatch();
+
+  const loadFormConfigs = useCallback(async (datasetId: DatasetIdT) => {
+    setLoading(true);
+    try {
+      const data = await getFormConfigs(datasetId);
+
+      dispatch(loadFormConfigsSuccess(data));
+    } catch (e) {
+      dispatch(loadFormConfigsError(e));
+    }
+    setLoading(false);
+  }, [dispatch]);
+
+  return {
+    loading,
+    loadFormConfigs,
+  };
+};
