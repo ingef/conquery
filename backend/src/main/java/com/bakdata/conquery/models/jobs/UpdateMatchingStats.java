eea0609de06df674842e056108d00eda09c645a9
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-06-04 17:23:31 +0200

    Parallelize by Concepts instead of CBlocks for trivial parallelism

--- backend/src/main/java/com/bakdata/conquery/models/jobs/UpdateMatchingStats.java
+++ backend/src/main/java/com/bakdata/conquery/models/jobs/UpdateMatchingStats.java
@@ -2,19 +2,27 @@ package com.bakdata.conquery.models.jobs;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import com.bakdata.conquery.models.concepts.Concept;
 import com.bakdata.conquery.models.concepts.MatchingStats;
 import com.bakdata.conquery.models.concepts.tree.ConceptTreeNode;
 import com.bakdata.conquery.models.concepts.tree.TreeConcept;
+import com.bakdata.conquery.models.config.ConqueryConfig;
 import com.bakdata.conquery.models.datasets.Table;
 import com.bakdata.conquery.models.events.Bucket;
 import com.bakdata.conquery.models.events.CBlock;
 import com.bakdata.conquery.models.identifiable.ids.specific.ConceptElementId;
 import com.bakdata.conquery.models.messages.namespaces.specific.UpdateElementMatchingStats;
 import com.bakdata.conquery.models.worker.Worker;
-
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
@@ -37,51 +45,36 @@ public class UpdateMatchingStats extends Job {
 
 		log.debug("Starting to update Matching stats with {}", worker);
 
-		progressReporter.setMax(worker.getStorage().getAllCBlocks().size());
+		progressReporter.setMax(worker.getStorage().getAllConcepts().size());
+
+
+		ListeningExecutorService pool = MoreExecutors.listeningDecorator(Executors.newWorkStealingPool(ConqueryConfig.getInstance().getStorage().getThreads())); // TODO refactor this.
+
+		List<ListenableFuture<Map<ConceptElementId<?>, MatchingStats.Entry>>> conceptMatches = new ArrayList<>();
+
+		for (Concept<?> concept :worker.getStorage().getAllConcepts()) {
+			conceptMatches.add(pool.submit(() -> calculateConceptMatches(concept)));
+		}
+
+		pool.shutdown();
 
 		Map<ConceptElementId<?>, MatchingStats.Entry> messages = new HashMap<>();
 
-		for (CBlock cBlock : new ArrayList<>(worker.getStorage().getAllCBlocks())) {
+		final ListenableFuture<List<Map<ConceptElementId<?>, MatchingStats.Entry>>> chunks = Futures.allAsList(conceptMatches);
 
-			if(isCancelled()) {
-				progressReporter.done();
+		while (true){
+			if(isCancelled()){
+				pool.shutdownNow();
 				return;
 			}
 
-			Concept<?> concept = worker.getStorage().getConcept(cBlock.getConnector().getConcept());
 			try {
-				Bucket bucket = worker.getStorage().getBucket(cBlock.getBucket());
-				Table table = worker.getStorage().getDataset().getTables().get(bucket.getImp().getTable());
-				
-				for (int event = 0; event < bucket.getNumberOfEvents(); event++) {
-					if (concept instanceof TreeConcept && cBlock.getMostSpecificChildren() != null) {
-						int[] localIds = cBlock.getMostSpecificChildren().get(event);
-						if (localIds != null) {
-							ConceptTreeNode<?> e = ((TreeConcept) concept).getElementByLocalId(localIds);
-	
-							while (e != null) {
-								messages.computeIfAbsent(e.getId(), (x) -> new MatchingStats.Entry())
-									.addEvent(table, bucket, cBlock, event);
-								e = e.getParent();
-							}
-						}
-						else {
-							messages
-								.computeIfAbsent(concept.getId(), (x) -> new MatchingStats.Entry())
-								.addEvent(table, bucket, cBlock, event);
-						}
-					}
-					else {
-						messages.computeIfAbsent(concept.getId(), (x) -> new MatchingStats.Entry())
-							.addEvent(table, bucket, cBlock, event);
-					}
-				}
+				chunks.get(1, TimeUnit.MINUTES)
+					  .forEach(messages::putAll);
+				break;
+			}catch (TimeoutException ignored){
+				// Intentionally left blank
 			}
-			catch (Exception e) {
-				log.error("Failed to collect the matching stats for CBlock " + cBlock.getId(), e);
-			}
-
-			progressReporter.report(1);
 		}
 
 		if (!messages.isEmpty()) {
@@ -89,6 +82,50 @@ public class UpdateMatchingStats extends Job {
 		}
 
 		progressReporter.done();
+	}
+
+	public Map<ConceptElementId<?>, MatchingStats.Entry> calculateConceptMatches(Concept<?> concept) {
+
+		Map<ConceptElementId<?>, MatchingStats.Entry> messages = new HashMap<>();
+
+		for (CBlock cBlock : new ArrayList<>(worker.getStorage().getAllCBlocks())) {
+
+			if(isCancelled())
+				return null;
+
+			if(!cBlock.getConnector().getConcept().equals(concept.getId()))
+				continue;
+
+			try {
+				Bucket bucket = worker.getStorage().getBucket(cBlock.getBucket());
+				Table table = worker.getStorage().getDataset().getTables().get(bucket.getImp().getTable());
+
+				for (int event = 0; event < bucket.getNumberOfEvents(); event++) {
+					if (!(concept instanceof TreeConcept) || cBlock.getMostSpecificChildren() == null || cBlock.getMostSpecificChildren().get(event) == null) {
+						messages.computeIfAbsent(concept.getId(), (x) -> new MatchingStats.Entry())
+								.addEvent(table, bucket, cBlock, event);
+						continue;
+					}
+
+					int[] localIds = cBlock.getMostSpecificChildren().get(event);
+
+					ConceptTreeNode<?> e = ((TreeConcept) concept).getElementByLocalId(localIds);
+
+					while (e != null) {
+						messages.computeIfAbsent(e.getId(), (x) -> new MatchingStats.Entry())
+								.addEvent(table, bucket, cBlock, event);
+						e = e.getParent();
+					}
+				}
+			}
+			catch (Exception e) {
+				log.error("Failed to collect the matching stats for CBlock " + cBlock.getId(), e);
+			}
+		}
+
+		progressReporter.report(1);
+
+		return messages;
 	}
 
 	@Override
