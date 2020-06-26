84a47c91610ca1c882b8a70104ed3379ebd667be
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-06-26 08:56:34 +0200

    documentation and cleanup of RoundRobinQueue.java

--- backend/src/main/java/com/bakdata/conquery/util/RoundRobinQueue.java
+++ backend/src/main/java/com/bakdata/conquery/util/RoundRobinQueue.java
@@ -1,10 +1,11 @@
 package com.bakdata.conquery.util;
 
 import java.util.AbstractQueue;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 
@@ -16,68 +17,123 @@ import org.apache.commons.lang3.ArrayUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Class implementing a queue that is backed by multiple queues at once that are evenly processed, avoiding starvation of jobs when a single producer creates a lot of jobs.
+ * @param <E>
+ */
 public class RoundRobinQueue<E> extends AbstractQueue<E> implements BlockingQueue<E> {
 
-    private static final Queue<?> EMPTY_QUEUE = new ArrayBlockingQueue<>(1);
+	/**
+	 * The backing queues.
+	 *
+	 * @implNote null denotes no queue, and this queue must not be contiguously filled: Deletions just unset the queue so it is no longer processed.
+	 */
+    private final Queue<E>[] queues;
+    private final Object signal = new Object();
 
-	private final Queue<E>[] queues;
+    /**
+     * The index to start polling. This is remembered so we don't have a bias towards lower indices.
+     */
+    private final ThreadLocal<Integer> cycleIndex = ThreadLocal.withInitial(() -> 0);
 
-	private final Object signal = new Object();
-
-	public RoundRobinQueue(int capacity) {
-	    queues = new Queue[capacity];
+    public RoundRobinQueue(int capacity) {
+        super();
+        queues = new Queue[capacity];
     }
 
 	/**
-	 * Clear switch serves as an Atomic comparator to communicate changes of {@code queues} to consuming threads.
-	 * @implNote  In order to guarantee atomic updates we use an integer that can be atomically altered, which is not possible with AtomicBooleans
-	 * @implSpec Iff clearSwitch != localClearSwitch, cycles is invalid.
+	 * @return Maximum number of queues allowed in this queue.
 	 */
-	private final ThreadLocal<Integer> cycleIndex = ThreadLocal.withInitial(() -> 0);
+	public int getCapacity() {
+		return queues.length;
+	}
 
 	/**
-	 * Helper class that notifies on {@code signal} when a new object is added to any queue, awakening all waiting threads.
+	 * @return Number of sub-queues.
 	 */
-	@RequiredArgsConstructor
-	private static class SignallingForwardingQueue<T> extends ForwardingQueue<T> {
+	public int getNQueues() {
+		return (int) Arrays.stream(queues).filter(Objects::nonNull).count();
+	}
 
+
+	/**
+     * Helper class that notifies on {@code signal} when a new object is added to it queue, awakening waiting threads. This effectively implements a semaphore aroud {@code signal} as notify only awakens a single waiting thread.
+     */
+    @RequiredArgsConstructor
+    private static class SignallingForwardingQueue<T> extends ForwardingQueue<T> {
+
+		/**
+		 * The original queue.
+		 */
 		@NonNull
-		private final Queue<T> base;
-		private final Object signal;
+        private final Queue<T> base;
 
+		/**
+		 * The signal object to be notified on.
+		 */
+        private final Object signal;
+
+        @Override
+        protected Queue<T> delegate() {
+            return base;
+        }
+
+		/**
+		 * Try to offer a new element to the queue, if successful notify on signal, awakening a waiting thread.
+		 */
 		@Override
-		protected Queue<T> delegate() {
-			return base;
-		}
+        public boolean offer(T element) {
+            final boolean offer = super.offer(element);
 
-		@Override
-		public boolean offer(T element) {
-			final boolean offer = super.offer(element);
-
-			if(offer){
+            if (offer) {
                 synchronized (signal) {
                     signal.notify();
                 }
-			}
+            }
 
-			return offer;
-		}
+            return offer;
+        }
 
-		@Override
-		public boolean add(T element) {
-			final boolean add = super.add(element);
+		/**
+		 * Try to add a new element to the queue, if successful notify on signal, awakening a waiting thread.
+		 */
+        @Override
+        public boolean add(T element) {
+            final boolean add = super.add(element);
 
-			if(add){
+            if (add) {
                 synchronized (signal) {
                     signal.notify();
                 }
+            }
+
+            return add;
+        }
+
+		/**
+		 * Try to offer multiple elements to the queue, if successful notify all threads waiting on signal.
+		 *
+		 * @implNote this can actually cause some threads to receive nothing, as the collections size might be smaller than the number of waiting threads.
+		 */
+		@Override
+		public boolean addAll(Collection<? extends T> collection) {
+			final boolean addAll = super.addAll(collection);
+
+			if(addAll){
+				//TODO does this cause problems?
+				signal.notifyAll();
 			}
 
-			return add;
+			return addAll;
 		}
 	}
 
-	public Queue<E> createQueue() {
+	/**
+	 * Create a new queue adding it as sub-queue if there is a slot available.
+	 * @throws IllegalStateException when no more slots are available.
+	 * @return
+	 */
+    public Queue<E> createQueue() {
         // TODO: 25.06.2020 FK: add supplier as creation parameter
         final Queue<E> out = new SignallingForwardingQueue<E>(Queues.newConcurrentLinkedQueue(), signal);
 
@@ -85,7 +141,7 @@ public class RoundRobinQueue<E> extends AbstractQueue<E> implements BlockingQueu
             final int free = ArrayUtils.indexOf(queues, null);
 
             if (free == -1) {
-                throw new IllegalStateException(String.format("Queue is full %d", this.queues.length));
+                throw new IllegalStateException(String.format("Queue is full %d", queues.length));
             }
 
             queues[free] = out;
@@ -94,11 +150,16 @@ public class RoundRobinQueue<E> extends AbstractQueue<E> implements BlockingQueu
         return out;
     }
 
+	/**
+	 * Tries to remove the queue if it is inside.
+	 * @param del the Queue to delete.
+	 * @return true if the queue was deleted, false if not.
+	 */
 	public boolean removeQueue(Queue<E> del) {
-	    synchronized (queues) {
+        synchronized (queues) {
             final int index = ArrayUtils.indexOf(queues, del);
 
-            if(index == -1) {
+            if (index == -1) {
                 return false;
             }
 
@@ -106,39 +167,52 @@ public class RoundRobinQueue<E> extends AbstractQueue<E> implements BlockingQueu
         }
 
         return true;
-	}
+    }
 
+	/**
+	 * The number of elements over all queues.
+	 */
 	@Override
-	public int size() {
+    public int size() {
         int sum = 0;
         for (Queue<E> queue : queues) {
-            if(queue != null) {
+            if (queue != null) {
                 sum += queue.size();
             }
         }
         return sum;
-	}
+    }
 
+	/**
+	 * @return true, if all queues are empty.
+	 */
 	@Override
-	public boolean isEmpty() {
+    public boolean isEmpty() {
         for (Queue<E> queue : queues) {
             if (queue != null && !queue.isEmpty()) {
                 return false;
             }
         }
         return true;
-	}
+    }
 
-    @Override
+	/**
+	 * @param o the Object to check.
+	 * @return True, if any queue contains the element.
+	 */
+	@Override
     public boolean contains(Object o) {
         for (Queue<E> queue : queues) {
-            if (queue != null &&  queue.contains(o)) {
+            if (queue != null && queue.contains(o)) {
                 return true;
             }
         }
         return false;
     }
-
+	/**
+	 * @param c the objects to check.
+	 * @return True, if all objects are contained in any of the queues.
+	 */
     @Override
     public boolean containsAll(@NotNull Collection<?> c) {
         for (Object o : c) {
@@ -150,177 +224,197 @@ public class RoundRobinQueue<E> extends AbstractQueue<E> implements BlockingQueu
         return true;
     }
 
+	/**
+	 * Create an iterator looping until the queues are empty.
+	 */
 	@NotNull
-	@Override
-	public Iterator<E> iterator() {
-		return new Iterator<E>() {
-			@Override
-			public boolean hasNext() {
-				return !isEmpty();
-			}
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            @Override
+            public boolean hasNext() {
+                return !isEmpty();
+            }
 
-			@Override
-			public E next() {
-				return poll();
-			}
-		};
-	}
+            @Override
+            public E next() {
+                return poll();
+            }
+        };
+    }
 
-
-	@NotNull
-	@Override
-	public E take() throws InterruptedException {
-		while(true){
-			E out = poll();
-			if(out != null){
-				return out;
-			}
+	/**
+	 * Take one element from the queue, blocking until there is a new object available.
+	 *
+	 * @implNote see {@link RoundRobinQueue::poll()} for implementation details.
+	 */
+    @NotNull
+    @Override
+    public E take() throws InterruptedException {
+        while (true) {
+            E out = poll();
+            if (out != null) {
+                return out;
+            }
 
             synchronized (signal) {
                 signal.wait();
             }
         }
-	}
+    }
 
 
+	/**
+	 * Try taking one element from the queue, blocking until there is a new object available, or the timeout expires.
+	 *
+	 * @implNote see {@link RoundRobinQueue::poll()} for implementation details.
+	 */
 	@Nullable
-	@Override
-	public E poll(long timeout, @NotNull TimeUnit unit) throws InterruptedException {
-		E out = poll();
+    @Override
+    public E poll(long timeout, @NotNull TimeUnit unit) throws InterruptedException {
+        E out = poll();
 
-		if(out != null){
-			return out;
-		}
+        if (out != null) {
+            return out;
+        }
 
-		synchronized (signal) {
+        synchronized (signal) {
             unit.timedWait(signal, timeout);
         }
 
         return poll();
-	}
+    }
 
 
-
-	/**
-	 * Do one cycle, starting from the next Queue polling all sub-queues.
-	 */
-	@Override
-	public E poll() {
+    /**
+     * Find an element in any queue. Start searching in a different queue each time.
+     */
+    @Override
+    public E poll() {
+    	// The last queue we looked at
         final int begin = cycleIndex.get();
 
-        for (int offset = 0; offset < queues.length; offset++) {
+        // Start at the next queue
+        for (int offset = 1; offset < queues.length; offset++) {
             final int index = (begin + offset) % (queues.length - 1);
             Queue<E> curr = queues[index];
 
-            if(curr == null){
+            if (curr == null) {
                 continue;
             }
 
+            // Poll once, if successful update the index of the last polled queue and return the polled element.
             E out = curr.poll();
 
-            if(out != null){
+            if (out != null) {
+                cycleIndex.set(index);
+                return out;
+            }
+        }
+
+        // If no queue had elements, return null.
+        return null;
+    }
+
+
+	/**
+	 * Peek all queues for the next available element.
+	 *
+	 * @apiNote this method is NOT guaranteed to be stable: peeking multiple times will peek different queues.
+	 * @implNote see {@link RoundRobinQueue::poll()} for implementation details.
+	 */
+	@Override
+    public E peek() {
+        final int begin = cycleIndex.get();
+
+        for (int offset = 1; offset < queues.length; offset++) {
+            final int index = (begin + offset) % (queues.length - 1);
+            Queue<E> curr = queues[index];
+
+            if (curr == null) {
+                continue;
+            }
+
+            E out = curr.peek();
+
+            if (out != null) {
                 cycleIndex.set(index);
                 return out;
             }
         }
 
         return null;
-	}
+    }
 
 
+    //=====================================
+    //=       Unsupported Methods         =
+    //=====================================
 
-	@Override
-	public E peek() {
-		final int begin = cycleIndex.get();
+    @Override
+    public int drainTo(@NotNull Collection<? super E> c) {
+        // TODO: 25.06.2020 implement this?
+        return 0;
+    }
 
-        for (int offset = 0; offset < queues.length; offset++) {
-            final int index = (begin + offset) % (queues.length - 1);
-            Queue<E> curr = queues[index];
+    @Override
+    public int drainTo(@NotNull Collection<? super E> c, int maxElements) {
+        // TODO: 25.06.2020 implement this?
+        return 0;
+    }
 
-            if(curr == null){
-                continue;
-            }
+    @NotNull
+    @Override
+    public Object[] toArray() {
+        // TODO: 25.06.2020 implement this?
+        return new Object[0];
+    }
 
-            E out = curr.peek();
+    @NotNull
+    @Override
+    public <T1> T1[] toArray(@NotNull T1[] a) {
+        // TODO: 25.06.2020 implement this?
+        return null;
+    }
 
-            if(out != null){
-                cycleIndex.set(index);
-                return out;
-            }
-        }
-
-		return null;
-	}
-
-
-	//=====================================
-	//=       Unsupported Methods         =
-	//=====================================
-
-	@Override
-	public int drainTo(@NotNull Collection<? super E> c) {
-		// TODO: 25.06.2020 implement this?
-		return 0;
-	}
-
-	@Override
-	public int drainTo(@NotNull Collection<? super E> c, int maxElements) {
-		// TODO: 25.06.2020 implement this?
-		return 0;
-	}
-
-	@NotNull
-	@Override
-	public Object[] toArray() {
-		// TODO: 25.06.2020 implement this?
-		return new Object[0];
-	}
-
-	@NotNull
-	@Override
-	public <T1> T1[] toArray(@NotNull T1[] a) {
-		// TODO: 25.06.2020 implement this?
-		return null;
-	}
-
-	@Override
-	public boolean remove(Object o) {
-		throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
-	}
+    @Override
+    public boolean remove(Object o) {
+        throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
+    }
 
 
-	@Override
-	public boolean removeAll(@NotNull Collection<?> c) {
-		throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
-	}
+    @Override
+    public boolean removeAll(@NotNull Collection<?> c) {
+        throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
+    }
 
-	@Override
-	public boolean retainAll(@NotNull Collection<?> c) {
-		throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
-	}
+    @Override
+    public boolean retainAll(@NotNull Collection<?> c) {
+        throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
+    }
 
-	@Override
-	public boolean offer(E t) {
-		throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
-	}
+    @Override
+    public boolean offer(E t) {
+        throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
+    }
 
-	@Override
-	public void put(@NotNull E e) throws InterruptedException {
-		throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
-	}
+    @Override
+    public void put(@NotNull E e) throws InterruptedException {
+        throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
+    }
 
-	@Override
-	public boolean offer(E e, long timeout, @NotNull TimeUnit unit) throws InterruptedException {
-		throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
-	}
+    @Override
+    public boolean offer(E e, long timeout, @NotNull TimeUnit unit) throws InterruptedException {
+        throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
+    }
 
-	@Override
-	public E remove() {
-		throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
-	}
+    @Override
+    public E remove() {
+        throw new IllegalStateException("Cannot directly mutate RoundRobin Queues");
+    }
 
-	@Override
-	public int remainingCapacity() {
-		return Integer.MAX_VALUE;
-	}
+    @Override
+    public int remainingCapacity() {
+        return Integer.MAX_VALUE;
+    }
 }
