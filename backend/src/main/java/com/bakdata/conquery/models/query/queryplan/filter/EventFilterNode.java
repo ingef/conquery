f2a8c0d508b8c96e20ce07566dd4aa279072e826
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-07-21 16:39:55 +0200

    Remove duplicate EventFilterNode.filterValue as it is not required and cause null values in cloning

--- backend/src/main/java/com/bakdata/conquery/models/query/queryplan/filter/EventFilterNode.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/queryplan/filter/EventFilterNode.java
@@ -1,18 +1,14 @@
 package com.bakdata.conquery.models.query.queryplan.filter;
 
 import com.bakdata.conquery.models.events.Bucket;
-import lombok.Getter;
-import lombok.Setter;
 
 public abstract class EventFilterNode<FILTER_VALUE> extends FilterNode<FILTER_VALUE> {
 
 	private boolean hit = false;
 
-	@Setter @Getter
-	protected FILTER_VALUE filterValue;
 
-	public EventFilterNode(FILTER_VALUE filter_value) {
-		super(filter_value);
+	public EventFilterNode(FILTER_VALUE filterValue) {
+		super(filterValue);
 	}
 
 	public abstract boolean checkEvent(Bucket bucket, int event);
