3355a9d9a0332a47ffecbfc5b341085a1192ab4e
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-07-22 09:46:56 +0200

    adds flag to signal if logout button should be hidden

--- backend/src/main/java/com/bakdata/conquery/models/auth/develop/DefaultInitialUserRealm.java
+++ backend/src/main/java/com/bakdata/conquery/models/auth/develop/DefaultInitialUserRealm.java
@@ -66,7 +66,7 @@ public class DefaultInitialUserRealm extends ConqueryAuthenticationRealm {
 			return null;
 		}
 		DevelopmentToken devToken = (DevelopmentToken) token;
-		return new ConqueryAuthenticationInfo(devToken.getPrincipal(), devToken.getCredentials(), this);
+		return new ConqueryAuthenticationInfo(devToken.getPrincipal(), devToken.getCredentials(), this, false);
 	}
 
 	/**
