11b175404bd51f0bc4182e6e088579f93e3a89ab
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-07-22 11:59:04 +0200

    add missing changes from previous commit

--- backend/src/main/java/com/bakdata/conquery/models/query/queryplan/aggregators/specific/ExistsAggregator.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/queryplan/aggregators/specific/ExistsAggregator.java
@@ -1,9 +1,5 @@
 package com.bakdata.conquery.models.query.queryplan.aggregators.specific;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import com.bakdata.conquery.models.events.Bucket;
@@ -11,8 +7,9 @@ import com.bakdata.conquery.models.externalservice.ResultType;
 import com.bakdata.conquery.models.identifiable.ids.specific.TableId;
 import com.bakdata.conquery.models.query.queryplan.aggregators.Aggregator;
 import com.bakdata.conquery.models.query.queryplan.clone.CloneContext;
-import com.bakdata.conquery.models.query.queryplan.filter.FilterNode;
+import com.bakdata.conquery.models.query.queryplan.specific.FiltersNode;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 /**
  * Helper Aggregator, returning if it was used at least once.
@@ -21,14 +18,15 @@ import lombok.RequiredArgsConstructor;
 public class ExistsAggregator implements Aggregator<Boolean> {
 
 	private final Set<TableId> requiredTables;
-	private final Set<FilterNode<?>> filters = new HashSet<>();
+	@Setter
+	private FiltersNode filters;
 
 	@Override
 	public void acceptEvent(Bucket bucket, int event) {  }
 
 	@Override
 	public Boolean getAggregationResult() {
-		return filters.stream().allMatch(FilterNode::isContained);
+		return filters.isContained();
 	}
 	
 	@Override
@@ -38,14 +36,7 @@ public class ExistsAggregator implements Aggregator<Boolean> {
 
 	@Override
 	public ExistsAggregator doClone(CloneContext ctx) {
-		final List<FilterNode<?>> clonedNodes = new ArrayList<>(filters);
-		clonedNodes.replaceAll(ctx::clone);
-
-		final ExistsAggregator aggregator = new ExistsAggregator(requiredTables);
-
-		aggregator.addFilters(clonedNodes);
-
-		return aggregator;
+		return new ExistsAggregator(requiredTables);
 	}
 	
 	@Override
@@ -56,9 +47,5 @@ public class ExistsAggregator implements Aggregator<Boolean> {
 	@Override
 	public String toString(){
 		return getClass().getSimpleName();
-	}
-
-	public void addFilters(Collection<? extends FilterNode<?>> filters) {
-		this.filters.addAll(filters);
 	}
 }
