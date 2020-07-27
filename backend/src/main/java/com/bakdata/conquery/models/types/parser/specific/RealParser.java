99e60b028dd6b93c40ec1b2d5bc58667c09d4570
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-07-27 13:48:45 +0200

    WIP Real Type storing doubles

--- backend/src/main/java/com/bakdata/conquery/models/types/parser/specific/RealParser.java
+++ backend/src/main/java/com/bakdata/conquery/models/types/parser/specific/RealParser.java
@@ -5,12 +5,17 @@ import com.bakdata.conquery.models.types.CType;
 import com.bakdata.conquery.models.types.parser.Decision;
 import com.bakdata.conquery.models.types.parser.NoopTransformer;
 import com.bakdata.conquery.models.types.parser.Parser;
+import com.bakdata.conquery.models.types.parser.Transformer;
 import com.bakdata.conquery.models.types.specific.RealTypeDouble;
+import com.bakdata.conquery.models.types.specific.RealTypeFloat;
 import com.bakdata.conquery.util.NumberParsing;
+import lombok.NonNull;
 import lombok.ToString;
 
 @ToString(callSuper = true)
 public class RealParser extends Parser<Double> {
+
+	private double floatULP = Float.NEGATIVE_INFINITY;
 
 	@Override
 	protected Double parseValue(String value) throws ParsingException {
@@ -18,7 +23,26 @@ public class RealParser extends Parser<Double> {
 	}
 
 	@Override
+	protected void registerValue(Double v) {
+		floatULP = Math.max(floatULP, Math.ulp(v.floatValue()));
+	}
+
+	@Override
 	protected Decision<Double, ?, ? extends CType<Double, ?>> decideType() {
+
+		if(floatULP < 1e-4){
+			return new Decision<>(
+					new Transformer<Double, Float>() {
+						@Override
+						public Float transform(@NonNull Double value) {
+							return value.floatValue();
+						}
+					},
+					new RealTypeFloat()
+			);
+		}
+
+
 		return new Decision<>(new NoopTransformer<Double>(), new RealTypeDouble());
 	}
 }
