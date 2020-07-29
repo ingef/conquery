cb1659afdcf36ea4857b3ea2609507e7afa45748
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-07-29 08:30:52 +0200

    format code

--- backend/src/main/java/com/bakdata/conquery/models/query/resultinfo/ResultInfo.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/resultinfo/ResultInfo.java
@@ -1,5 +1,3 @@
-
-
 package com.bakdata.conquery.models.query.resultinfo;
 
 import java.util.HashMap;
@@ -18,52 +16,57 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
-@Getter @Setter @ToString @EqualsAndHashCode
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 @RequiredArgsConstructor
 @Slf4j
 public abstract class ResultInfo {
+
 	private final static int UNSET_PREFIX = -1;
 	/**
 	 * Calculated same name index for this column. If 0, the postfix can be omitted.
 	 */
 	@EqualsAndHashCode.Exclude
 	private int postfix = UNSET_PREFIX;
-	
+
 	/**
 	 * Is injected by the {@link ResultInfoCollector} for {@link SelectResultInfo}s.
 	 */
 	@EqualsAndHashCode.Exclude
 	private HashMap<String, Integer> ocurrenceCounter;
 	private ClassToInstanceMap<Object> appendices = MutableClassToInstanceMap.create();
-	
+
 	@NonNull
 	@JsonIgnore
 	public final String getUniqueName(PrintSettings settings) {
 		String name = getName(settings);
-		if(ocurrenceCounter == null) {
+		if (ocurrenceCounter == null) {
 			return name;
 		}
 		synchronized (ocurrenceCounter) {
-			if(postfix == UNSET_PREFIX) {
-				postfix = ocurrenceCounter.compute(name, (k, v) -> (v == null) ? 0 : ++v );
+			if (postfix == UNSET_PREFIX) {
+				postfix = ocurrenceCounter.compute(name, (k, v) -> (v == null) ? 0 : ++v);
 			}
 		}
 		String uniqueName = (postfix > 0) ? name + "_" + postfix : name;
-		if(ocurrenceCounter.containsKey(uniqueName)) {
-			log.warn("Even with postfixing the result will contain column name duplicates. This might be caused by another column that is having a number postfix by default.");
+		if (ocurrenceCounter.containsKey(uniqueName)) {
+			log.warn(
+				"Even with postfixing the result will contain column name duplicates. This might be caused by another column that is having a number postfix by default.");
 		}
 		return (postfix > 0) ? name + "_" + postfix : name;
 	}
-	
+
 	protected abstract String getName(PrintSettings settings);
-	
+
 	@ToString.Include
 	public abstract ResultType getType();
-	
+
 	public <T> void addAppendix(Class<T> cl, T obj) {
 		appendices.putInstance(cl, obj);
 	}
-	
+
 	public ColumnDescriptor asColumnDescriptor(PrintSettings settings) {
 		return ColumnDescriptor.builder()
 			.label(getUniqueName(settings))
