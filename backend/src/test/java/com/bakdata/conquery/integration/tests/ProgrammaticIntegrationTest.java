769abef8519bbe2a05ea6df170289e10f7db10d5
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-07-29 17:10:07 +0200

    reenabled all programmatic integration tests

--- backend/src/test/java/com/bakdata/conquery/integration/tests/ProgrammaticIntegrationTest.java
+++ backend/src/test/java/com/bakdata/conquery/integration/tests/ProgrammaticIntegrationTest.java
@@ -3,9 +3,5 @@ package com.bakdata.conquery.integration.tests;
 import com.bakdata.conquery.integration.IntegrationTest;
 
 public interface ProgrammaticIntegrationTest extends IntegrationTest {
-	
-	public interface This extends ProgrammaticIntegrationTest{
-		
-	}
 
 }
