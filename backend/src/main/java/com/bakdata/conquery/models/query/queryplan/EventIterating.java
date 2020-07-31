33917d478982a1702997dde3df420729ed7fdc6d
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-07-31 11:03:50 +0200

    more docs

--- backend/src/main/java/com/bakdata/conquery/models/query/queryplan/EventIterating.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/queryplan/EventIterating.java
@@ -18,10 +18,17 @@ import com.bakdata.conquery.models.query.entity.Entity;
  * 	- {@link EventIterating::nextBlock}
  * 	- {@link EventIterating::acceptEvent}
  *
- * 	none of the isOfInterest methods may 
+ * 	@implNote none of the isOfInterest methods may maintain state.
  */
 public interface EventIterating {
-	
+
+	/**
+	 * Add all {@code TableId} necessary for processing of this object.
+	 *
+	 * @apiNote an object without required tables is always active (See {@link QPParentNode::nextTable}), but passive.
+	 *
+	 * @param requiredTables the set to inser the tables into.
+	 */
 	default void collectRequiredTables(Set<TableId> requiredTables) {}
 	
 	default Set<TableId> collectRequiredTables() {
@@ -29,15 +36,39 @@ public interface EventIterating {
 		this.collectRequiredTables(out);
 		return out;
 	}
-	
+
+	/**
+	 * Check if the Entity has relevant data. This method is used as early exit for queries to avoid unnecessary computation.
+	 *
+	 * @implNote you may want to create some indices in the Entity to facilitate this object, but consider the additional memory overhead first.
+	 *
+	 * @return false iff the object will never be included in a response of this object. True, if the object may be included in a response by this object.
+	 */
+	default boolean isOfInterest(Entity entity){ return true; }
+
+	/**
+	 * Prefetch data belonging to the nextTable. For example disable some logic based on the data in the table or updating dateRestrictions.
+	 */
 	default void nextTable(QueryExecutionContext ctx, TableId currentTable) {}
-	
+
+	/**
+	 * Check if the Bucket will contain data relevant to this EventIterating.
+	 *
+	 * @return false iff the object will never be included in a response of this object. True, if the object may be included in a response by this object.
+	 */
+	default boolean isOfInterest(Bucket bucket){ return true; }
+
+	/**
+	 * Prefetch data relevant to the incoming Bucket, for example load string-value mappings of the bucket.
+	 */
 	default void nextBlock(Bucket bucket) {}
 
+	/**
+	 * Process the event of the Bucket. It is not guaranteed, that the bucket has associated data, so a {@link Bucket::has}, call is necessary.
+	 *
+	 * @param bucket the bucket containing the data.
+	 * @param event the event to process.
+	 */
 	void acceptEvent(Bucket bucket, int event);
 
-
-	default boolean isOfInterest(Bucket bucket){ return true; }
-	
-	default boolean isOfInterest(Entity entity){ return true; }
 }
