bc3be882e3754fbe024f6837c2375914ee3d3318
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-08-06 11:10:18 +0200

    some comments

--- backend/src/main/java/com/bakdata/conquery/models/concepts/conditions/EqualCondition.java
+++ backend/src/main/java/com/bakdata/conquery/models/concepts/conditions/EqualCondition.java
@@ -3,19 +3,19 @@ package com.bakdata.conquery.models.concepts.conditions;
 import java.util.HashSet;
 import java.util.Map;
 
-import org.hibernate.validator.constraints.NotEmpty;
-
 import com.bakdata.conquery.io.cps.CPSType;
 import com.bakdata.conquery.util.CalculatedValue;
-
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * This condition requires each value to be exactly as given in the list.
  */
 @CPSType(id="EQUAL", base=CTCondition.class)
 public class EqualCondition implements CTCondition {
+
+	// TODO: 06.08.2020 FK: @JsonCreator that uses different Sets for different applications. eg Collections.singleton or ArraySet (for small sets)
 
 	@Setter @Getter @NotEmpty
 	private HashSet<String> values;
