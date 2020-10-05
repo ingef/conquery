eaef1568830c887a5aa6083cdef31ab7ce682931
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-10-05 14:39:56 +0200

    add missing constructor

--- backend/src/main/java/com/bakdata/conquery/models/query/concept/filter/ValidityDateContainer.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/concept/filter/ValidityDateContainer.java
@@ -3,9 +3,12 @@ package com.bakdata.conquery.models.query.concept.filter;
 import javax.validation.constraints.NotNull;
 
 import com.bakdata.conquery.models.identifiable.ids.specific.ValidityDateId;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor(onConstructor_ = @JsonCreator )
 public class ValidityDateContainer {
 	@NotNull
 	private ValidityDateId value;
