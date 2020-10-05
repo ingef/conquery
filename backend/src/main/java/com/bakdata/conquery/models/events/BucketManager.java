d7343058004043302b8202c7a31fd5c80e926fcf
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-10-05 21:57:03 +0200

    adds logs

--- backend/src/main/java/com/bakdata/conquery/models/events/BucketManager.java
+++ backend/src/main/java/com/bakdata/conquery/models/events/BucketManager.java
@@ -76,11 +76,11 @@ public class BucketManager {
 							CBlockId cBlockId = new CBlockId(bucketId, conName);
 
 							if (cBlocks.containsKey(cBlockId)) {
-								log.trace("Skip calculation of CBlock with Id {}, because it was loaded from the storage.");
+								log.trace("Skip calculation of CBlock[{}], because it was loaded from the storage.",cBlockId);
 								continue;
 							}
 
-							log.warn("CBlock[{}] missing in Storage.", cBlockId);
+							log.warn("CBlock[{}] missing in Storage. Queuing recalculation", cBlockId);
 							job.addCBlock(imp, bucket.get(), cBlockId);
 						}
 					}
