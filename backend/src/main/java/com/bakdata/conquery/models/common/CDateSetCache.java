97dbe9bfcf315a2fb02730d529c942e00d5ef830
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-10-15 11:57:07 +0200

    Add documentation

--- backend/src/main/java/com/bakdata/conquery/models/common/CDateSetCache.java
+++ backend/src/main/java/com/bakdata/conquery/models/common/CDateSetCache.java
@@ -4,14 +4,18 @@ import java.lang.ref.Cleaner;
 import java.lang.ref.Reference;
 import java.lang.ref.SoftReference;
 import java.util.BitSet;
-import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.queue.SynchronizedQueue;
 
+/**
+ * Cache to avoid constant reallocation of huge CDateSets, instead cache/reuse their BitSets by way of a {@link Cleaner} that hooks into the GC and will give us those objects back.
+ *
+ * The underlying data-structures are also maintained as {@link SoftReference} such that they are also subject to GC when memory is demanded.
+ */
 @Slf4j
 public class CDateSetCache {
 
@@ -19,42 +23,58 @@ public class CDateSetCache {
 	private final Cleaner cleaner = Cleaner.create();
 
 	public CDateSetCache() {
-		pool = SynchronizedQueue.synchronizedQueue(new LinkedList<>());
+		pool = new ConcurrentLinkedQueue<>();
 	}
 
 	public BitMapCDateSet acquire() {
-		final BitMapCDateSet back = doAcquire();
+		final BitMapCDateSet out = doAcquire();
 
-		final Container container = new Container(back.getNegativeBits(), back.getPositiveBits());
+		// create and maintain hardref on the bitsets
+		final Container container = new Container(out.getNegativeBits(), out.getPositiveBits());
 
-		cleaner.register(back, () -> {
-			log.info("Releasing Object");
+		cleaner.register(out, () -> {
+			log.trace("Object was released.");
 
+			// Reset the bitsets
 			container.getLeft().clear();
 			container.getRight().clear();
 
+			// Add reference of container to the bitset, so that they can be freed if memory is demanded.
 			pool.add(new SoftReference<>(container));
 		});
 
-		return back;
+		return out;
 	}
 
+	/**
+	 * Try to reuse old BitSets if available. Else create a new one.
+	 */
 	private BitMapCDateSet doAcquire() {
 		Reference<Container> reference;
 		Container container;
 
-		while ((reference = pool.poll()) != null) {
+		log.trace("Have {} Objects available", pool.size());
+
+		while (true) {
+			reference = pool.poll();
+
+			// Pool is empty?
+			if(reference == null){
+				break;
+			}
+
 			container = reference.get();
 
+			// Reference was cleared by GC?
 			if(container == null){
 				continue;
 			}
 
-			log.info("Found prior Object");
+			log.trace("Found prior Object");
 			return new BitMapCDateSet(container.getLeft(), container.getRight());
 		}
 
-		log.info("Creating new new Object.");
+		log.trace("Creating new new Object.");
 		return BitMapCDateSet.create();
 	}
 
