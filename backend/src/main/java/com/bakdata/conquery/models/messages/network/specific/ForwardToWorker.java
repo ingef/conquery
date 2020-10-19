b22e9c3a17c1af84142ed088656e7bb900fdfde3
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-10-19 17:22:29 +0200

    Revert "Deserialize Worker Messages in Namespace"
    This reverts commit 56468aaf

--- backend/src/main/java/com/bakdata/conquery/models/messages/network/specific/ForwardToWorker.java
+++ backend/src/main/java/com/bakdata/conquery/models/messages/network/specific/ForwardToWorker.java
@@ -3,52 +3,49 @@ package com.bakdata.conquery.models.messages.network.specific;
 import java.util.Objects;
 
 import com.bakdata.conquery.io.cps.CPSType;
-import com.bakdata.conquery.io.jackson.Jackson;
 import com.bakdata.conquery.models.identifiable.ids.specific.WorkerId;
 import com.bakdata.conquery.models.messages.SlowMessage;
 import com.bakdata.conquery.models.messages.namespaces.WorkerMessage;
 import com.bakdata.conquery.models.messages.network.MessageToShardNode;
 import com.bakdata.conquery.models.messages.network.NetworkMessage;
 import com.bakdata.conquery.models.messages.network.NetworkMessageContext.ShardNodeNetworkContext;
-import com.bakdata.conquery.models.worker.SingletonNamespaceCollection;
 import com.bakdata.conquery.models.worker.Worker;
 import com.bakdata.conquery.util.io.ConqueryMDC;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.bakdata.conquery.util.progressreporter.ProgressReporter;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@CPSType(id = "FORWARD_TO_WORKER", base = NetworkMessage.class)
-@Getter
-public class ForwardToWorker extends MessageToShardNode.Slow {
+@CPSType(id="FORWARD_TO_WORKER", base=NetworkMessage.class)
+@RequiredArgsConstructor @Getter
+public class ForwardToWorker extends MessageToShardNode implements SlowMessage {
 
 	private final WorkerId workerId;
-	private final JsonNode message;
-
-	public ForwardToWorker(WorkerId workerId, WorkerMessage workerMessage) {
-		this.workerId = workerId;
-		this.message = Jackson.MAPPER.valueToTree(workerMessage);
-	}
-
+	private final WorkerMessage message;
+	
 	@Override
 	public void react(ShardNodeNetworkContext context) throws Exception {
 		Worker w = Objects.requireNonNull(context.getWorkers().getWorker(workerId));
 		ConqueryMDC.setLocation(w.toString());
-
-		final ObjectMapper objectMapper = new SingletonNamespaceCollection(
-				w.getStorage().getCentralRegistry()).injectInto(w.getStorage().getDataset().injectInto(Jackson.MAPPER));
-		WorkerMessage workerMessage = objectMapper
-													.readValue(message.traverse(objectMapper), WorkerMessage.class);
-
-		if(workerMessage.isSlowMessage()){
-			((SlowMessage) workerMessage).setProgressReporter(getProgressReporter());
-		}
-
-		workerMessage.react(w);
+		message.react(w);
 	}
 
+	@Override
+	public boolean isSlowMessage() {
+		return message.isSlowMessage();
+	}
 
 	@Override
+	public ProgressReporter getProgressReporter() {
+		return ((SlowMessage)message).getProgressReporter();
+	}
+
+	@Override
+	public void setProgressReporter(ProgressReporter progressReporter) {
+		((SlowMessage)message).setProgressReporter(progressReporter);
+	}
+	
+	@Override
 	public String toString() {
-		return message.toString() + " for worker " + workerId;
+		return message.toString()+" for worker "+workerId;
 	}
 }
