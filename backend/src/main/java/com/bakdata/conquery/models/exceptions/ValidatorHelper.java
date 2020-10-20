4f9193737f6753d45d8f804e22526139cf99be0c
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-10-20 18:49:47 +0200

    improve validation helper

--- backend/src/main/java/com/bakdata/conquery/models/exceptions/ValidatorHelper.java
+++ backend/src/main/java/com/bakdata/conquery/models/exceptions/ValidatorHelper.java
@@ -1,46 +1,54 @@
 package com.bakdata.conquery.models.exceptions;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.validation.ConstraintViolation;
-import javax.ws.rs.core.Request;
 
-import io.dropwizard.jersey.validation.ConstraintMessage;
 import lombok.experimental.UtilityClass;
-import org.glassfish.jersey.process.Inflector;
-import org.glassfish.jersey.server.model.Invocable;
 import org.slf4j.Logger;
 
 @UtilityClass
 public final class ValidatorHelper {
+	
+	private static final String VERTICAL_DIVIDER = "------------------------------------\n";
 
 	public static void failOnError(Logger log, Set<? extends ConstraintViolation<?>> violations) throws JSONException {
 		failOnError(log, violations, null);
 	}
 	
-	public static void failOnError(Logger log, Set<? extends ConstraintViolation<?>> violations, String context) throws JSONException {
-		List<String> violationMessages = violations
-				.stream()
-				.map( v->
-					ConstraintMessage.getMessage(v,Invocable.create((Inflector<Request, Void>) data -> null)))
-				.collect(Collectors.toList());
+	public static <V extends ConstraintViolation<?>> void failOnError(Logger log, Set<V> violations, String context) throws JSONException {
 		
-		failOnError(log, violationMessages, context);
+		Map<Object, List<V>> mapByLeaf = violations.stream().collect(Collectors.groupingBy(ConstraintViolation::getLeafBean));
+		
+		throw new JSONException(mapByLeaf.entrySet().stream().map(ValidatorHelper::createViolationString).collect(Collectors.joining(VERTICAL_DIVIDER)));
 	}
 	
-	public static void failOnError(Logger log, List<String> violations, String context) throws JSONException {
-		if(violations.size()>0) {
-			for(String v:violations) {
-				log.error(v);
-			}
-			
-			if(context!=null) {
-				throw new JSONException("Failed with "+violations.size()+" errors in "+context+".");
-			}
-			throw new JSONException("Failed with "+violations.size()+" errors.");
+	/**
+	 * Combines all violations for a given leaf object and gives the path to the root object if possible.
+	 */
+	private static <V extends ConstraintViolation<?>> String createViolationString(Map.Entry<Object, List<V>> objectToViolation) {
+		Object leaf = objectToViolation.getKey();
+		List<V> violations = objectToViolation.getValue();
+		V firstViolation = violations.get(0);
+		
+		if(leaf == null) {
+			// if validation is not directly mappable to an object
+			return violations.stream().map(ConstraintViolation::getMessage).collect(Collectors.joining("\n"));
 		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("\nThe object of type ").append(leaf.getClass()).append(" caused the following problem(s):\n");
+		for(V violation : violations) {
+			sb.append(violation.getMessage()).append("\n");
+		}
+		if(firstViolation.getRootBean() != null && !firstViolation.getRootBean().equals(firstViolation.getLeafBean())) {
+			sb.append("The object was nested in an object of type ").append(firstViolation.getRootBean().getClass()).append(" through the path ").append(firstViolation.getPropertyPath()).append("\n");
+		}
+		sb.append("String representation of the failing object:\n").append(leaf);
+		return sb.toString();
 	}
 
 }
