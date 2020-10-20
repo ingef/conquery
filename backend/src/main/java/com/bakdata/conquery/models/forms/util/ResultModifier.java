72650d22a70ea82e0993aa30fd9c0f9b32200beb
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-10-20 15:21:59 +0200

    remove commented code block

--- backend/src/main/java/com/bakdata/conquery/models/forms/util/ResultModifier.java
+++ backend/src/main/java/com/bakdata/conquery/models/forms/util/ResultModifier.java
@@ -11,66 +11,38 @@ import com.bakdata.conquery.models.query.results.ContainedEntityResult;
 import com.bakdata.conquery.util.functions.ChainableUnaryOperator;
 
 public class ResultModifier {
+
 	/**
 	 * Modifies the given result lines with the given function.
 	 * 
-	 * If the result is not contained this method creates a default result row anyways.
+	 * If the result is not contained this method creates a default result row
+	 * anyways.
 	 */
 	public static <T extends ContainedEntityResult> T modify(T inResult, UnaryOperator<Object[]> modification) {
-		
+
 		inResult.modifyResultLinesInplace(modification);
-		
+
 		return inResult;
 	}
-	
+
 	public static ChainableUnaryOperator<Object[]> existAggValuesSetterFor(List<Aggregator<?>> aggregators, OptionalInt firstAggPos) {
 		return (result) -> setExistAggValues(aggregators, result, firstAggPos);
 	}
-	
-	private static Object[] setExistAggValues(List<Aggregator<?>> aggregators, Object[] result, OptionalInt firstAggPos){
+
+	private static Object[] setExistAggValues(List<Aggregator<?>> aggregators, Object[] result, OptionalInt firstAggPos) {
 		int aggIdx = firstAggPos.orElse(0);
-		/* Special handling here, because a subquery might not be contained but has an EXIST select.
-		 * This would cause null (empty cell), so we fetch all EXIST result and put them to the end result.
+		/*
+		 * Special handling here, because a subquery might not be contained but has an
+		 * EXIST select. This would cause null (empty cell), so we fetch all EXIST
+		 * result and put them to the end result.
 		 */
-		for(Aggregator<?> agg : aggregators) {
+		for (Aggregator<?> agg : aggregators) {
 			// Fill EXIST aggregators with false which evaluated to 'null'
-			if(agg instanceof ExistsAggregator && Objects.isNull(result[aggIdx])) {
+			if (agg instanceof ExistsAggregator && Objects.isNull(result[aggIdx])) {
 				result[aggIdx] = false;
 			}
 			aggIdx++;
 		}
 		return result;
 	}
-	
-//	/**
-//	 * Modifies the given result lines with the given function.
-//	 * 
-//	 * If the result is not contained this method returns an empty list
-//	 */
-//	public static List<Object[]> modify(EntityResult inResult, UnaryOperator<Object[]> modification) {
-//		if(inResult.isFailed()) {
-//			throw new RuntimeException("failed result can't be modified: "+inResult);
-//		}
-//		if(!inResult.isContained()) {
-//			return Collections.emptyList();
-//		}
-//		
-//		if(inResult instanceof SinglelineContainedEntityResult) {
-//			return Collections.singletonList(
-//				modification.apply(
-//					((SinglelineContainedEntityResult) inResult).getValues()
-//				)
-//			);
-//		}
-//		else if(inResult instanceof MultilineContainedEntityResult) {
-//			var values = ((MultilineContainedEntityResult) inResult).getValues();
-//			List<Object[]> result = new ArrayList<>(values);
-//			result.replaceAll(modification);
-//			return result;
-//		}
-//		else {
-//			throw new UnsupportedOperationException();
-//		}
-//	}
-
 }
