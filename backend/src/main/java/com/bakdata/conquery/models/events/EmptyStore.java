19c1c432e00e6e47262da437f0a379e505721b55
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-11-25 13:13:04 +0100

    Implement empty Store

---
+++ backend/src/main/java/com/bakdata/conquery/models/events/EmptyStore.java
@@ -0,0 +1,84 @@
+package com.bakdata.conquery.models.events;
+
+import java.math.BigDecimal;
+
+import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.models.common.daterange.CDateRange;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+
+@CPSType(base = ColumnStore.class, id = "EMPTY")
+public class EmptyStore implements ColumnStore {
+
+	@Getter(onMethod_ = JsonCreator.class)
+	private static EmptyStore Instance = new EmptyStore();
+
+	private EmptyStore(){
+
+	}
+
+	@Override
+	public ColumnStore select(int[] starts, int[] length) {
+		return null;
+	}
+
+	@Override
+	public void set(int event, Object value) {
+
+	}
+
+	@Override
+	public Object get(int event) {
+		return null;
+	}
+
+	@Override
+	public boolean has(int event) {
+		return false;
+	}
+
+	@Override
+	public int getString(int event) {
+		return 0;
+	}
+
+	@Override
+	public long getInteger(int event) {
+		return 0;
+	}
+
+	@Override
+	public boolean getBoolean(int event) {
+		return false;
+	}
+
+	@Override
+	public double getReal(int event) {
+		return 0;
+	}
+
+	@Override
+	public BigDecimal getDecimal(int event) {
+		return null;
+	}
+
+	@Override
+	public long getMoney(int event) {
+		return 0;
+	}
+
+	@Override
+	public int getDate(int event) {
+		return 0;
+	}
+
+	@Override
+	public CDateRange getDateRange(int event) {
+		return null;
+	}
+
+	@Override
+	public Object getAsObject(int event) {
+		return null;
+	}
+}
