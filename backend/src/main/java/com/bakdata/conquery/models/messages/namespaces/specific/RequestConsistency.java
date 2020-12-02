ae7556c119b0334c1a653db806a817f7b788b2d7
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-12-02 15:11:28 +0100

    gather bucket ids

--- backend/src/main/java/com/bakdata/conquery/models/messages/namespaces/specific/RequestConsistency.java
+++ backend/src/main/java/com/bakdata/conquery/models/messages/namespaces/specific/RequestConsistency.java
@@ -2,11 +2,11 @@ package com.bakdata.conquery.models.messages.namespaces.specific;
 
 import com.bakdata.conquery.io.cps.CPSType;
 import com.bakdata.conquery.models.datasets.Import;
+import com.bakdata.conquery.models.events.Bucket;
 import com.bakdata.conquery.models.identifiable.ids.specific.BucketId;
 import com.bakdata.conquery.models.identifiable.ids.specific.ImportId;
 import com.bakdata.conquery.models.messages.namespaces.NamespacedMessage;
 import com.bakdata.conquery.models.messages.namespaces.WorkerMessage;
-import com.bakdata.conquery.models.messages.namespaces.specific.ReportConsistency;
 import com.bakdata.conquery.models.worker.Worker;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,8 +18,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @CPSType(id="REQUEST_CONSISTENCY", base= NamespacedMessage.class)
-@AllArgsConstructor
-@NoArgsConstructor
 @Setter
 @Getter
 @Slf4j
@@ -30,8 +28,8 @@ public class RequestConsistency extends WorkerMessage {
         // Gather ImportIds
         Set<ImportId> workerImports = context.getStorage().getAllImports().stream().map(Import::getId).collect(Collectors.toSet());
 
-        //TODO Gather BucketIds
-        Set<BucketId> workerBuckets = Set.of();
+        // Gather BucketIds
+        Set<BucketId> workerBuckets = context.getStorage().getAllBuckets().stream().map(Bucket::getId).collect(Collectors.toSet());
 
         // Send report
         context.send(new ReportConsistency(context.getInfo().getId(), workerImports, workerBuckets));
