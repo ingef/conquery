b7bbd32e021964355ec35bfd26ce0eea7e97ebbb
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-12-02 13:32:20 +0100

    check import consistency

--- backend/src/main/java/com/bakdata/conquery/models/messages/network/specific/ReportConsistency.java
+++ backend/src/main/java/com/bakdata/conquery/models/messages/network/specific/ReportConsistency.java
@@ -4,36 +4,53 @@ import com.bakdata.conquery.models.datasets.Import;
 import com.bakdata.conquery.models.identifiable.ids.specific.BucketId;
 import com.bakdata.conquery.models.identifiable.ids.specific.ImportId;
 import com.bakdata.conquery.models.identifiable.ids.specific.WorkerId;
+import com.bakdata.conquery.models.messages.namespaces.NamespaceMessage;
 import com.bakdata.conquery.models.messages.network.MessageToManagerNode;
 import com.bakdata.conquery.models.messages.network.NetworkMessageContext;
 import com.bakdata.conquery.models.worker.Namespace;
 import com.google.common.collect.Sets;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
-public class ReportConsistency extends MessageToManagerNode {
+@RequiredArgsConstructor
+public class ReportConsistency extends NamespaceMessage {
 
-    private WorkerId workerId;
-    private Set<ImportId> workerImports;
-    private Set<BucketId> workerBuckets;
+    private final WorkerId workerId;
+    private final Set<ImportId> workerImports;
+    private final Set<BucketId> workerBuckets;
+
 
     @Override
-    public void react(NetworkMessageContext.ManagerNodeNetworkContext context) throws Exception {
+    public void react(Namespace context) throws Exception {
         Namespace namespace = context.getNamespaces().get(workerId.getDataset());
         Set<ImportId> managerImports = namespace.getStorage().getAllImports().stream().map(Import::getId).collect(Collectors.toSet());
 
-        checkImports(managerImports, workerImports);
+        boolean importsOkay = isImportsConsistent(managerImports, workerImports, workerId);
     }
 
-    private static void checkImports(Set<ImportId> managerImports, Set<ImportId> workerImports, Worker) {
-        Sets.SetView<ImportId> difference = Sets.difference(managerImports, workerImports);
+    private static boolean isImportsConsistent(Set<ImportId> managerImports, Set<ImportId> workerImports, WorkerId workerId) {
+        Sets.SetView<ImportId> differences = Sets.difference(managerImports, workerImports);
 
-        if (difference.isEmpty()) {
-            log.info("Imports of worker {} are consistent with the imports of manager", );
-            return;
+        if (differences.isEmpty()) {
+            log.info("Imports of worker {} are consistent with the imports of manager", workerId);
+            return true;
         }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Found inconsistencies:\n");
+        for( ImportId difference : differences) {
+            if(!managerImports.contains(difference)) {
+                sb.append("\tImport [").append(difference).append("] is not present on the manager but on the worker [").append(workerId).append("].\n");
+            }
+            else {
+                sb.append("\tImport [").append(difference).append("] is not present on the worker but on the manager [").append(workerId).append("].\n");
+            }
+        }
+        log.error(sb.toString());
+        return false;
     }
 }
--- backend/src/main/java/com/bakdata/conquery/models/messages/network/specific/RequestConsistency.java
+++ backend/src/main/java/com/bakdata/conquery/models/messages/network/specific/RequestConsistency.java
@@ -1,12 +1,27 @@
 package com.bakdata.conquery.models.messages.network.specific;
 
+import com.bakdata.conquery.models.datasets.Import;
+import com.bakdata.conquery.models.identifiable.ids.specific.BucketId;
+import com.bakdata.conquery.models.identifiable.ids.specific.ImportId;
+import com.bakdata.conquery.models.messages.namespaces.WorkerMessage;
 import com.bakdata.conquery.models.messages.network.MessageToShardNode;
 import com.bakdata.conquery.models.messages.network.NetworkMessageContext;
+import com.bakdata.conquery.models.worker.Worker;
 
-public class RequestConsistency extends MessageToShardNode {
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class RequestConsistency extends WorkerMessage {
+
     @Override
-    public void react(NetworkMessageContext.ShardNodeNetworkContext context) throws Exception {
+    public void react(Worker context) throws Exception {
+        // Gather ImportIds
+        Set<ImportId> workerImports = context.getStorage().getAllImports().stream().map(Import::getId).collect(Collectors.toSet());
 
-        context.send(new ReportConsistency(worker.getInfo()));
+        //TODO Gather BucketIds
+        Set<BucketId> workerBuckets = Set.of();
+
+        // Send report
+        context.send(new ReportConsistency(context.getInfo().getId(), workerImports, workerBuckets));
     }
 }
