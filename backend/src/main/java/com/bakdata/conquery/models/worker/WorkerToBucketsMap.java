cbb46edb9fda8604707c1903f9d058f3ef540b23
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-12-03 17:22:46 +0100

    change deser to getter/setter away from JsonCreator

--- backend/src/main/java/com/bakdata/conquery/models/worker/WorkerToBucketsMap.java
+++ backend/src/main/java/com/bakdata/conquery/models/worker/WorkerToBucketsMap.java
@@ -1,17 +1,24 @@
 package com.bakdata.conquery.models.worker;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 import com.bakdata.conquery.models.identifiable.ids.specific.BucketId;
 import com.bakdata.conquery.models.identifiable.ids.specific.ImportId;
 import com.bakdata.conquery.models.identifiable.ids.specific.WorkerId;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-
-@AllArgsConstructor(onConstructor = @__({@JsonCreator}))
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class WorkerToBucketsMap {
     private Map<WorkerId, Set<BucketId>> map = new ConcurrentHashMap<>();
 
