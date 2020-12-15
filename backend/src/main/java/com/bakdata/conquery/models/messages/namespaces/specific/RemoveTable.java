213dc8cd0d3c1a0511e6f4012d928216a65a078a
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2020-12-15 17:03:20 +0100

    implement CRUD messages for Table

---
+++ backend/src/main/java/com/bakdata/conquery/models/messages/namespaces/specific/RemoveTable.java
@@ -0,0 +1,28 @@
+package com.bakdata.conquery.models.messages.namespaces.specific;
+
+import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.models.identifiable.ids.specific.TableId;
+import com.bakdata.conquery.models.messages.namespaces.NamespacedMessage;
+import com.bakdata.conquery.models.messages.namespaces.WorkerMessage;
+import com.bakdata.conquery.models.worker.Worker;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@CPSType(id="UPDATE_DATASET", base=NamespacedMessage.class) @Slf4j
+@AllArgsConstructor(onConstructor_=@JsonCreator) @Getter @Setter @ToString(callSuper=true)
+public class RemoveTable extends WorkerMessage.Slow {
+
+	private TableId table;
+
+	@Override
+	public void react(Worker context) throws Exception {
+		log.info("Received update of Table {}", table);
+		synchronized (context.getStorage()) {
+			context.removeTable(table);
+		}
+	}
+}
---
+++ backend/src/main/java/com/bakdata/conquery/models/messages/namespaces/specific/UpdateTable.java
@@ -0,0 +1,28 @@
+package com.bakdata.conquery.models.messages.namespaces.specific;
+
+import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.models.datasets.Table;
+import com.bakdata.conquery.models.messages.namespaces.NamespacedMessage;
+import com.bakdata.conquery.models.messages.namespaces.WorkerMessage;
+import com.bakdata.conquery.models.worker.Worker;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@CPSType(id="UPDATE_DATASET", base=NamespacedMessage.class) @Slf4j
+@AllArgsConstructor(onConstructor_=@JsonCreator) @Getter @Setter @ToString(callSuper=true)
+public class UpdateTable extends WorkerMessage.Slow {
+
+	private Table table;
+
+	@Override
+	public void react(Worker context) throws Exception {
+		log.info("Received update of Table {}", table.getId());
+		synchronized (context.getStorage()) {
+			context.addTable(table);
+		}
+	}
+}
