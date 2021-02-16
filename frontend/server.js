9cc0a64a3d068963a22b1fda8fead315575ae7cf
Kai Rollmann <kai.rollmann@gmail.com>
2021-02-16 17:48:27 +0100

    Report CSP issues only

--- frontend/server.js
+++ frontend/server.js
@@ -11,7 +11,13 @@ const path = require("path");
 
 const PORT = process.env.PORT || 4242;
 
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      reportOnly: true,
+    },
+  })
+);
 app.disable("x-powered-by");
 app.use(compression());
 app.use(cors());
