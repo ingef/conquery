49eb646883e90d860f8e8bfd92d63f27f05191b1
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2021-04-06 12:59:57 +0200

    fixes serializationtest

--- backend/src/test/java/com/bakdata/conquery/models/dictionary/MapDictionaryTest.java
+++ backend/src/test/java/com/bakdata/conquery/models/dictionary/MapDictionaryTest.java
@@ -8,6 +8,7 @@ import com.bakdata.conquery.io.jackson.Jackson;
 import com.bakdata.conquery.io.jackson.serializer.SerializationTestUtil;
 import com.bakdata.conquery.models.datasets.Dataset;
 import com.bakdata.conquery.models.exceptions.JSONException;
+import com.bakdata.conquery.models.identifiable.CentralRegistry;
 import org.junit.jupiter.api.Test;
 
 
@@ -22,12 +23,16 @@ class MapDictionaryTest {
 		map.add("b".getBytes());
 		map.add("c".getBytes());
 
+		final CentralRegistry registry = new CentralRegistry();
+		registry.register(Dataset.PLACEHOLDER);
+
 		String json = Jackson.MAPPER.writeValueAsString(map);
 		assertThat(json)
-				.startsWith("{\"type\":\"MAP_DICTIONARY\",\"name\":\"dictionary\",\"id2Value\":[")
-				.endsWith("],\"dataset\":\"dataset\"}");
+				.startsWith("{\"type\":\"MAP_DICTIONARY\",\"dataset\":\"PLACEHOLDER\",\"name\":\"dictionary\",\"id2Value\":[")
+				.endsWith("]}");
 		SerializationTestUtil
 				.forType(MapDictionary.class)
+				.registry(registry)
 				.test(map);
 	}
 
