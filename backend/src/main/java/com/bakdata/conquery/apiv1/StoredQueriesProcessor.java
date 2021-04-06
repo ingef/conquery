567ef681f72ff23d56647348db5ceb848b5514be
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2021-04-06 15:33:52 +0200

    Check if user is allowed to delete query

--- backend/src/main/java/com/bakdata/conquery/apiv1/StoredQueriesProcessor.java
+++ backend/src/main/java/com/bakdata/conquery/apiv1/StoredQueriesProcessor.java
@@ -108,13 +108,16 @@ public class StoredQueriesProcessor {
 		return root instanceof CQAnd;
 	}
 
-    public void deleteQuery(ManagedExecutionId executionId) {
+    public void deleteQuery(User user, ManagedExecutionId executionId) {
         ManagedExecution<?> execution = storage.getExecution(executionId);
 		if (execution == null) {
 			throw new NotFoundException(executionId.toString());
 		}
 
-        storage.removeExecution(executionId);
+		authorize(user, execution, Ability.DELETE);
+
+
+		storage.removeExecution(executionId);
     }
 
     public FullExecutionStatus getQueryFullStatus(ManagedExecutionId queryId, User user, UriBuilder url) {
