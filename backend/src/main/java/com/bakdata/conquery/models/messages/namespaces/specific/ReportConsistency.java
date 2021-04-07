4f81caf768b51808d54086a790884f24386291a5
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2021-04-07 17:09:24 +0200

    Cherry Picked ReportConsistency with Imports

--- backend/src/main/java/com/bakdata/conquery/models/messages/namespaces/specific/ReportConsistency.java
+++ backend/src/main/java/com/bakdata/conquery/models/messages/namespaces/specific/ReportConsistency.java
@@ -1,10 +1,13 @@
 package com.bakdata.conquery.models.messages.namespaces.specific;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.models.datasets.Import;
 import com.bakdata.conquery.models.identifiable.ids.IId;
 import com.bakdata.conquery.models.identifiable.ids.specific.BucketId;
+import com.bakdata.conquery.models.identifiable.ids.specific.ImportId;
 import com.bakdata.conquery.models.identifiable.ids.specific.WorkerId;
 import com.bakdata.conquery.models.messages.namespaces.NamespaceMessage;
 import com.bakdata.conquery.models.messages.namespaces.NamespacedMessage;
@@ -22,7 +25,7 @@ import lombok.extern.slf4j.Slf4j;
  * the {@link com.bakdata.conquery.commands.ManagerNode} assumed the Worker to have and reports an error if there are
  * inconsistencies.
  */
-@CPSType(id = "REPORT_CONSISTENCY", base = NamespacedMessage.class)
+@CPSType(id="REPORT_CONSISTENCY", base= NamespacedMessage.class)
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
@@ -30,45 +33,50 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ReportConsistency extends NamespaceMessage {
 
-	private WorkerId workerId;
-	// Set default here because an empty set send by the worker is not set (it is null) after deserialization
-	private Set<BucketId> workerBuckets = Set.of();
+    private WorkerId workerId;
+    // Set default here because an empty set send by the worker is not set (it is null) after deserialization
+    private Set<ImportId> workerImports = Set.of();
+    private Set<BucketId> workerBuckets = Set.of();
 
 
-	@Override
-	public void react(Namespace context) throws Exception {
-		Set<BucketId> assignedWorkerBuckets = context.getBucketsForWorker(workerId);
+    @Override
+    public void react(Namespace context) throws Exception {
+        Set<ImportId> managerImports = context.getStorage().getAllImports().stream().map(Import::getId).collect(Collectors.toSet());
 
-		boolean bucketsOkay = isConsistent("Buckets", assignedWorkerBuckets, workerBuckets, workerId);
+        Set<BucketId> assignedWorkerBuckets = context.getBucketsForWorker(workerId);
 
-		log.trace("Buckets on worker[{}}: {}", workerId, workerBuckets);
+        boolean importsOkay = isConsistent("Imports", managerImports, workerImports, workerId);
+        boolean bucketsOkay = isConsistent("Buckets", assignedWorkerBuckets, workerBuckets, workerId);
 
-		if (bucketsOkay) {
-			log.info("Consistency check was successful");
-			return;
-		}
-		throw new IllegalStateException("Detected inconsistency between manager and worker [" + workerId + "]");
-	}
+        log.trace("Imports on worker[{}}: {}", workerId, workerImports);
+        log.trace("Buckets on worker[{}}: {}", workerId, workerBuckets);
 
-	private static <ID extends IId<?>> boolean isConsistent(String typeName, @NonNull Set<ID> managerIds, @NonNull Set<ID> workerIds, WorkerId workerId) {
-		Sets.SetView<ID> notInWorker = Sets.difference(managerIds, workerIds);
-		Sets.SetView<ID> notInManager = Sets.difference(workerIds, managerIds);
+        if (importsOkay && bucketsOkay) {
+            log.info("Consistency check was successful");
+            return;
+        }
+        throw new IllegalStateException("Detected inconsistency between manager and worker [" + workerId + "]");
+    }
 
-		if (notInWorker.isEmpty() && notInManager.isEmpty()) {
-			log.info("{} of worker {} are consistent with the manager: {} {}", typeName, workerId, managerIds.size(), typeName);
-			return true;
-		}
+    private static <ID extends IId<?>> boolean isConsistent(String typeName, @NonNull Set<ID> managerIds, @NonNull Set<ID> workerIds, WorkerId workerId) {
+        Sets.SetView<ID> notInWorker = Sets.difference(managerIds, workerIds);
+        Sets.SetView<ID> notInManager = Sets.difference(workerIds, managerIds);
 
-		StringBuilder sb = new StringBuilder();
-		sb.append("Found inconsistencies for ").append(typeName).append(":\n");
-		for (ID difference : notInWorker) {
-			sb.append("\t[").append(difference).append("] is not present on the worker but on the manager [").append(workerId).append("].\n");
-		}
-		for (ID difference : notInManager) {
-			sb.append("\t[").append(difference).append("] is not present on the manager but on the worker [").append(workerId).append("].\n");
-		}
+        if (notInWorker.isEmpty() && notInManager.isEmpty()) {
+            log.info("{} of worker {} are consistent with the manager: {} {}", typeName, workerId, managerIds.size(), typeName);
+            return true;
+        }
 
-		log.error(sb.toString());
-		return false;
-	}
+        StringBuilder sb = new StringBuilder();
+        sb.append("Found inconsistencies for ").append(typeName).append(":\n");
+        for( ID difference : notInWorker) {
+            sb.append("\t[").append(difference).append("] is not present on the worker but on the manager [").append(workerId).append("].\n");
+        }
+        for( ID difference : notInManager) {
+            sb.append("\t[").append(difference).append("] is not present on the manager but on the worker [").append(workerId).append("].\n");
+        }
+
+        log.error(sb.toString());
+        return false;
+    }
 }
