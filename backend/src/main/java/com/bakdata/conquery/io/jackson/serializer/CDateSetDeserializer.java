298c27d344d3b72c5641c7aacf153860f4683a99
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2021-04-22 10:53:59 +0200

    cleanup CDateSetDeserializer

--- backend/src/main/java/com/bakdata/conquery/io/jackson/serializer/CDateSetDeserializer.java
+++ backend/src/main/java/com/bakdata/conquery/io/jackson/serializer/CDateSetDeserializer.java
@@ -13,32 +13,33 @@ import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 public class CDateSetDeserializer extends StdDeserializer<CDateSet> {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private final DateFormats formats;
+    private final DateFormats formats;
 
 
-	public CDateSetDeserializer(DateFormats formats) {
-		super(CDateSet.class);
-		this.formats = formats;
-	}
+    public CDateSetDeserializer(DateFormats formats) {
+        super(CDateSet.class);
+        this.formats = formats;
+    }
 
-	@Override
-	public CDateSet deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-		if (p.currentToken() == JsonToken.START_ARRAY) {
-			int[][] ints = p.readValueAs(int[][].class);
-			
-			CDateSet set = CDateSet.create();
-			for(int i=0; i<ints.length; i++) {
-				set.add(CDateRange.of(ints[i][0], ints[i][1]));
-			}
-			return set;
-		}
-		else if(p.currentToken() == JsonToken.VALUE_STRING) {
-			return CDateSet.parse(p.readValueAs(String.class), formats);
-		}
-		else {
-			return (CDateSet) ctxt.handleUnexpectedToken(CDateSet.class, p.currentToken(), p, "can't deserialize CDateSet");
-		}
-	}
+    @Override
+    public CDateSet deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        if (p.currentToken() == JsonToken.START_ARRAY) {
+            int[][] ints = p.readValueAs(int[][].class);
+
+            CDateSet set = CDateSet.create();
+            for (int i = 0; i < ints.length; i++) {
+                set.add(CDateRange.of(ints[i][0], ints[i][1]));
+            }
+            return set;
+        }
+
+        if (p.currentToken() == JsonToken.VALUE_STRING) {
+            return CDateSet.parse(p.readValueAs(String.class), formats);
+        }
+
+        return (CDateSet) ctxt.handleUnexpectedToken(CDateSet.class, p.currentToken(), p, "can't deserialize CDateSet");
+
+    }
 }
