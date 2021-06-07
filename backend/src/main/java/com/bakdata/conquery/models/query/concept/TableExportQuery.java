3ce4987cf2ff9b2f4348675bada9c2a7bccd4fba
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2021-06-07 14:40:06 +0200

    restructures TableExportQuery, pulling SecondaryIds and Dates to the front, merging them where possible, to declutter the output

--- backend/src/main/java/com/bakdata/conquery/models/query/concept/TableExportQuery.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/concept/TableExportQuery.java
@@ -3,20 +3,30 @@ package com.bakdata.conquery.models.query.concept;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import com.bakdata.conquery.ConqueryConstants;
 import com.bakdata.conquery.apiv1.QueryDescription;
 import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.io.jackson.InternalOnly;
+import com.bakdata.conquery.io.jackson.serializer.IdReferenceKeySerializer;
+import com.bakdata.conquery.io.jackson.serializer.NsIdReferenceKeyDeserializer;
 import com.bakdata.conquery.models.common.Range;
 import com.bakdata.conquery.models.common.daterange.CDateRange;
 import com.bakdata.conquery.models.concepts.Connector;
 import com.bakdata.conquery.models.datasets.Column;
+import com.bakdata.conquery.models.datasets.SecondaryIdDescription;
 import com.bakdata.conquery.models.execution.ManagedExecution;
 import com.bakdata.conquery.models.externalservice.ResultType;
 import com.bakdata.conquery.models.query.IQuery;
@@ -24,12 +34,16 @@ import com.bakdata.conquery.models.query.QueryPlanContext;
 import com.bakdata.conquery.models.query.QueryResolveContext;
 import com.bakdata.conquery.models.query.Visitable;
 import com.bakdata.conquery.models.query.concept.filter.CQUnfilteredTable;
+import com.bakdata.conquery.models.query.concept.filter.ValidityDateContainer;
 import com.bakdata.conquery.models.query.queryplan.TableExportQueryPlan;
 import com.bakdata.conquery.models.query.queryplan.TableExportQueryPlan.TableExportDescription;
+import com.bakdata.conquery.models.query.resultinfo.ResultInfo;
 import com.bakdata.conquery.models.query.resultinfo.ResultInfoCollector;
 import com.bakdata.conquery.models.query.resultinfo.SimpleResultInfo;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -57,45 +71,40 @@ public class TableExportQuery extends IQuery {
 	@Valid
 	private List<CQUnfilteredTable> tables;
 
+	@InternalOnly
+	@JsonDeserialize(keyUsing = NsIdReferenceKeyDeserializer.class)
+	@JsonSerialize(keyUsing = IdReferenceKeySerializer.class)
+	private Map<Column, Integer> positions;
+
 	@JsonIgnore
-	private List<Column> resolvedHeader;
+	private ResultInfo[] resultInfos = null;
 
 	@Override
 	public TableExportQueryPlan createQueryPlan(QueryPlanContext context) {
-		int totalColumns = 0;
 		List<TableExportDescription> resolvedConnectors = new ArrayList<>();
+
+		//TODO collect column positions, pull validity-dates to the front, collect secondaryIds to the front and merge them
+
 		for (CQUnfilteredTable table : tables) {
 			Connector connector = table.getTable();
-			final Column validityDateColumn;
 
 			// if no dateColumn is provided, we use the default instead which is always the first one.
 			// Set to null if none-available in the connector.
-			if (table.getDateColumn() != null) {
-				validityDateColumn = table.getDateColumn().getValue().getColumn();
-			}
-			else if (!connector.getValidityDates().isEmpty()) {
-				validityDateColumn = connector.getValidityDates().get(0).getColumn();
-			}
-			else {
-				validityDateColumn = null;
-			}
+			final Column validityDateColumn = findValidityDateColumn(connector, table.getDateColumn());
 
 			final TableExportDescription exportDescription = new TableExportDescription(
 					connector.getTable(),
-					validityDateColumn,
-					totalColumns
+					validityDateColumn
 			);
 
 			resolvedConnectors.add(exportDescription);
-
-			totalColumns += connector.getTable().getColumns().length;
 		}
 
 		return new TableExportQueryPlan(
 				query.createQueryPlan(context),
 				CDateRange.of(dateRange),
 				resolvedConnectors,
-				totalColumns
+				positions
 		);
 	}
 
@@ -107,19 +116,91 @@ public class TableExportQuery extends IQuery {
 	@Override
 	public void resolve(QueryResolveContext context) {
 		query.resolve(context);
-		resolvedHeader = new ArrayList<>();
+
+		AtomicInteger currentPosition = new AtomicInteger(1); // First is dates
+
+		positions = new HashMap<>();
+
+		Map<SecondaryIdDescription, Integer> secondaryIdPositions = new HashMap<>();
+
+		// SecondaryIds are grouped from all tables, and at the beginning
+		tables.stream()
+			  .map(cqUnfilteredTable -> cqUnfilteredTable.getTable().getTable().getColumns())
+			  .flatMap(Arrays::stream)
+			  .map(Column::getSecondaryId)
+			  .filter(Objects::nonNull)
+			  .distinct()
+			  .sorted(Comparator.comparing(SecondaryIdDescription::getLabel))
+			  // First position is dates, so
+			  .forEach(secondaryId -> secondaryIdPositions.put(secondaryId, currentPosition.getAndIncrement()));
+
 
 		for (CQUnfilteredTable table : tables) {
 			Connector connector = table.getTable();
+			final Column validityDateColumn = findValidityDateColumn(connector, table.getDateColumn());
 
-			resolvedHeader.addAll(Arrays.asList(connector.getTable().getColumns()));
+			if (validityDateColumn != null) {
+				positions.putIfAbsent(validityDateColumn, 0);
+			}
+
+			for (Column column : connector.getTable().getColumns()) {
+				if(column.getSecondaryId() != null){
+					positions.computeIfAbsent(column, col -> secondaryIdPositions.get(column.getSecondaryId()));
+				}
+				else {
+					positions.computeIfAbsent(column, col -> currentPosition.getAndIncrement());
+				}
+			}
 		}
+
+		resultInfos = new ResultInfo[currentPosition.get()];
+
+		resultInfos[0] = ConqueryConstants.DATES_INFO;
+
+		secondaryIdPositions.forEach((desc, pos) -> resultInfos[pos] = new SimpleResultInfo(desc.getLabel(), ResultType.IdT.INSTANCE));
+
+		for (Map.Entry<Column, Integer> entry : positions.entrySet()) {
+			// 0 Position is date, already covered
+			final int position = entry.getValue();
+
+			if (position == 0) {
+				continue;
+			}
+
+			// SecondaryIds are pulled to the front, already covered.
+			final Column column = entry.getKey();
+
+			if (column.getSecondaryId() != null) {
+				continue;
+			}
+
+			resultInfos[position] = new SimpleResultInfo(column.getTable().getLabel() + " - " + column.getLabel(), ResultType.resolveResultType(column.getType()));
+		}
+	}
+
+	public Column findValidityDateColumn(Connector connector, ValidityDateContainer dateColumn) {
+		// if no dateColumn is provided, we use the default instead which is always the first one.
+		// Set to null if none-available in the connector.
+		if (dateColumn != null) {
+			return dateColumn.getValue().getColumn();
+		}
+
+		if (!connector.getValidityDates().isEmpty()) {
+			return connector.getValidityDates().get(0).getColumn();
+		}
+
+		return null;
 	}
 
 	@Override
 	public void collectResultInfos(ResultInfoCollector collector) {
-		for (Column col : resolvedHeader) {
-			collector.add(new SimpleResultInfo(col.getId().toStringWithoutDataset(), ResultType.resolveResultType(col.getType())));
+
+		if(resultInfos == null){
+			return;
+		}
+
+		for (ResultInfo resultInfo : resultInfos) {
+			collector.add(resultInfo);
 		}
 	}
 
