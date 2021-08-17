d89fd6bcedcc90b002445d1abdca3947976f7d33
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2021-08-17 12:48:21 +0200

    formatting

--- backend/src/main/java/com/bakdata/conquery/models/datasets/concepts/conditions/GroovyCondition.java
+++ backend/src/main/java/com/bakdata/conquery/models/datasets/concepts/conditions/GroovyCondition.java
@@ -23,39 +23,43 @@ import org.codehaus.groovy.control.customizers.ImportCustomizer;
 /**
  * A condition that is a groovy script and thus able to represent everything.
  */
-@Slf4j @CPSType(id="GROOVY", base=CTCondition.class)
+@Slf4j
+@CPSType(id = "GROOVY", base = CTCondition.class)
 public class GroovyCondition implements CTCondition {
 
 	public static final String[] AUTO_IMPORTS = Stream.of(
-		LocalDate.class,
-		Range.class
+			LocalDate.class,
+			Range.class
 	).map(Class::getName).toArray(String[]::new);
 
-	@Getter @Setter @NotEmpty
+	@Getter
+	@Setter
+	@NotEmpty
 	private String script;
 	@JsonIgnore
 	private transient ConditionScript compiled;
 	@JsonIgnore
 	private ConceptTreeNode node;
-	
+
 	@Override
 	public void init(ConceptTreeNode node) throws ConceptConfigurationException {
 		this.node = node;
 		compile();
 	}
-	
+
 	private void compile() throws ConceptConfigurationException {
-		if(compiled==null) {
+		if (compiled == null) {
 			try {
 				CompilerConfiguration config = new CompilerConfiguration();
 				config.addCompilationCustomizers(new ImportCustomizer().addImports(AUTO_IMPORTS));
 				config.setScriptBaseClass(ConditionScript.class.getName());
 				GroovyShell groovy = new GroovyShell(config);
-				
+
 				compiled = (ConditionScript) groovy.parse(script);
 				compiled.setNode(node);
-			} catch(Exception|Error e) {
-				throw new ConceptConfigurationException(node,"Failed to compile condition '"+script+"'",e);
+			}
+			catch (Exception | Error e) {
+				throw new ConceptConfigurationException(node, "Failed to compile condition '" + script + "'", e);
 			}
 		}
 	}
@@ -64,41 +68,44 @@ public class GroovyCondition implements CTCondition {
 	public boolean matches(String value, CalculatedValue<Map<String, Object>> rowMap) throws ConceptConfigurationException {
 		try {
 			return compiled.matches(rowMap.getValue(), value);
-		} catch(Exception e) {
-			throw new ConceptConfigurationException(node, "Could not execute condition \""+script+"\" on "+rowMap.getValue(), e);
+		}
+		catch (Exception e) {
+			throw new ConceptConfigurationException(node, "Could not execute condition \"" + script + "\" on " + rowMap.getValue(), e);
 		}
 	}
 
 
 	public abstract static class ConditionScript extends Script {
-		
+
 		@Getter
 		protected Map<String, Object> row;
 		@Getter
 		protected String value;
 		@Setter
 		protected ConceptTreeNode node;
-		
+
 		public boolean matches(Map<String, Object> row, String value) throws ConceptConfigurationException {
-			this.row=row;
-			this.value=value;
+			this.row = row;
+			this.value = value;
 			Boolean result = run();
-			if(result==null) {
-				throw new ConceptConfigurationException(node, "The condition returned null for input '"+value+"'");
+			if (result == null) {
+				throw new ConceptConfigurationException(node, "The condition returned null for input '" + value + "'");
 			}
 			return result;
 		}
-		
+
 		@Override
 		public abstract Boolean run();
-		
+
 		@Override
 		public Object getProperty(String property) {
-			switch(property) {
-				case "row": return row;
-				case "value": return value;
+			switch (property) {
+				case "row":
+					return row;
+				case "value":
+					return value;
 				default: {
-					log.error("\t\t\tAccessed untypical propert '{}' in condition", property);
+					log.warn("Accessed unknown property `{}` in condition", property);
 					return super.getProperty(property);
 				}
 			}
