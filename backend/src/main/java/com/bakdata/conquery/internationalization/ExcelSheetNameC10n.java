50803d0c35d6c8040867431afe0f58790f903281
Brandon Tietse <Brandon.Tietse@spectrumk.de>
2021-08-23 12:23:33 +0200

    internationalization of sheet name: add missing file

---
+++ backend/src/main/java/com/bakdata/conquery/internationalization/ExcelSheetNameC10n.java
@@ -0,0 +1,10 @@
+package com.bakdata.conquery.internationalization;
+
+import c10n.annotations.De;
+import c10n.annotations.En;
+
+public interface ExcelSheetNameC10n {
+    @En("Result")
+    @De("Ergebnis")
+    String result();
+}
