e439d0216cb1b043e44ab9c690e92afb98d54f76
Brandon Tietse <Brandon.Tietse@spectrumk.de>
2021-08-27 15:02:40 +0200

    update of unit-tests

--- backend/src/test/java/com/bakdata/conquery/models/datasets/concepts/tree/MatchingStatsTests.java
+++ backend/src/test/java/com/bakdata/conquery/models/datasets/concepts/tree/MatchingStatsTests.java
@@ -4,24 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.bakdata.conquery.models.common.daterange.CDateRange;
 import com.bakdata.conquery.models.datasets.Column;
-import com.bakdata.conquery.models.datasets.Dataset;
 import com.bakdata.conquery.models.datasets.Table;
 import com.bakdata.conquery.models.datasets.concepts.MatchingStats;
 import com.bakdata.conquery.models.identifiable.ids.specific.DatasetId;
 import com.bakdata.conquery.models.identifiable.ids.specific.WorkerId;
-import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
-import it.unimi.dsi.fastutil.ints.IntSet;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
-import java.util.Set;
 
 public class MatchingStatsTests {
 
-
+    WorkerId workerId1 = new WorkerId(new DatasetId("sampleDataset"), "sampleWorker");
+    WorkerId workerId2 = new WorkerId(new DatasetId("sampleDataset2"), "sampleWorker2");
     @Test
     public void entitiesCountTest() {
 
@@ -31,14 +26,13 @@ public class MatchingStatsTests {
         assertThat(stats.countEntities()).isEqualTo(0);
 
         Map<WorkerId, MatchingStats.Entry> entries = new HashMap<>();
-        entries.put(new WorkerId(new DatasetId("sampleDataset"), "sampleWorker"), new MatchingStats.Entry(5, 5, CDateRange.of(10, 20)));
+        entries.put(workerId1, new MatchingStats.Entry(5, 5, CDateRange.of(10, 20)));
         stats.setEntries(entries);
         assertThat(stats.countEntities()).isEqualTo(5);
 
-        entries.put(new WorkerId(new DatasetId("sampleDataset"), "sampleWorker"), new MatchingStats.Entry(5, 8, CDateRange.of(10, 20)));
+        entries.put(workerId1, new MatchingStats.Entry(5, 8, CDateRange.of(10, 20)));
         stats.setEntries(entries);
         assertThat(stats.countEntities()).isEqualTo(8);
-
 
 
         entries.put(new WorkerId(new DatasetId("sampleDataset2"), "sampleWorker"), new MatchingStats.Entry(5, 10, CDateRange.of(10, 20)));
@@ -46,7 +40,7 @@ public class MatchingStatsTests {
         assertThat(stats.countEntities()).isEqualTo(18);
 
 
-        entries.put(new WorkerId(new DatasetId("sampleDataset2"), "sampleWorker2"), new MatchingStats.Entry(5, 2, CDateRange.of(10, 20)));
+        entries.put(workerId2, new MatchingStats.Entry(5, 2, CDateRange.of(10, 20)));
         stats.setEntries(entries);
         assertThat(stats.countEntities()).isEqualTo(20);
 
@@ -65,25 +59,27 @@ public class MatchingStatsTests {
         Map<WorkerId, MatchingStats.Entry> entries = new HashMap<>();
 
         MatchingStats.Entry entry1 =  new MatchingStats.Entry();
-        for (int i = 0; i< 3; i++)
+        final int entitiesPerEntry1 = 10;
+        for (int i = 0; i< entitiesPerEntry1; i++)
         {
             entry1.addEvent(table, null, 1, 2+i);
         }
-        entries.put(new WorkerId(new DatasetId("sampleDataset"), "sampleWorker"), entry1);
+        entries.put(workerId1, entry1);
         stats.setEntries(entries);
-        assertThat(stats.countEvents()).isEqualTo(3);
-        assertThat(stats.countEntities()).isEqualTo(3);
+        assertThat(stats.countEvents()).isEqualTo(entitiesPerEntry1);
+        assertThat(stats.countEntities()).isEqualTo(entitiesPerEntry1);
 
 
         MatchingStats.Entry entry2 =  new MatchingStats.Entry();
-        for (int i = 0; i< 4; i++)
+        final int entitiesPerEntry2 = 20;
+        for (int i = 0; i< entitiesPerEntry2; i++)
         {
             entry2.addEvent(table, null, 5, 9+i);
         }
-        entries.put(new WorkerId(new DatasetId("sampleDataset2"), "sampleWorker2"), entry2);
+        entries.put(workerId2, entry2);
         stats.setEntries(entries);
-        assertThat(stats.countEvents()).isEqualTo(7);
-        assertThat(stats.countEntities()).isEqualTo(7);
+        assertThat(stats.countEvents()).isEqualTo(entitiesPerEntry1+entitiesPerEntry2);
+        assertThat(stats.countEntities()).isEqualTo(entitiesPerEntry1+entitiesPerEntry2);
 
 
 
