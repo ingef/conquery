1ec4d908abd463ee9c783f46f6e4fb74e333194f
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2021-09-03 14:45:32 +0200

    also check if we can run forms

--- backend/src/main/java/com/bakdata/conquery/models/messages/namespaces/specific/ExecuteForm.java
+++ backend/src/main/java/com/bakdata/conquery/models/messages/namespaces/specific/ExecuteForm.java
@@ -1,15 +1,19 @@
 package com.bakdata.conquery.models.messages.namespaces.specific;
 
+import static com.bakdata.conquery.models.error.ConqueryError.asConqueryError;
+
 import java.util.Map;
 import java.util.Map.Entry;
 
 import com.bakdata.conquery.apiv1.query.Query;
 import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.models.error.ConqueryError;
 import com.bakdata.conquery.models.identifiable.ids.specific.ManagedExecutionId;
 import com.bakdata.conquery.models.messages.namespaces.NamespacedMessage;
 import com.bakdata.conquery.models.messages.namespaces.WorkerMessage;
 import com.bakdata.conquery.models.query.QueryExecutionContext;
 import com.bakdata.conquery.models.query.QueryExecutor;
+import com.bakdata.conquery.models.query.QueryPlanContext;
 import com.bakdata.conquery.models.query.results.FormShardResult;
 import com.bakdata.conquery.models.query.results.ShardResult;
 import com.bakdata.conquery.models.worker.Worker;
@@ -52,13 +56,26 @@ public class ExecuteForm extends WorkerMessage {
 
 		queryExecutor.unsetQueryCancelled(formId);
 
+
 		// Execute all plans.
 		for (Entry<ManagedExecutionId, Query> entry : queries.entrySet()) {
+			final Query query = entry.getValue();
 			ShardResult result = createResult(worker, entry.getKey());
+
+			// Before we start the query, we create it once to test if it will succeed before creating it multiple times for evaluation per core.
+			try {
+				query.createQueryPlan(new QueryPlanContext(worker));
+			}
+			catch (Exception e) {
+				ConqueryError err = asConqueryError(e);
+				log.warn("Failed to create query plans for {}.", formId, err);
+				queryExecutor.sendFailureToManagerNode(result, err);
+				return;
+			}
 
 			final QueryExecutionContext subQueryContext = new QueryExecutionContext(formId, queryExecutor, worker.getStorage(), worker.getBucketManager());
 
-			if (!queryExecutor.execute(entry.getValue(), subQueryContext, result)) {
+			if (!queryExecutor.execute(query, subQueryContext, result)) {
 				return;
 			}
 		}
