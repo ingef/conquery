18c3778721ea87e1e8aea8dfe6bc7910e2545d5d
Njimefo <34373197+Njimefo@users.noreply.github.com>
2021-10-06 15:05:42 +0200

    Update backend/src/main/java/com/bakdata/conquery/models/preproc/parser/specific/CompoundDateRangeParser.java
    Co-authored-by: awildturtok <1553491+awildturtok@users.noreply.github.com>

--- backend/src/main/java/com/bakdata/conquery/models/preproc/parser/specific/CompoundDateRangeParser.java
+++ backend/src/main/java/com/bakdata/conquery/models/preproc/parser/specific/CompoundDateRangeParser.java
@@ -46,7 +46,6 @@ public class CompoundDateRangeParser extends Parser<CDateRange, DateRangeStore> 
 
 	@Override
 	public void setValue(DateRangeStore store, int event, CDateRange value) {
-		store.setDateRange(event, value);
 	}
 
 	@Override
