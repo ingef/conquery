687e955f42ff52f39be6c975b8dd0ac4c5fb031c
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2021-10-13 14:30:37 +0200

    adds test for aggregated temporal query

--- backend/src/main/java/com/bakdata/conquery/apiv1/query/concept/specific/temporal/CQSampled.java
+++ backend/src/main/java/com/bakdata/conquery/apiv1/query/concept/specific/temporal/CQSampled.java
@@ -4,10 +4,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import com.bakdata.conquery.apiv1.query.CQElement;
-import com.bakdata.conquery.models.query.QueryPlanContext;
-import com.bakdata.conquery.models.query.QueryResolveContext;
-import com.bakdata.conquery.models.query.queryplan.ConceptQueryPlan;
-import com.bakdata.conquery.models.query.queryplan.specific.temporal.SampledNode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,26 +26,5 @@ public class CQSampled {
 	 */
 	@NotNull @Valid
 	private TemporalSampler sampler;
-	
-	/**
-	 * Creates a SampleNode containing the samples and a sub query plan for this part of the query.
-	 * @param ctx the context used to create the query plan
-	 * @return a new SampledNode
-	 */
-	public SampledNode createQueryPlan(QueryPlanContext ctx) {
-		ConceptQueryPlan subPlan = new ConceptQueryPlan(true);
-		subPlan.setChild(child.createQueryPlan(ctx, subPlan));
-		// Since we create the plan manually we have to register the lower date aggregators manually.
-		// Such an aggregator exists because it was enforced in CQAbstractTemporalQuery::resolve on the child.
-		subPlan.getDateAggregator().registerAll(subPlan.getChild().getDateAggregators());
-		return new SampledNode(subPlan, sampler);
-	}
 
-	/**
-	 * @see CQElement#resolve(QueryResolveContext)
-	 */
-	public void resolve(QueryResolveContext context) {
-
-		child.resolve(context);
-	}
 }
