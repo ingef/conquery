95686af26ef6c76107e4aedacbe55697361d594f
Njimefo <34373197+Njimefo@users.noreply.github.com>
2021-12-02 10:01:35 +0100

    Update backend/src/main/java/com/bakdata/conquery/models/datasets/concepts/filters/postalcode/PostalCodeRecord.java
    Co-authored-by: awildturtok <1553491+awildturtok@users.noreply.github.com>

--- backend/src/main/java/com/bakdata/conquery/models/datasets/concepts/filters/postalcode/PostalCodeRecord.java
+++ backend/src/main/java/com/bakdata/conquery/models/datasets/concepts/filters/postalcode/PostalCodeRecord.java
@@ -3,7 +3,7 @@ package com.bakdata.conquery.models.datasets.concepts.filters.postalcode;
 import lombok.Data;
 
 @Data
-public class PostalCodeRecord {
+public class PostalCodeDistance {
 	final private int plz1;
 	final private int plz2;
 	final private double distanceInKm;
