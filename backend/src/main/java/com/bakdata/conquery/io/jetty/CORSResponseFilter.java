4855b54deddcf61a758a3dc86689998c46e56bcb
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2022-01-28 17:29:56 +0100

    adds ETag header to cors allow list

--- backend/src/main/java/com/bakdata/conquery/io/jetty/CORSResponseFilter.java
+++ backend/src/main/java/com/bakdata/conquery/io/jetty/CORSResponseFilter.java
@@ -9,7 +9,7 @@ public class CORSResponseFilter implements ContainerResponseFilter {
 	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext rCtx) {
 		rCtx.getHeaders().add("Access-Control-Allow-Origin", "*");
-		rCtx.getHeaders().add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+		rCtx.getHeaders().add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, ETag");
 		rCtx.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PATCH,DELETE, PUT");
 	}
 
