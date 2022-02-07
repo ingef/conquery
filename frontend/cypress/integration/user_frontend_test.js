5138d6366e559421c00c0cd55c89d32beb26b113
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2022-02-07 12:50:44 +0100

    use prettier

--- frontend/cypress/integration/user_frontend_test.js
+++ frontend/cypress/integration/user_frontend_test.js
@@ -1,82 +1,71 @@
 /// <reference types="cypress" />
 
-context('Visit conquery as users with different permissions', () => {
-    describe('Visit as a authenticated user without permissions', () => {
-        before(() => {        
-            cy.intercept({
-                method: 'GET',
-                url: 'http://localhost:8080/api/config/frontend',
-            }
-            ).as('config')
-            cy.intercept({
-                method: 'GET',
-                url: 'http://localhost:8080/api/datasets',
-            }
-            ).as('datasets')
-            cy.intercept({
-                method: 'GET',
-                url: 'http://localhost:8080/api/me',
-            }
-            ).as('meInfo')
-    
-            cy.visit("http://localhost:3000/?access_token=user.user1")
-    
-            cy.wait(['@config', '@datasets', '@meInfo'])
-        })
+context("Visit conquery as users with different permissions", () => {
+  describe("Visit as a authenticated user without permissions", () => {
+    before(() => {
+      cy.intercept({
+        method: "GET",
+        url: "http://localhost:8080/api/config/frontend",
+      }).as("config");
+      cy.intercept({
+        method: "GET",
+        url: "http://localhost:8080/api/datasets",
+      }).as("datasets");
+      cy.intercept({
+        method: "GET",
+        url: "http://localhost:8080/api/me",
+      }).as("meInfo");
 
-        it('Cannot see concepts', () => {
-            cy.contains("Keine Konzepte")
-        })
+      cy.visit("http://localhost:3000/?access_token=user.user1");
 
+      cy.wait(["@config", "@datasets", "@meInfo"]);
+    });
 
-        it('Cannot see queries', () => {
+    it("Cannot see concepts", () => {
+      cy.contains("Keine Konzepte");
+    });
 
-            cy.get('.Pane1').contains('Anfragen').click()
+    it("Cannot see queries", () => {
+      cy.get(".Pane1").contains("Anfragen").click();
 
+      cy.contains("Keine Anfragen gefunden");
+    });
 
-            cy.contains("Keine Anfragen gefunden")
-        })
+    it("Cannot see forms", () => {
+      cy.get(".Pane1").contains("Formulare").click();
 
-        
-        it('Cannot see forms', () => {
+      cy.contains("Keine Formular-Konfigurationen gefunden");
+    });
+  });
 
-            cy.get('.Pane1').contains('Formulare').click()
+  describe("Visit as a authenticated user with permissions", () => {
+    before(() => {
+      cy.intercept({
+        method: "GET",
+        url: "http://localhost:8080/api/config/frontend",
+      }).as("config");
+      cy.intercept({
+        method: "GET",
+        url: "http://localhost:8080/api/datasets",
+      }).as("datasets");
+      cy.intercept({
+        method: "GET",
+        url: "http://localhost:8080/api/me",
+      }).as("meInfo");
 
+      cy.visit("http://localhost:3000/?access_token=user.user2");
 
-            cy.contains("Keine Formular-Konfigurationen gefunden")
-        })
-    })
+      cy.wait(["@config", "@datasets", "@meInfo"]);
+    });
 
-    describe('Visit as a authenticated user with permissions', () => {
-        before(() => {        
-            cy.intercept({
-                method: 'GET',
-                url: 'http://localhost:8080/api/config/frontend',
-            }
-            ).as('config')
-            cy.intercept({
-                method: 'GET',
-                url: 'http://localhost:8080/api/datasets',
-            }
-            ).as('datasets')
-            cy.intercept({
-                method: 'GET',
-                url: 'http://localhost:8080/api/me',
-            }
-            ).as('meInfo')
-    
-            cy.visit("http://localhost:3000/?access_token=user.user2")
-    
-            cy.wait(['@config', '@datasets', '@meInfo'])
-        })
+    it("Can see dataset", () => {
+      cy.get("#downshift-0-input")
+        .invoke("attr", "value")
+        .should("eq", "Dataset1");
+    });
 
-        
-        it('Can see dataset', () => {
-            cy.get('#downshift-0-input').invoke('attr','value').should('eq',"Dataset1")
-        })
-
-        it('Can see concepts', () => {
-            cy.contains("Concept1")
-        })
-    })
-})
+    it("Can see concepts", () => {
+      cy.contains("Concept1");
+    });
+  });
+});
