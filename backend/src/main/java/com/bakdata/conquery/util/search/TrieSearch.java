72512abdc83d7ea8d67d82802b1725268d8e7c57
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2022-02-21 14:43:53 +0100

    rough sketch of a PatriciaTrie based implementation of search

---
+++ backend/src/main/java/com/bakdata/conquery/util/search/TrieSearch.java
@@ -0,0 +1,45 @@
+package com.bakdata.conquery.util.search;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.google.common.base.Strings;
+import org.apache.commons.collections4.trie.PatriciaTrie;
+
+public class TrieSearch<T> {
+	private static final Pattern SPLIT = Pattern.compile("[\\s()_,:-]+");
+	private final PatriciaTrie<List<T>> trie = new PatriciaTrie<>();
+
+	private Stream<String> split(String keyword) {
+		return SPLIT.splitAsStream(keyword.trim())
+					.map(String::trim)
+					.filter(Predicate.not(Strings::isNullOrEmpty));
+	}
+
+	public List<T> findItems(String keyword) {
+		return split(keyword)
+				.map(trie::prefixMap)
+				.map(Map::values)
+				.flatMap(Collection::stream)
+				.flatMap(List::stream)
+				.collect(Collectors.toList());
+	}
+
+	public void addItem(T item, List<String> keywords) {
+		keywords.stream()
+				.filter(Predicate.not(Strings::isNullOrEmpty))
+				.flatMap(this::split)
+				.forEach(kw -> trie.computeIfAbsent(kw, (ignored) -> new ArrayList<>()).add(item));
+	}
+
+	public Collection<T> listItems() {
+		return trie.values().stream().flatMap(List::stream).collect(Collectors.toList());
+	}
+
+}
--- backend/src/test/java/com/bakdata/conquery/util/search/QuickSearchTest.java
+++ backend/src/test/java/com/bakdata/conquery/util/search/QuickSearchTest.java
@@ -2,6 +2,8 @@ package com.bakdata.conquery.util.search;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -17,8 +19,34 @@ class QuickSearchTest {
 
 		assertThat(quickSearch.listItems())
 				.containsExactly(0, 1, 2);
+	}
 
+	@Test
+	public void insertAndSearch() {
+		final QuickSearch<String> quickSearch = new QuickSearch<>();
 
+		quickSearch.addItem("a b c", "a b c");
+
+		quickSearch.addItem("abc def", "abc def");
+
+		quickSearch.addItem("def ghiabc", "def ghiabc");
+
+		quickSearch.listItems();
+		log.info("{}", quickSearch.findItems("h", 10));
+	}
+
+	@Test
+	public void trieInsertAndSearch() {
+		final TrieSearch<String> quickSearch = new TrieSearch<>();
+
+		quickSearch.addItem("a b c", List.of("a b c"));
+
+		quickSearch.addItem("abc def", List.of("abc def"));
+
+		quickSearch.addItem("def ghiabc", List.of("def ghiabc"));
+
+		quickSearch.listItems();
+		log.info("{}", quickSearch.findItems("d"));
 	}
 
 }
