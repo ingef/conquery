77d57850165310ec8088c2b2c8208737efe32845
Kai Rollmann <kai.rollmann@gmail.com>
2022-02-22 12:17:56 +0100

    Add toPath

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,3 +1,7 @@
+const path = require("path");
+
+const toPath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
