2cf3ee36e46d540d03cbf402d5dd9b50f675f698
Kai Rollmann <kai.rollmann@gmail.com>
2022-02-28 13:19:32 +0100

    Add some preliminary symbols for Query and Form

---
+++ frontend/src/js/symbols/FormSymbol.tsx
@@ -0,0 +1,13 @@
+import FaIcon from "../icon/FaIcon";
+
+import { InABox } from "./InABox";
+
+const FormSymbol = () => {
+  return (
+    <InABox>
+      <FaIcon icon="chart-column" active />
+    </InABox>
+  );
+};
+
+export default FormSymbol;
---
+++ frontend/src/js/symbols/QuerySymbol.tsx
@@ -0,0 +1,12 @@
+import FaIcon from "../icon/FaIcon";
+
+import { InABox } from "./InABox";
+
+const QuerySymbol = () => {
+  return (
+    <InABox>
+      <FaIcon icon="question" active />
+    </InABox>
+  );
+};
+export default QuerySymbol;
