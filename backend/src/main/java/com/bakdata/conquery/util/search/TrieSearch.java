e80348c8155151556fe09a04c34802928c1ba9d9
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2022-03-07 16:36:43 +0100

    even more experiments with Union[T,List[T]]

--- backend/src/main/java/com/bakdata/conquery/util/search/TrieSearch.java
+++ backend/src/main/java/com/bakdata/conquery/util/search/TrieSearch.java
@@ -2,7 +2,6 @@ package com.bakdata.conquery.util.search;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
@@ -27,10 +26,13 @@ public class TrieSearch<T extends Comparable<T>> {
 	 * We saturate matches here to avoid favoring very short keywords, when multiple keywords are used.
 	 */
 	private static final double MATCH_THRESHOLD = 1d / 20d;
-	private static final int SUFFIX_CUTOFF = 3;
+	private static final int SUFFIX_CUTOFF = 1;
 
 	private static final Pattern SPLIT = Pattern.compile("[\\s(),:\"']+"); //TODO FK: Investigate better split patterns
-	private final PatriciaTrie<List<T>> trie = new PatriciaTrie<>();
+	/**
+	 * @implSpec the underlying type of the trie is a union of T or List<T>, please never access it directly.
+	 */
+	private final PatriciaTrie<Object> trie = new PatriciaTrie<>();
 
 	public void clear() {
 		trie.clear();
@@ -49,9 +51,7 @@ public class TrieSearch<T extends Comparable<T>> {
 		return SPLIT.splitAsStream(keyword.trim())
 					.map(String::trim)
 					.filter(StringUtils::isNotBlank)
-					.filter(kw -> kw.length() >= SUFFIX_CUTOFF)
-					.map(String::toLowerCase)
-					.flatMap(this::suffixes);
+					.map(String::toLowerCase);
 	}
 
 	/**
@@ -80,18 +80,21 @@ public class TrieSearch<T extends Comparable<T>> {
 
 		for (String keyword : keywords) {
 			// Query trie for all items associated with extensions of keywords
-			final SortedMap<String, List<T>> hits = trie.prefixMap(keyword);
+			final SortedMap<String, Object> hits = trie.prefixMap(keyword);
 
-			for (Map.Entry<String, List<T>> entry : hits.entrySet()) {
+			for (Map.Entry<String, Object> entry : hits.entrySet()) {
+
 				// calculate and update weights for all queried items
 				final String itemWord = entry.getKey();
 				final double weight = weightWord(keyword, itemWord);
 
-				for (T item : entry.getValue()) {
-					// We combine hits multiplicative to favor items with multiple hits
-					final double currentWeight = itemWeights.getOrDefault(item, 1);
-					itemWeights.put(item, currentWeight * weight);
-				}
+				raise(entry.getValue())
+						.forEach(item ->
+								 {
+									 // We combine hits multiplicative to favor items with multiple hits
+									 final double currentWeight = itemWeights.getOrDefault(item, 1);
+									 itemWeights.put(item, currentWeight * weight);
+								 });
 			}
 		}
 
@@ -108,11 +111,47 @@ public class TrieSearch<T extends Comparable<T>> {
 	public List<T> findExact(Collection<String> keywords, int limit) {
 		return
 				keywords.stream().flatMap(this::split)
-						.map(kw -> trie.getOrDefault(kw, Collections.emptyList()))
-						.flatMap(List::stream)
+						.flatMap(this::doGet)
 						.distinct()
 						.limit(limit)
 						.collect(Collectors.toList());
+	}
+
+	private Stream<T> doGet(String kw) {
+		final Object maybeItems = trie.get(kw);
+
+		return raise(maybeItems);
+	}
+
+	private Stream<T> raise(Object maybeItems) {
+		if (maybeItems == null) {
+			return Stream.empty();
+		}
+
+		if (maybeItems instanceof ArrayList) {
+			return ((ArrayList<T>) maybeItems).stream();
+		}
+
+		return Stream.of(((T) maybeItems));
+	}
+
+	private void doPut(String kw, T item) {
+		trie.compute(kw, (ignored, prior) -> {
+			if (prior instanceof ArrayList) {
+
+				((ArrayList) prior).add(item);
+
+				return prior;
+			}
+			if (prior != null) {
+				final List items = new ArrayList<>(2);
+				items.add(prior);
+				items.add(item);
+
+				return items;
+			}
+			return item;
+		});
 	}
 
 	public void addItem(T item, List<String> keywords) {
@@ -120,17 +159,17 @@ public class TrieSearch<T extends Comparable<T>> {
 		keywords.stream()
 				.filter(Predicate.not(Strings::isNullOrEmpty))
 				.flatMap(this::split)
-				//TODO FK: We could lessen the memory demand a bit by making this a union of List<T> and T, such that items are initially stored directly and only upgraded to lists when needed (although this would complicate ahdnling a tiny bit)
-				.forEach(kw -> trie.computeIfAbsent(kw, (ignored) -> new ArrayList<>()).add(item));
+				.flatMap(this::suffixes)
+				.forEach(kw -> doPut(kw, item));
 	}
 
 	public Iterator<T> iterator() {
-		return trie.keySet().stream().map(trie::get).flatMap(List::stream).distinct().iterator();
+		return trie.keySet().stream().flatMap(this::doGet).distinct().iterator();
 	}
 
 	public Collection<T> listItems() {
 		return trie.values().stream()
-				   .flatMap(List::stream)
+				   .flatMap(this::raise)
 				   .distinct()
 				   .sorted()
 				   .collect(Collectors.toList());
