28ed411829e60d8eddadbe3e247746cfae695000
Kai Rollmann <kai.rollmann@gmail.com>
2022-03-09 17:34:10 +0100

    Adjust scripts paths again

--- scripts/run_backend_cypress.sh
+++ scripts/run_backend_cypress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-jar="../executable/target/executable*.jar"
-config="../frontend/cypress/support/backend_config.json"
+jar="./executable/target/executable*.jar"
+config="./frontend/cypress/support/backend_config.json"
 
 java -jar $jar standalone $config
