664c2a1d660fb6d85429a14ea8f18b25855baf2a
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2022-03-24 13:18:37 +0100

    remove QuickSearch

--- backend/src/test/java/com/bakdata/conquery/util/search/QuickSearchTest.java
+++ backend/src/test/java/com/bakdata/conquery/util/search/QuickSearchTest.java
@@ -9,17 +9,6 @@ import org.junit.jupiter.api.Test;
 
 @Slf4j
 class QuickSearchTest {
-	@Test
-	public void listItems() {
-		final QuickSearch<Integer> quickSearch = new QuickSearch<>();
-
-		quickSearch.addItem(0, "b");
-		quickSearch.addItem(1, "c");
-		quickSearch.addItem(2, "a");
-
-		assertThat(quickSearch.listItems())
-				.containsExactly(0, 1, 2);
-	}
 
 
 	private void fill(TrieSearch<String> search, List<String> items) {
