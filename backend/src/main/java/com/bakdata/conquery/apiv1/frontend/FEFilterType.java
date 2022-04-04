3ed3d9ddaa4f4dcbf17ab40d30bd554596ee45b3
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2022-04-04 11:57:02 +0200

    adds doku

--- backend/src/main/java/com/bakdata/conquery/apiv1/frontend/FEFilterType.java
+++ backend/src/main/java/com/bakdata/conquery/apiv1/frontend/FEFilterType.java
@@ -6,6 +6,9 @@ import lombok.experimental.FieldNameConstants;
 
 /**
  * This class contains the types of filters that the frontend knows.
+ * It is only accessed using the FieldNameConstants when building the frontend configuration and for the deserialization of {@link com.bakdata.conquery.apiv1.query.concept.filter.FilterValue}.
+ * <p>
+ * This way, the filter types may be extended and their deserialization id can be referenced instead of being loosely hardcoded.
  */
 @FieldNameConstants
 @NoArgsConstructor(access = AccessLevel.NONE)
