b5ccee78863cee61ed7b36c8cbf77f7d0f1b4abb
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2022-04-07 17:57:01 +0200

    make ids URL-save

--- backend/src/main/java/com/bakdata/conquery/util/ConqueryEscape.java
+++ backend/src/main/java/com/bakdata/conquery/util/ConqueryEscape.java
@@ -1,105 +1,42 @@
 package com.bakdata.conquery.util;
 
-import java.io.ByteArrayOutputStream;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-import org.apache.commons.lang3.ArrayUtils;
-
+import com.bakdata.conquery.models.identifiable.ids.IId;
 import lombok.NonNull;
+import org.apache.commons.codec.binary.Hex;
 
 
 public class ConqueryEscape {
-	
-	private static final byte ESCAPER = '$';
-	private static final ConqueryEscape INSTANCE = new ConqueryEscape();
 
+	private static final char URL_ESCAPE_CODE_PREFIX = '%';
+	private static final String IID_JOIN_CHAR_URL_ESCAPED;
+
+	static {
+		IID_JOIN_CHAR_URL_ESCAPED = urlEscapeChar(IId.JOIN_CHAR);
+		assert (!IID_JOIN_CHAR_URL_ESCAPED.contains(String.valueOf(IId.JOIN_CHAR)));
+	}
+
+	/**
+	 * We explicitly escape the IId.JOIN_CHAR so that the parts of a serialized ID don't contain it.
+	 */
 	public static String escape(@NonNull String word) {
-		return INSTANCE.escapeString(word);
+		final String encode = URLEncoder.encode(word, StandardCharsets.UTF_8);
+		return encode.replace(String.valueOf(IId.JOIN_CHAR), IID_JOIN_CHAR_URL_ESCAPED);
 	}
-		
-	protected String escapeString(String word) {
-		if(word.isEmpty()) {
-			return word;
-		}
-		
-		byte[] bytes = word.getBytes(StandardCharsets.UTF_8);
-		
-		//if the first does not match we escape everything
-		if(!matchesFirst(bytes[0])) {
-			ByteArrayOutputStream baos = new ByteArrayOutputStream(bytes.length + 5);
-			encode(bytes[0], baos);
-			return escapeRequired(bytes, 1, baos);
-		}
-		
-		//if the first matched we walk through the rest and check if any don't match the allowed characters
-		for(int i=1;i<bytes.length;i++) {
-			if(!matchesOther(bytes[i])) {
-				ByteArrayOutputStream baos = new ByteArrayOutputStream(bytes.length + 5);
-				baos.write(bytes, 0, i);
-				return escapeRequired(bytes, i, baos);
-			}
-		}
-		
-		return word;
-	}
-	
-			
-	private String escapeRequired(byte[] bytes, int index, ByteArrayOutputStream baos) {
-		for(int i=index;i<bytes.length;i++) {
-			if(matchesOther(bytes[i])) {
-				baos.write(bytes[i]);
-			}
-			else {
-				encode(bytes[i], baos);
-			}
-		}
-		return new String(baos.toByteArray(), StandardCharsets.US_ASCII);
-	}
-	
+
 	public static String unescape(@NonNull String word) {
-		return INSTANCE.unescapeString(word);
-	}
-	
-	protected String unescapeString(String word) {
-		if(word.isEmpty()) {
-			return word;
-		}
-		
-		byte[] bytes = word.getBytes(StandardCharsets.US_ASCII);
-		if(!ArrayUtils.contains(bytes, ESCAPER)) {
-			return word;
-		}
-		
-		ByteArrayOutputStream out = new ByteArrayOutputStream(bytes.length);
-		
-		for(int i=0;i<bytes.length;i++) {
-			if(bytes[i] == ESCAPER) {
-				i += decode(bytes, i, out);
-			}
-			else {
-				out.write(bytes[i]);
-			}
-		}
-		
-		return new String(out.toByteArray(), StandardCharsets.UTF_8);
-	}
-	
-	private void encode(byte b, ByteArrayOutputStream out) {
-		out.write(ESCAPER);
-		out.write(Character.forDigit((b >> 4) & 0xF, 16));
-		out.write(Character.forDigit((b & 0xF), 16));
-	}
-	
-	private int decode(byte[] bytes, int i, ByteArrayOutputStream out) {
-		out.write((Character.digit(bytes[i+1], 16) << 4) + Character.digit(bytes[i+2], 16));
-		return 2;
+		return URLDecoder.decode(word, StandardCharsets.UTF_8);
 	}
 
-	protected boolean matchesOther(byte v) {
-		return v>=(byte)'!' && v<=(byte)'~' && v!=ESCAPER && v!=(byte)'.';
-	}
+	private static String urlEscapeChar(char c) {
+		byte[] buf = new byte[2];
+		buf[0] = (byte) ((c & 0xFF00) >> 8);
+		buf[1] = (byte) (c & 0x00FF);
+		final String s = URL_ESCAPE_CODE_PREFIX + Hex.encodeHexString(buf).replaceFirst("^0*", "");
 
-	protected boolean matchesFirst(byte v) {
-		return matchesOther(v);
+		return s;
 	}
 }
