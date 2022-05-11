26b0df354b5abd65921e9f8687d5bca01e677703
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2022-05-11 11:13:04 +0200

    fix EXCEL MediaType to use odf headers

--- backend/src/main/java/com/bakdata/conquery/apiv1/AdditionalMediaTypes.java
+++ backend/src/main/java/com/bakdata/conquery/apiv1/AdditionalMediaTypes.java
@@ -5,7 +5,7 @@ public interface AdditionalMediaTypes {
 	static final String JSON = "application/json; charset=utf-8";
 
 	static final String CSV = "text/csv; charset=utf-8";
-	static final String EXCEL = "application/vnd.ms-excel";
+	static final String EXCEL = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
 	
 	// As proposed in https://issues.apache.org/jira/browse/ARROW-7396
 	static final String ARROW_STREAM = "application/vnd.apache.arrow.stream";
