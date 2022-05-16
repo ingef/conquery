3ba545806039d81d36951750ff25f6eb47cddf4b
Fabian Kovacs <1553491+awildturtok@users.noreply.github.com>
2022-05-16 17:31:40 +0200

    Set creatable=true, when no Searches are available

--- backend/src/main/java/com/bakdata/conquery/apiv1/frontend/FEFilter.java
+++ backend/src/main/java/com/bakdata/conquery/apiv1/frontend/FEFilter.java
@@ -19,19 +19,53 @@ import lombok.Data;
 public class FEFilter {
 
 	private FilterId id;
+	/**
+	 * User readable name of the Filter.
+	 */
 	private String label;
+	/**
+	 * Kind of filter: Communicates to the frontend which UI element to use and what values are valid.
+	 */
 	private String type;
+	/**
+	 * Used as display unit for enumerations etc in UI elements.
+	 */
 	private String unit;
+
 	private String description;
 	@Builder.Default
 	private List<FEValue> options = Collections.emptyList();
+
+	/**
+	 * min value for range selects.
+	 */
 	private Integer min;
+	/**
+	 * max value for range selects.
+	 */
 	private Integer max;
+
+	/**
+	 * TODO Is this used by the frontend?
+	 */
 	private FilterTemplate template;
 	private String pattern;
+	/**
+	 * If true, enables users to use drag and drop files into the filter element (usually for {@link com.bakdata.conquery.models.datasets.concepts.filters.specific.SelectFilter}).
+	 */
 	private Boolean allowDropFile;
+	/**
+	 * If true, user can manually insert their input. At the moment only true for SelectFilter without any enabled backing searches.
+	 */
+	private Boolean creatable;
+	/**
+	 * If set, default value used for the filter by the frontend.
+	 */
 	@Nullable
 	private Object defaultValue;
 
+	/**
+	 * When {@link com.bakdata.conquery.models.datasets.concepts.filters.GroupFilter} is used, the sub-fields of this filter.
+	 */
 	private Map<String, FEFilter> filters;
 }
