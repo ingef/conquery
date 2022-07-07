95c233718d7798a4d2bbf313cfbd9f5eb447002a
awildturtok <1553491+awildturtok@users.noreply.github.com>
2022-07-07 10:34:03 +0200

    Apply missed usages of @View.ApiManagerPersistence

--- backend/src/main/java/com/bakdata/conquery/models/datasets/concepts/select/connector/DistinctSelect.java
+++ backend/src/main/java/com/bakdata/conquery/models/datasets/concepts/select/connector/DistinctSelect.java
@@ -1,8 +1,8 @@
 package com.bakdata.conquery.models.datasets.concepts.select.connector;
 
 import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.io.jackson.View;
 import com.bakdata.conquery.io.jackson.serializer.NsIdRef;
-import com.bakdata.conquery.io.jackson.serializer.SerdesTarget;
 import com.bakdata.conquery.models.datasets.Column;
 import com.bakdata.conquery.models.datasets.concepts.select.Select;
 import com.bakdata.conquery.models.datasets.concepts.select.connector.specific.MappableSingleColumnSelect;
@@ -16,8 +16,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 public class DistinctSelect extends MappableSingleColumnSelect {
 
 	@JsonCreator
-	public DistinctSelect(@NsIdRef Column column,
-						  @NsIdRef(serdesTarget = SerdesTarget.MANAGER) InternToExternMapper mapping) {
+	public DistinctSelect(@NsIdRef Column column, @View.ApiManagerPersistence @NsIdRef InternToExternMapper mapping) {
 		super(column, mapping);
 	}
 
--- backend/src/main/java/com/bakdata/conquery/models/datasets/concepts/select/connector/FirstValueSelect.java
+++ backend/src/main/java/com/bakdata/conquery/models/datasets/concepts/select/connector/FirstValueSelect.java
@@ -1,8 +1,8 @@
 package com.bakdata.conquery.models.datasets.concepts.select.connector;
 
 import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.io.jackson.View;
 import com.bakdata.conquery.io.jackson.serializer.NsIdRef;
-import com.bakdata.conquery.io.jackson.serializer.SerdesTarget;
 import com.bakdata.conquery.models.datasets.Column;
 import com.bakdata.conquery.models.datasets.concepts.select.Select;
 import com.bakdata.conquery.models.datasets.concepts.select.connector.specific.MappableSingleColumnSelect;
@@ -15,8 +15,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 public class FirstValueSelect extends MappableSingleColumnSelect {
 
 	@JsonCreator
-	public FirstValueSelect(@NsIdRef Column column,
-							@NsIdRef(serdesTarget = SerdesTarget.MANAGER) InternToExternMapper mapping) {
+	public FirstValueSelect(@NsIdRef Column column, @View.ApiManagerPersistence InternToExternMapper mapping) {
 		super(column, mapping);
 	}
 
