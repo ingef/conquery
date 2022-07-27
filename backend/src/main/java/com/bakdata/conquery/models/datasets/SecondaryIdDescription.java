065382e16a26f11c4ede9d6f13781e1df59b7676
awildturtok <1553491+awildturtok@users.noreply.github.com>
2022-07-27 17:49:41 +0200

    also remove Searchable from SecondaryIdDescription

--- backend/src/main/java/com/bakdata/conquery/models/datasets/SecondaryIdDescription.java
+++ backend/src/main/java/com/bakdata/conquery/models/datasets/SecondaryIdDescription.java
@@ -1,18 +1,9 @@
 package com.bakdata.conquery.models.datasets;
 
-import java.util.Arrays;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
-
-import com.bakdata.conquery.apiv1.frontend.FEValue;
 import com.bakdata.conquery.io.jackson.serializer.NsIdRef;
-import com.bakdata.conquery.io.storage.NamespaceStorage;
-import com.bakdata.conquery.models.config.CSVConfig;
-import com.bakdata.conquery.models.datasets.concepts.Searchable;
 import com.bakdata.conquery.models.identifiable.Labeled;
 import com.bakdata.conquery.models.identifiable.ids.NamespacedIdentifiable;
 import com.bakdata.conquery.models.identifiable.ids.specific.SecondaryIdDescriptionId;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,39 +11,12 @@ import lombok.Setter;
 @NoArgsConstructor
 @Setter
 @Getter
-//TODO delete searchable from here
-public class SecondaryIdDescription extends Labeled<SecondaryIdDescriptionId> implements NamespacedIdentifiable<SecondaryIdDescriptionId>, Searchable {
+public class SecondaryIdDescription extends Labeled<SecondaryIdDescriptionId> implements NamespacedIdentifiable<SecondaryIdDescriptionId> {
 
 	@NsIdRef
 	private Dataset dataset;
 
 	private String description;
-
-	private int searchMinSuffixLength = 3;
-	private boolean generateSearchSuffixes = true;
-	private boolean searchDisabled = false;
-
-	@Override
-	@JsonIgnore
-	public boolean isGenerateSuffixes() {
-		return generateSearchSuffixes;
-	}
-
-	@Override
-	@JsonIgnore
-	public int getMinSuffixLength() {
-		return searchMinSuffixLength;
-	}
-
-	@Override
-	public Stream<FEValue> getSearchValues(CSVConfig config, NamespaceStorage storage) {
-		return storage.getTables().stream()
-					  .map(Table::getColumns)
-					  .flatMap(Arrays::stream)
-					  .filter(Predicate.not(Column::isSearchDisabled))
-					  .filter(column -> this.equals(column.getSecondaryId()))
-					  .flatMap(column -> column.getSearchValues(config, storage));
-	}
 
 	@Override
 	public SecondaryIdDescriptionId createId() {
