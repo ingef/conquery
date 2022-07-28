aa2b404b0f0826f910e6e1351a17078b6b921140
awildturtok <1553491+awildturtok@users.noreply.github.com>
2022-07-28 12:21:26 +0200

    Fixes usage of tree that needs comparable

--- backend/src/main/java/com/bakdata/conquery/apiv1/FilterSearch.java
+++ backend/src/main/java/com/bakdata/conquery/apiv1/FilterSearch.java
@@ -28,9 +28,9 @@ import com.bakdata.conquery.util.search.TrieSearch;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.base.Functions;
 import com.google.common.collect.Sets;
-import it.unimi.dsi.fastutil.objects.Object2LongAVLTreeMap;
 import it.unimi.dsi.fastutil.objects.Object2LongMap;
 import it.unimi.dsi.fastutil.objects.Object2LongMaps;
+import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.StopWatch;
@@ -171,22 +171,14 @@ public class FilterSearch {
 					log.debug("BEGIN counting Search totals.");
 
 
-					// Precompute totals as that can be extremly slow.
-					totals = new Object2LongAVLTreeMap<>(
+					// Precompute totals as that can be slow when doing it on-demand.
+					totals = new Object2LongOpenHashMap<>(
 							allSelectFilters.parallelStream()
 											.collect(Collectors.toMap(
 													Functions.identity(),
 													filter -> filter.getSearchReferences().stream()
-																	.map(searchable -> {
-																		TrieSearch<FEValue> out = searchCache.get(searchable);
-
-																		if (out == null) {
-																			log.warn("No Search for `{}`", searchable);
-																		}
-
-																		return out;
-																	})
-																	.filter(Objects::nonNull) // Failed searches are null
+																	.map(searchCache::get)
+																	.filter(Objects::nonNull) // Failed or disabled searches are null
 																	.flatMap(TrieSearch::stream)
 																	.mapToInt(FEValue::hashCode)
 																	.distinct()
