e30f3e4b42670350bb32a7d9a5460f1115da32c4
awildturtok <1553491+awildturtok@users.noreply.github.com>
2022-08-16 15:33:33 +0200

    Adapt test for EntityPreviewCard

--- backend/src/main/java/com/bakdata/conquery/models/config/PreviewConfig.java
+++ backend/src/main/java/com/bakdata/conquery/models/config/PreviewConfig.java
@@ -2,11 +2,15 @@ package com.bakdata.conquery.models.config;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Data
 @Slf4j
+@AllArgsConstructor
+@NoArgsConstructor
 public class PreviewConfig {
 	private List<String> infoCardSelects = List.of();
 
