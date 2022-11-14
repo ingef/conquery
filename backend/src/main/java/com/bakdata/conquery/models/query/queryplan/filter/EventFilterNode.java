cf1209420bc410076dfce879dd3f772fea4cdba4
awildturtok <1553491+awildturtok@users.noreply.github.com>
2022-11-14 10:49:48 +0100

    maybe boolean arrays are faster?

--- backend/src/main/java/com/bakdata/conquery/models/query/queryplan/filter/EventFilterNode.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/queryplan/filter/EventFilterNode.java
@@ -1,7 +1,5 @@
 package com.bakdata.conquery.models.query.queryplan.filter;
 
-import java.util.BitSet;
-
 import com.bakdata.conquery.models.events.Bucket;
 import com.bakdata.conquery.models.query.QueryExecutionContext;
 import com.bakdata.conquery.models.query.entity.Entity;
@@ -9,7 +7,7 @@ import com.bakdata.conquery.models.query.entity.Entity;
 public abstract class EventFilterNode<FILTER_VALUE> extends FilterNode<FILTER_VALUE> {
 
 	private boolean hit = false;
-	private BitSet hits = new BitSet(0);
+	private boolean[] hits = null;
 	private int size = -1;
 
 	public EventFilterNode(FILTER_VALUE filterValue) {
@@ -20,19 +18,17 @@ public abstract class EventFilterNode<FILTER_VALUE> extends FilterNode<FILTER_VA
 	public void nextBlock(Bucket bucket) {
 		if (size < bucket.getNumberOfEvents()) {
 			size = bucket.getNumberOfEvents();
-			hits = new BitSet(size);
+			hits = new boolean[size];
 		}
 
-		hits.clear(); // TODO Probably not necessary
-
 		for (int event = 0; event < bucket.getNumberOfEvents(); event++) {
-			hits.set(event, checkEvent(bucket, event));
+			hits[event] = checkEvent(bucket, event);
 		}
 	}
 
 	public final boolean included(Bucket bucket, int event) {
 		//TODO assert Buckets equal
-		return hits.get(event);
+		return hits[event];
 	}
 
 	public abstract boolean checkEvent(Bucket bucket, int event);
