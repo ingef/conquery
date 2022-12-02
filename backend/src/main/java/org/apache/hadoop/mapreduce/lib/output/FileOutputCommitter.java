f9f8fcd64c42247016dcd07d93e396c53ad53c47
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2022-12-02 15:34:09 +0100

    make add-opens a one-liner again
    exclude javax.activation included by keycloak
    
    strip shiro-core dep of unnecessary transitive deps
    
    remove kryo and transitive imported parquet deps
    
    fix missing ignored files
    
    adds exclusions
    
    adds stub disclaimer
    
    remove already included parquet dependencies
    
    stub hadoop dependency
    
    don't minimize
    
    configure excludes and transformers for shade

---
+++ backend/src/main/java/org/apache/hadoop/mapreduce/lib/output/FileOutputCommitter.java
@@ -0,0 +1,9 @@
+package org.apache.hadoop.mapreduce.lib.output;
+
+import org.apache.hadoop.mapreduce.OutputCommitter;
+
+/**
+ * Stub class - see package-info
+ */
+public class FileOutputCommitter extends OutputCommitter {
+}
