f4b89e671d4496593204a4f685803a12171e2eb6
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2023-01-05 09:33:55 +0100

    adds rate limiter to frontend serve

--- frontend/server.js
+++ frontend/server.js
@@ -8,8 +8,11 @@ const compression = require("compression");
 const helmet = require("helmet");
 const cors = require("cors");
 const path = require("path");
+const rateLimit = require('express-rate-limit');
 
 const PORT = process.env.PORT || 8000;
+// Maximum requests per minute
+const requestsPerMinute = process.env.REQUESTS_PER_MINUTE || 50;
 
 app.use(
   helmet({
@@ -19,6 +22,14 @@ app.use(
 app.disable("x-powered-by");
 app.use(compression());
 app.use(cors());
+
+// set up rate limiter: maximum of fifty requests per minute
+app.use(
+  rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: requestsPerMinute
+  })
+);
 
 app.use(express.static(path.resolve(__dirname, "build")));
 app.get("*", (req, res) =>
