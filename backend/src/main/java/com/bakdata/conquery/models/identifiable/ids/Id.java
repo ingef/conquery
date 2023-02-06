4bfbffc7c1aea01fefc12edc1766ca3028ce8978
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2023-02-06 16:32:29 +0100

    cache escaped ids

--- backend/src/main/java/com/bakdata/conquery/models/identifiable/ids/Id.java
+++ backend/src/main/java/com/bakdata/conquery/models/identifiable/ids/Id.java
@@ -1,19 +1,32 @@
 package com.bakdata.conquery.models.identifiable.ids;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import com.bakdata.conquery.io.jackson.serializer.IdDeserializer;
 import com.bakdata.conquery.util.ConqueryEscape;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @JsonDeserialize(using = IdDeserializer.class)
 public abstract class Id<TYPE> {
+
+	private static final LoadingCache<Id<?>, String>
+			ESCAPED_IDS =
+			CacheBuilder.newBuilder().maximumSize(10_000).expireAfterAccess(2, TimeUnit.DAYS).build(new CacheLoader<Id<?>, String>() {
+				@Override
+				public String load(Id<?> key) throws Exception {
+					return key.escapedIdString();
+				}
+			});
 
 	@Override
 	public abstract boolean equals(Object obj);
@@ -24,6 +37,15 @@ public abstract class Id<TYPE> {
 	@Override
 	@JsonValue
 	public String toString() {
+		try {
+			return ESCAPED_IDS.get(this);
+		}
+		catch (ExecutionException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private final String escapedIdString() {
 		List<Object> components = getComponents();
 		components.replaceAll(o -> ConqueryEscape.escape(Objects.toString(o)));
 		return IdUtil.JOINER.join(components);
