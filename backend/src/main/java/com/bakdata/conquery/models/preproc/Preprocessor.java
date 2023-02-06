92c7b1b752d239b19bf64f6fe3cb94465b992c46
awildturtok <1553491+awildturtok@users.noreply.github.com>
2023-02-06 16:06:54 +0100

    Pull up RowProcessor

--- backend/src/main/java/com/bakdata/conquery/models/preproc/Preprocessor.java
+++ backend/src/main/java/com/bakdata/conquery/models/preproc/Preprocessor.java
@@ -7,32 +7,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.function.ToIntFunction;
 import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 
 import com.bakdata.conquery.models.config.CSVConfig;
 import com.bakdata.conquery.models.config.ConqueryConfig;
-import com.bakdata.conquery.models.preproc.outputs.OutputDescription;
-import com.bakdata.conquery.models.preproc.parser.Parser;
-import com.bakdata.conquery.util.DateReader;
 import com.bakdata.conquery.util.io.ConqueryMDC;
 import com.bakdata.conquery.util.io.FileUtil;
 import com.bakdata.conquery.util.io.LogUtil;
 import com.bakdata.conquery.util.io.ProgressBar;
 import com.google.common.base.Strings;
-import com.univocity.parsers.common.ParsingContext;
-import com.univocity.parsers.common.processor.AbstractRowProcessor;
 import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
 import it.unimi.dsi.fastutil.objects.Object2IntArrayMap;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
@@ -109,7 +98,8 @@ public class Preprocessor {
 
 			CsvParser parser = null;
 			final PreprocessingRowProcessor processor =
-					new PreprocessingRowProcessor(input, result, exceptions, progressBar, config.getPreprocessor().getMaximumPrintedErrors(), config.getLocale().getDateReader(), config);
+					new PreprocessingRowProcessor(input, result, exceptions, progressBar, config.getPreprocessor().getMaximumPrintedErrors(), config.getLocale()
+																																					.getDateReader(), config);
 
 			try (InputStream inputStream = new FileInputStream(sourceFile)) {
 
@@ -189,141 +179,5 @@ public class Preprocessor {
 		}
 
 		return csvDirectory.resolve(name + "." + tag.get() + suffix).toFile();
-	}
-
-	/**
-	 * Apply each output for a single row. Returning all resulting values.
-	 */
-	private static Object[] applyOutputs(List<OutputDescription.Output> outputs, PPColumn[] columns, String[] row, long lineId)
-			throws OutputDescription.OutputException {
-		final Object[] outRow = new Object[outputs.size()];
-
-		for (int index = 0; index < outputs.size(); index++) {
-			final OutputDescription.Output out = outputs.get(index);
-
-			try {
-				final Parser parser = columns[index].getParser();
-
-				final Object result = out.createOutput(row, parser, lineId);
-
-				if (result == null) {
-					continue;
-				}
-
-				outRow[index] = result;
-			}
-			catch (Exception e) {
-				throw new OutputDescription.OutputException(out.getDescription(), e);
-			}
-		}
-		return outRow;
-	}
-
-	@Data
-	@RequiredArgsConstructor
-	private static class PreprocessingRowProcessor extends AbstractRowProcessor {
-		private final TableInputDescriptor input;
-		private final Preprocessed result;
-		private final Object2IntMap<Class<? extends Throwable>> exceptions;
-		private final ProgressBar totalProgress;
-		private final int maximumPrintedErrors;
-		private final DateReader dateReader;
-		private final ConqueryConfig config;
-		private GroovyPredicate filter;
-		private OutputDescription.Output primaryOut;
-		private List<OutputDescription.Output> outputs;
-		private long progress;
-		private long errors;
-
-		@Override
-		public void processStarted(ParsingContext context) {
-			final String[] headers = context.selectedHeaders();
-
-			final Object2IntArrayMap<String> headerMap = new Object2IntArrayMap<>(headers.length);
-
-			for (String header : headers) {
-				headerMap.computeIfAbsent(header, (ToIntFunction<String>) context::indexOf);
-			}
-
-			// Compile filter.
-			filter = input.createFilter(headers);
-
-			primaryOut = input.getPrimary().createForHeaders(headerMap, dateReader, config);
-			outputs = new ArrayList<>();
-
-			// Instantiate Outputs based on descriptors (apply header positions)
-			for (OutputDescription op : input.getOutput()) {
-				outputs.add(op.createForHeaders(headerMap, dateReader, config));
-			}
-		}
-
-		@Override
-		public void rowProcessed(String[] row, ParsingContext context) {
-			try {
-				// Check if row shall be evaluated
-				// This is explicitly NOT in a try-catch block as scripts may not fail and we should not recover from faulty scripts.
-				if (filter != null && !filter.filterRow(row)) {
-					return;
-				}
-
-				try {
-					final int primaryId =
-							(int) Objects.requireNonNull(
-									primaryOut.createOutput(row, result.getPrimaryColumn(), context.currentLine()),
-									"primaryId may not be null"
-							);
-
-					final int primary = result.addPrimary(primaryId);
-					final PPColumn[] columns = result.getColumns();
-
-					result.addRow(primary, columns, applyOutputs(outputs, columns, row, context.currentLine()));
-
-				}
-				catch (OutputDescription.OutputException e) {
-					handleOutputException(row, e, context.currentLine());
-				}
-				catch (Exception e) {
-					handleRawException(row, e, context.currentLine());
-				}
-			}
-			finally {
-				//report progress
-				totalProgress.addCurrentValue(1);
-			}
-		}
-
-		/**
-		 * OutputExceptions have more Information on their origin.
-		 */
-		private void handleOutputException(String[] row, OutputDescription.OutputException e, long lineId) {
-			exceptions.put(e.getCause().getClass(), exceptions.getInt(e.getCause().getClass()) + 1);
-
-			errors++;
-
-			if (log.isTraceEnabled() || errors < maximumPrintedErrors) {
-				log.warn("Failed to parse `{}` from line: {} content: {}", e.getSource(), lineId, row, e.getCause());
-			}
-			else if (errors == maximumPrintedErrors) {
-				log.warn("More erroneous lines occurred. Only the first {}", maximumPrintedErrors);
-			}
-		}
-
-		private void handleRawException(String[] row, Exception e, long lineId) {
-			exceptions.put(e.getClass(), exceptions.getInt(e.getClass()) + 1);
-
-			errors++;
-
-			if (log.isTraceEnabled() || errors < maximumPrintedErrors) {
-				log.warn("Failed to parse line: {} content: {}", lineId, row, e);
-			}
-			else if (errors == maximumPrintedErrors) {
-				log.warn("More erroneous lines occurred. Only the first {} were printed", maximumPrintedErrors);
-			}
-		}
-
-		@Override
-		public void processEnded(ParsingContext context) {
-			log.info("Done reading file.");
-		}
 	}
 }
