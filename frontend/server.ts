33da60031f9287c5131317f275692307863f9977
Kai Rollmann <kai.rollmann@gmail.com>
2023-02-21 16:59:41 +0100

    Fix build path

--- frontend/server.ts
+++ frontend/server.ts
@@ -12,7 +12,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const BUILD_FOLDER = "build";
+const BUILD_FOLDER = "dist";
 
 const PORT = process.env.PORT || 8000;
 // Maximum requests per minute
