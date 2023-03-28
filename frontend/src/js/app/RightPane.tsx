c93c1e2bd436fc5561a4e78f47538861d509b768
Kai Rollmann <kai.rollmann@gmail.com>
2023-03-28 15:18:30 +0200

    Simplify RightPane code a bit

--- frontend/src/js/app/RightPane.tsx
+++ frontend/src/js/app/RightPane.tsx
@@ -1,12 +1,11 @@
 import styled from "@emotion/styled";
-import React, { useEffect } from "react";
+import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import FormsTab from "../external-forms/FormsTab";
 import Pane from "../pane/Pane";
-import { clickPaneTab } from "../pane/actions";
-import { TabT } from "../pane/types";
+import { TabNavigationTab } from "../pane/TabNavigation";
 import StandardQueryEditorTab from "../standard-query-editor/StandardQueryEditorTab";
 import TimebasedQueryEditorTab from "../timebased-query-editor/TimebasedQueryEditorTab";
 
@@ -24,58 +23,44 @@ const SxPane = styled(Pane)`
   background-color: ${({ theme }) => theme.col.bgAlt};
 `;
 
-const tabs: TabT[] = [
-  {
-    key: "queryEditor",
-    labelKey: "rightPane.queryEditor",
-    tooltipKey: "help.tabQueryEditor",
-    component: StandardQueryEditorTab,
-  },
-  {
-    key: "timebasedQueryEditor",
-    labelKey: "rightPane.timebasedQueryEditor",
-    tooltipKey: "help.tabTimebasedEditor",
-    component: TimebasedQueryEditorTab,
-  },
-  {
-    key: "externalForms",
-    labelKey: "rightPane.externalForms",
-    tooltipKey: "help.tabFormEditor",
-    component: FormsTab,
-  },
-];
-
 const RightPane = () => {
   const { t } = useTranslation();
-  const dispatch = useDispatch();
   const activeTab = useSelector<StateT, string | null>(
     (state) => state.panes.right.activeTab,
   );
 
-  useEffect(() => {
-    dispatch(clickPaneTab({ paneType: "right", tab: tabs[0].key }));
-  }, [dispatch]);
+  const tabs: TabNavigationTab[] = useMemo(
+    () => [
+      {
+        key: "queryEditor",
+        label: t("rightPane.queryEditor"),
+        tooltip: t("help.tabQueryEditor"),
+      },
+      {
+        key: "timebasedQueryEditor",
+        label: t("rightPane.timebasedQueryEditor"),
+        tooltip: t("help.tabTimebasedEditor"),
+      },
+      {
+        key: "externalForms",
+        label: t("rightPane.externalForms"),
+        tooltip: t("help.tabFormEditor"),
+      },
+    ],
+    [t],
+  );
 
   return (
-    <SxPane
-      right
-      tabs={tabs.map((tab) => ({
-        key: tab.key,
-        label: t(tab.labelKey), // TODO: Somehow make this non-dynamic
-        tooltip: t(tab.tooltipKey), // TODO: Somehow make this non-dynamic
-      }))}
-      dataTestId="right-pane"
-    >
-      {tabs.map((tab) => {
-        const isActive = tab.key === activeTab;
-        const tabComponent = React.createElement(tab.component);
-
-        return (
-          <Tab key={tab.key} isActive={isActive}>
-            {tabComponent}
-          </Tab>
-        );
-      })}
+    <SxPane right tabs={tabs} dataTestId="right-pane">
+      <Tab key={tabs[0].key} isActive={activeTab === tabs[0].key}>
+        <StandardQueryEditorTab />
+      </Tab>
+      <Tab key={tabs[1].key} isActive={activeTab === tabs[1].key}>
+        <TimebasedQueryEditorTab />
+      </Tab>
+      <Tab key={tabs[2].key} isActive={activeTab === tabs[2].key}>
+        <FormsTab />
+      </Tab>
     </SxPane>
   );
 };
