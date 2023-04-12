19b823ec6c12208dbc1e3e37d02969345a8d66d3
awildturtok <1553491+awildturtok@users.noreply.github.com>
2023-04-12 09:43:16 +0200

    adds test for listing all values

--- backend/src/test/java/com/bakdata/conquery/integration/tests/FilterAutocompleteTest.java
+++ backend/src/test/java/com/bakdata/conquery/integration/tests/FilterAutocompleteTest.java
@@ -50,26 +50,25 @@ public class FilterAutocompleteTest extends IntegrationTest.Simple implements Pr
 	@Override
 	public void execute(StandaloneSupport conquery) throws Exception {
 		//read test specification
-		String
-				testJson =
+		final String testJson =
 				In.resource("/tests/query/MULTI_SELECT_DATE_RESTRICTION_OR_CONCEPT_QUERY/MULTI_SELECT_DATE_RESTRICTION_OR_CONCEPT_QUERY.test.json")
 				  .withUTF8()
 				  .readAll();
 
-		DatasetId dataset = conquery.getDataset().getId();
+		final DatasetId dataset = conquery.getDataset().getId();
 
-		ConqueryTestSpec test = JsonIntegrationTest.readJson(dataset, testJson);
+		final ConqueryTestSpec test = JsonIntegrationTest.readJson(dataset, testJson);
 
 		ValidatorHelper.failOnError(log, conquery.getValidator().validate(test));
 
 		test.importRequiredData(conquery);
-		CSVConfig csvConf = conquery.getConfig().getCsv();
+		final CSVConfig csvConf = conquery.getConfig().getCsv();
 
 		conquery.waitUntilWorkDone();
 
-		Concept<?> concept = conquery.getNamespace().getStorage().getAllConcepts().iterator().next();
-		Connector connector = concept.getConnectors().iterator().next();
-		SelectFilter<?> filter = (SelectFilter<?>) connector.getFilters().iterator().next();
+		final Concept<?> concept = conquery.getNamespace().getStorage().getAllConcepts().iterator().next();
+		final Connector connector = concept.getConnectors().iterator().next();
+		final SelectFilter<?> filter = (SelectFilter<?>) connector.getFilters().iterator().next();
 
 		// Copy search csv from resources to tmp folder.
 		final Path tmpCSv = Files.createTempFile("conquery_search", "csv");
@@ -140,6 +139,22 @@ public class FilterAutocompleteTest extends IntegrationTest.Simple implements Pr
 			//check the resolved values
 			assertThat(resolvedFromValues.values().stream().map(FrontendValue::getValue))
 					.containsExactly("f", "fm");
+		}
+
+
+		// Data starting with a is in reference csv
+		{
+			final Response fromCsvResponse = conquery.getClient().target(autocompleteUri)
+													 .request(MediaType.APPLICATION_JSON_TYPE)
+													 .post(Entity.entity(new FilterResource.AutocompleteRequest(
+															 Optional.of(""),
+															 OptionalInt.empty(),
+															 OptionalInt.empty()
+													 ), MediaType.APPLICATION_JSON_TYPE));
+
+			final ConceptsProcessor.AutoCompleteResult resolvedFromCsv = fromCsvResponse.readEntity(ConceptsProcessor.AutoCompleteResult.class);
+			assertThat(resolvedFromCsv.values().stream().map(FrontendValue::getValue))
+					.containsExactly("", "aaa", "a", "baaa", "aab", "b", "f", "fm", "m", "mf");
 		}
 	}
 }
