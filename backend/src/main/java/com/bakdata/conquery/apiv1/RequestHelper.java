13cd7ec9be54c012abf6761e43b38718772548f8
awildturtok <1553491+awildturtok@users.noreply.github.com>
2023-04-18 17:42:23 +0200

    fixes building URI, otherwise we'd have weird IPv6 adresses

--- backend/src/main/java/com/bakdata/conquery/apiv1/RequestHelper.java
+++ backend/src/main/java/com/bakdata/conquery/apiv1/RequestHelper.java
@@ -1,7 +1,6 @@
 package com.bakdata.conquery.apiv1;
 
 import java.net.URI;
-import java.net.URL;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ContainerRequestContext;
@@ -10,6 +9,7 @@ import javax.ws.rs.core.MultivaluedMap;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.client.utils.URIBuilder;
 import org.eclipse.jetty.http.HttpHeader;
 
 @Slf4j
@@ -22,9 +22,12 @@ public class RequestHelper {
 				final String host = req.getHeader(HttpHeader.X_FORWARDED_HOST.asString());
 				final String protocol = req.getHeader(HttpHeader.X_FORWARDED_PROTO.asString());
 
-				log.debug("Proto=`{}` Fwd-Host=`{}`", protocol, host);
+				log.trace("Proto=`{}` Fwd-Host=`{}`", protocol, host);
 
-				return new URL(protocol, host, "").toString();
+				return new URIBuilder()
+						.setHost(host)
+						.setScheme(protocol)
+						.toString();
 			}
 			catch (Exception e) {
 				log.warn("Failed to build response URL from X-Forward headers", e);
@@ -41,13 +44,18 @@ public class RequestHelper {
 	 */
 	public static URI getRequestURL(ContainerRequestContext req) {
 		final MultivaluedMap<String, String> headers = req.getHeaders();
-		if (headers.getFirst(AdditionalHeaders.HTTP_HEADER_REAL_HOST) != null) {
+		if (headers.getFirst(HttpHeader.X_FORWARDED_HOST.asString()) != null) {
 			try {
-				return new URL(
-						headers.getFirst(AdditionalHeaders.HTTP_HEADER_REAL_PROTO),
-						headers.getFirst(AdditionalHeaders.HTTP_HEADER_REAL_HOST),
-						""
-				).toURI();
+
+				final String host = headers.getFirst(HttpHeader.X_FORWARDED_HOST.asString());
+				final String protocol = headers.getFirst(HttpHeader.X_FORWARDED_PROTO.asString());
+
+				log.trace("Proto=`{}` Fwd-Host=`{}`", protocol, host);
+
+				return new URIBuilder()
+						.setHost(host)
+						.setScheme(protocol)
+						.build();
 			}
 			catch (Exception e) {
 				log.warn("Failed to build response URL from X-Forward headers", e);
