d1c38e1309412de5b2d45792d5e0784c336d3849
awildturtok <1553491+awildturtok@users.noreply.github.com>
2023-05-17 16:38:51 +0200

    adds missing JsonCreator to UpdateJobManagerStatus

--- backend/src/main/java/com/bakdata/conquery/models/jobs/JobManagerStatus.java
+++ backend/src/main/java/com/bakdata/conquery/models/jobs/JobManagerStatus.java
@@ -2,7 +2,7 @@ package com.bakdata.conquery.models.jobs;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Collection;
+import java.util.List;
 
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
@@ -10,15 +10,15 @@ import javax.validation.constraints.NotNull;
 import com.bakdata.conquery.models.identifiable.ids.specific.DatasetId;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
 import lombok.With;
 import org.apache.commons.lang3.time.DurationFormatUtils;
 
 
 @Data
-@RequiredArgsConstructor(onConstructor_ = @JsonCreator)
+@AllArgsConstructor(onConstructor_ = @JsonCreator)
 public class JobManagerStatus {
 	@With
 	@Nullable
@@ -30,8 +30,9 @@ public class JobManagerStatus {
 	private final LocalDateTime timestamp;
 	@NotNull
 	@EqualsAndHashCode.Exclude
-	private final Collection<JobStatus> jobs;
-	public JobManagerStatus(String origin, DatasetId dataset, Collection<JobStatus> statuses) {
+	private final List<JobStatus> jobs;
+
+	public JobManagerStatus(String origin, DatasetId dataset, List<JobStatus> statuses) {
 		this(origin, dataset, LocalDateTime.now(), statuses);
 	}
 
--- backend/src/main/java/com/bakdata/conquery/models/messages/network/specific/UpdateJobManagerStatus.java
+++ backend/src/main/java/com/bakdata/conquery/models/messages/network/specific/UpdateJobManagerStatus.java
@@ -8,21 +8,22 @@ import com.bakdata.conquery.models.messages.network.MessageToManagerNode;
 import com.bakdata.conquery.models.messages.network.NetworkMessage;
 import com.bakdata.conquery.models.messages.network.NetworkMessageContext.ManagerNodeNetworkContext;
 import com.bakdata.conquery.models.worker.ShardNodeInformation;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @CPSType(id = "UPDATE_JOB_MANAGER_STATUS", base = NetworkMessage.class)
 @Slf4j
 @Data
+@RequiredArgsConstructor(onConstructor_ = {@JsonCreator})
 public class UpdateJobManagerStatus extends MessageToManagerNode {
 	@NotNull
 	private final JobManagerStatus status;
 
 	@Override
 	public void react(ManagerNodeNetworkContext context) throws Exception {
-		final ShardNodeInformation node = context.getNamespaces()
-												 .getShardNodes()
-												 .get(context.getRemoteAddress());
+		final ShardNodeInformation node = context.getNamespaces().getShardNodes().get(context.getRemoteAddress());
 
 		if (node == null) {
 			log.error("Could not find ShardNode `{}`, I only know of {}", context.getRemoteAddress(), context.getNamespaces().getShardNodes().keySet());
