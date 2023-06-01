f51f526788f0753788fe56cbf44ce431fd5ca0c1
Torben Meyer <torben.meyer@bakdata.com>
2023-06-01 15:31:53 +0200

    Resolve error due to rebase

--- backend/src/test/java/com/bakdata/conquery/integration/sql/SqlStandaloneSupport.java
+++ backend/src/test/java/com/bakdata/conquery/integration/sql/SqlStandaloneSupport.java
@@ -1,21 +1,20 @@
 package com.bakdata.conquery.integration.sql;
 
-import java.util.function.Function;
-
 import javax.validation.Validator;
 
 import com.bakdata.conquery.integration.IntegrationTests;
 import com.bakdata.conquery.io.jackson.Jackson;
-import com.bakdata.conquery.io.jackson.View;
 import com.bakdata.conquery.io.storage.MetaStorage;
 import com.bakdata.conquery.io.storage.NamespaceStorage;
+import com.bakdata.conquery.mode.InternalObjectMapperCreator;
+import com.bakdata.conquery.mode.local.LocalNamespaceHandler;
 import com.bakdata.conquery.models.config.ConqueryConfig;
 import com.bakdata.conquery.models.datasets.Dataset;
 import com.bakdata.conquery.models.worker.DatasetRegistry;
+import com.bakdata.conquery.models.worker.LocalNamespace;
 import com.bakdata.conquery.models.worker.Namespace;
 import com.bakdata.conquery.util.NonPersistentStoreFactory;
 import com.bakdata.conquery.util.support.TestSupport;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.jersey.validation.Validators;
 import lombok.Value;
 
@@ -35,11 +34,15 @@ public class SqlStandaloneSupport implements TestSupport {
 		};
 		storage.openStores(Jackson.MAPPER.copy());
 		storage.updateDataset(dataset);
-		Function<Class<? extends View>, ObjectMapper> mapperFunction = view -> Jackson.MAPPER.copy();
 
-		this.config = IntegrationTests.DEFAULT_CONFIG;
-		this.namespace = Namespace.create(null, storage, IntegrationTests.DEFAULT_CONFIG, mapperFunction);
-		this.metaStorage = new MetaStorage(new NonPersistentStoreFactory(), new DatasetRegistry(2, this.config, mapperFunction));
+		config = IntegrationTests.DEFAULT_CONFIG;
+		InternalObjectMapperCreator mapperCreator = new InternalObjectMapperCreator(config, getValidator());
+		LocalNamespaceHandler localNamespaceHandler = new LocalNamespaceHandler(config, mapperCreator);
+		DatasetRegistry<LocalNamespace> datasetRegistry = new DatasetRegistry<>(2, config, mapperCreator, localNamespaceHandler);
+		metaStorage = new MetaStorage(new NonPersistentStoreFactory(), datasetRegistry);
+		datasetRegistry.setMetaStorage(metaStorage);
+		mapperCreator.init(datasetRegistry);
+		namespace = localNamespaceHandler.createNamespace(storage, metaStorage);
 	}
 
 	@Override
