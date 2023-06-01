cef7ec5abe7f2747475d0c0b576cbed037f789db
awildturtok <1553491+awildturtok@users.noreply.github.com>
2023-06-01 10:01:34 +0200

    adds test for CQYes and implements YesNode, as it needs ALL_IDS_TABLE as marker

--- backend/src/main/java/com/bakdata/conquery/apiv1/query/CQYes.java
+++ backend/src/main/java/com/bakdata/conquery/apiv1/query/CQYes.java
@@ -4,15 +4,17 @@ import java.util.Collections;
 import java.util.List;
 
 import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.models.query.QueryExecutionContext;
 import com.bakdata.conquery.models.query.QueryPlanContext;
 import com.bakdata.conquery.models.query.QueryResolveContext;
+import com.bakdata.conquery.models.query.RequiredEntities;
 import com.bakdata.conquery.models.query.queryplan.ConceptQueryPlan;
 import com.bakdata.conquery.models.query.queryplan.QPNode;
-import com.bakdata.conquery.models.query.queryplan.specific.Leaf;
+import com.bakdata.conquery.models.query.queryplan.specific.Yes;
 import com.bakdata.conquery.models.query.resultinfo.ResultInfo;
 
 @CPSType(id = "YES", base = CQElement.class)
-public class CQYes extends CQElement{
+public class CQYes extends CQElement {
 
 	@Override
 	public void resolve(QueryResolveContext context) {
@@ -21,11 +23,16 @@ public class CQYes extends CQElement{
 
 	@Override
 	public QPNode createQueryPlan(QueryPlanContext context, ConceptQueryPlan plan) {
-		return new Leaf();
+		return new Yes(context.getDataset().getAllIdsTable());
 	}
 
 	@Override
 	public List<ResultInfo> getResultInfos() {
 		return Collections.emptyList();
+	}
+
+	@Override
+	public RequiredEntities collectRequiredEntities(QueryExecutionContext context) {
+		return new RequiredEntities(context.getBucketManager().getEntities().keySet());
 	}
 }
