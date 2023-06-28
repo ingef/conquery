af2f5cd59a95b1e9bb8d312cb24cebdc5beb48ea
awildturtok <1553491+awildturtok@users.noreply.github.com>
2023-06-28 15:45:11 +0200

    validate uniqueness constraint for timeStratifiedInfos globally

--- backend/src/main/java/com/bakdata/conquery/models/datasets/PreviewConfig.java
+++ backend/src/main/java/com/bakdata/conquery/models/datasets/PreviewConfig.java
@@ -1,5 +1,6 @@
 package com.bakdata.conquery.models.datasets;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -114,17 +115,18 @@ public class PreviewConfig {
 	 * Defines a group of selects that will be evaluated per quarter and year in the requested period of the entity-preview.
 	 */
 	public record TimeStratifiedSelects(@NotNull String label, String description, @NotEmpty List<InfoCardSelect> selects){
-		@ValidationMethod(message = "Selects may be referenced only once.")
-		@JsonIgnore
-		public boolean isSelectsUnique() {
-			return selects().stream().map(InfoCardSelect::select).distinct().count() == selects().size();
-		}
+	}
 
-		@ValidationMethod(message = "Labels must be unique.")
-		@JsonIgnore
-		public boolean isLabelsUnique() {
-			return selects().stream().map(InfoCardSelect::label).distinct().count() == selects().size();
-		}
+	@ValidationMethod(message = "Selects may be referenced only once.")
+	@JsonIgnore
+	public boolean isSelectsUnique() {
+		return timeStratifiedSelects.stream().map(TimeStratifiedSelects::selects).flatMap(Collection::stream).map(InfoCardSelect::select).distinct().count() == timeStratifiedSelects.stream().map(TimeStratifiedSelects::selects).flatMap(Collection::stream).count();
+	}
+
+	@ValidationMethod(message = "Labels must be unique.")
+	@JsonIgnore
+	public boolean isLabelsUnique() {
+		return timeStratifiedSelects.stream().map(TimeStratifiedSelects::selects).flatMap(Collection::stream).map(InfoCardSelect::label).distinct().count() == timeStratifiedSelects.stream().map(TimeStratifiedSelects::selects).flatMap(Collection::stream).count();
 	}
 
 	@JsonIgnore
