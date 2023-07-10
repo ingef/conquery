b8fa71aaea1baa7e4c61e926168f1a44a7ce9ec8
Jonas Arnhold <jonas.arnhold@bakdata.com>
2023-07-10 14:57:51 +0200

    Update backend/src/main/java/com/bakdata/conquery/sql/conversion/cqelement/CQOrConverter.java
    Co-authored-by: awildturtok <1553491+awildturtok@users.noreply.github.com>

--- backend/src/main/java/com/bakdata/conquery/sql/conversion/cqelement/CQOrConverter.java
+++ backend/src/main/java/com/bakdata/conquery/sql/conversion/cqelement/CQOrConverter.java
@@ -15,7 +15,7 @@ public class CQOrConverter implements NodeConverter<CQOr> {
 
 	@Override
 	public ConversionContext convert(CQOr orNode, ConversionContext context) {
-		if (orNode.getChildren().size() <= 1) {
+		if (orNode.getChildren().size() == 1) {
 			return context.getNodeConverterService().convert(orNode.getChildren().get(0), context);
 		}
 		return StepJoiner.joinChildren(orNode.getChildren(), context, LogicalOperation.OR);
