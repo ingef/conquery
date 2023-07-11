4fd98313f145f4528da5447f4c3a22a6edd9aa65
Jonas Arnhold <jonas.arnhold@bakdata.com>
2023-07-11 17:23:46 +0200

    Add abstraction for date restriction and validity date date ranges
    Co-authored-by: Torben Meyer <torben.meyer@bakdata.com>

--- backend/src/main/java/com/bakdata/conquery/sql/conversion/query/ConceptQueryConverter.java
+++ backend/src/main/java/com/bakdata/conquery/sql/conversion/query/ConceptQueryConverter.java
@@ -5,8 +5,12 @@ import java.util.List;
 import com.bakdata.conquery.apiv1.query.ConceptQuery;
 import com.bakdata.conquery.sql.conversion.NodeConverter;
 import com.bakdata.conquery.sql.conversion.context.ConversionContext;
+import com.bakdata.conquery.sql.conversion.context.selects.Selects;
 import com.bakdata.conquery.sql.conversion.context.step.QueryStep;
 import com.bakdata.conquery.sql.conversion.context.step.QueryStepTransformer;
+import com.bakdata.conquery.sql.conversion.dialect.SqlFunctionProvider;
+import com.bakdata.conquery.sql.models.ColumnDateRange;
+import org.jooq.Field;
 import org.jooq.Record;
 import org.jooq.Select;
 
@@ -19,6 +23,11 @@ public class ConceptQueryConverter implements NodeConverter<ConceptQuery> {
 	}
 
 	@Override
+	public Class<ConceptQuery> getConversionClass() {
+		return ConceptQuery.class;
+	}
+
+	@Override
 	public ConversionContext convert(ConceptQuery node, ConversionContext context) {
 
 		ConversionContext contextAfterConversion = context.getNodeConverterService()
@@ -27,7 +36,7 @@ public class ConceptQueryConverter implements NodeConverter<ConceptQuery> {
 		QueryStep preFinalStep = contextAfterConversion.getQuerySteps().iterator().next();
 		QueryStep finalStep = QueryStep.builder()
 									   .cteName(null)  // the final QueryStep won't be converted to a CTE
-									   .selects(preFinalStep.getQualifiedSelects())
+									   .selects(this.toFinalSelects(preFinalStep, context))
 									   .fromTable(QueryStep.toTableLike(preFinalStep.getCteName()))
 									   .conditions(preFinalStep.getConditions())
 									   .predecessors(List.of(preFinalStep))
@@ -37,8 +46,22 @@ public class ConceptQueryConverter implements NodeConverter<ConceptQuery> {
 		return context.withFinalQuery(finalQuery);
 	}
 
-	@Override
-	public Class<ConceptQuery> getConversionClass() {
-		return ConceptQuery.class;
+	/**
+	 * @return The final selects containing the final validity date, if present, as a string aggregation field.
+	 */
+	private Selects toFinalSelects(QueryStep preFinalStep, ConversionContext context) {
+
+		Selects finalSelects = preFinalStep.getQualifiedSelects();
+
+		if (finalSelects.getValidityDate().isEmpty()) {
+			return finalSelects;
+		}
+
+		SqlFunctionProvider functionProvider = context.getSqlDialect().getFunction();
+		Field<Object> finalValidityDateSelect = functionProvider.daterangeString(finalSelects.getValidityDate().get())
+																.as("dates");
+
+		return finalSelects.withValidityDate(ColumnDateRange.of(finalValidityDateSelect));
 	}
+
 }
