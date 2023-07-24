28d81772f84f63f10eba154c88dfced5f8be7fa1
awildturtok <1553491+awildturtok@users.noreply.github.com>
2023-07-24 12:34:55 +0200

    test if query is already reset

--- backend/src/main/java/com/bakdata/conquery/models/execution/ManagedExecution.java
+++ backend/src/main/java/com/bakdata/conquery/models/execution/ManagedExecution.java
@@ -401,6 +401,11 @@ public abstract class ManagedExecution extends IdentifiableImpl<ManagedExecution
 	}
 
 	public void reset() {
+		// This avoids endless loops with already reset queries
+		if(getState().equals(ExecutionState.NEW)){
+			return;
+		}
+
 		setState(ExecutionState.NEW);
 
 		namespace.getExecutionManager().clearQueryResults(this);
