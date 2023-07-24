133956b61bc24cacd8cae53ec3ff428b2e3ed3ba
Kai Rollmann <kai.rollmann@gmail.com>
2023-07-24 11:57:07 +0200

    Add empty state, make chart scrollable

--- frontend/src/js/entity-history/TimeStratifiedConceptChart.tsx
+++ frontend/src/js/entity-history/TimeStratifiedConceptChart.tsx
@@ -1,10 +1,13 @@
 import styled from "@emotion/styled";
+import { faBan } from "@fortawesome/free-solid-svg-icons";
+import { useTranslation } from "react-i18next";
 
 import {
   ColumnDescriptionSemanticConceptColumn,
   TimeStratifiedInfo,
 } from "../api/types";
 import { getConceptById } from "../concept-trees/globalTreeStoreHelper";
+import FaIcon from "../icon/FaIcon";
 import WithTooltip from "../tooltip/WithTooltip";
 
 import { ConceptBubble } from "./ConceptBubble";
@@ -12,19 +15,30 @@ import { ConceptBubble } from "./ConceptBubble";
 const Container = styled("div")`
   display: grid;
   place-items: center;
+  max-width: 100%;
+  overflow-x: auto;
   gap: 0 3px;
   padding: 10px;
 `;
 
+const EmptyMsg = styled("p")`
+  font-size: ${({ theme }) => theme.font.md};
+  color: ${({ theme }) => theme.col.gray};
+  margin: 40px 0;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
 const BubbleYes = styled("div")`
-  width: 10px;
-  height: 10px;
+  width: 14px;
+  height: 14px;
   border-radius: ${({ theme }) => theme.borderRadius};
   background-color: ${({ theme }) => theme.col.blueGray};
 `;
 const BubbleNo = styled("div")`
-  width: 10px;
-  height: 10px;
+  width: 14px;
+  height: 14px;
   border-radius: ${({ theme }) => theme.borderRadius};
   background-color: ${({ theme }) => theme.col.grayLight};
 `;
@@ -38,6 +52,7 @@ export const TimeStratifiedConceptChart = ({
 }: {
   timeStratifiedInfo: TimeStratifiedInfo;
 }) => {
+  const { t } = useTranslation();
   const conceptColumn = timeStratifiedInfo.columns.at(-1);
 
   if (!conceptColumn) return null;
@@ -72,6 +87,16 @@ export const TimeStratifiedConceptChart = ({
         }),
     ),
   ];
+
+  if (allValues.length === 0)
+    return (
+      <Container>
+        <EmptyMsg>
+          <FaIcon gray icon={faBan} />
+          {t("history.noData")}
+        </EmptyMsg>
+      </Container>
+    );
 
   return (
     <Container
