7681291fc335867eb2ceebd37eb7b36e423e442d
awildturtok <1553491+awildturtok@users.noreply.github.com>
2023-11-28 12:22:29 +0100

    fix error when empty

--- backend/src/main/java/com/bakdata/conquery/models/query/statistics/NumberColumnStatsCollector.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/statistics/NumberColumnStatsCollector.java
@@ -2,6 +2,7 @@ package com.bakdata.conquery.models.query.statistics;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
@@ -34,15 +35,15 @@ public class NumberColumnStatsCollector<TYPE extends Number & Comparable<TYPE>> 
 
 	private Comparator<TYPE> selectComparator(ResultType resultType) {
 		// The java type system was not made to handle the silliness, sorry.
-		if (resultType instanceof ResultType.IntegerT){
+		if (resultType instanceof ResultType.IntegerT) {
 			return Comparator.comparingInt(Number::intValue);
 		}
 
-		if (resultType instanceof ResultType.NumericT){
+		if (resultType instanceof ResultType.NumericT) {
 			return Comparator.comparingDouble(Number::doubleValue);
 		}
 
-		if (resultType instanceof ResultType.MoneyT){
+		if (resultType instanceof ResultType.MoneyT) {
 			return Comparator.comparingDouble(Number::doubleValue);
 		}
 
@@ -57,7 +58,7 @@ public class NumberColumnStatsCollector<TYPE extends Number & Comparable<TYPE>> 
 		}
 
 		// TODO this feels like a pretty borked abstraction
-		if(getType() instanceof ResultType.MoneyT moneyT){
+		if (getType() instanceof ResultType.MoneyT moneyT) {
 			value = moneyT.readIntermediateValue(getPrintSettings(), value);
 		}
 
@@ -70,20 +71,22 @@ public class NumberColumnStatsCollector<TYPE extends Number & Comparable<TYPE>> 
 
 	@Override
 	public ResultColumnStatistics describe() {
+		// If no real samples were collected, we short-circuit, as Statistics will throw an exception when empty.
+		if (getStatistics().getN() == 0) {
+			return new ColumnDescription(
+					getName(), getLabel(), getDescription(), getType().toString(),
+					getNulls().intValue(), getNulls().intValue(),
+					Double.NaN, Double.NaN, Double.NaN, Double.NaN, Double.NaN, Collections.emptyList()
+			);
+		}
+
 		final double p99 = getStatistics().getPercentile(99d);
 		final double maybeP01 = getStatistics().getPercentile(1d);
 
 		// If min is basically 0, we don't prune for it, as those are usually relevant values.
-		final double p01 = (Math.abs(maybeP01) < 2 * Double.MIN_VALUE) ?  Double.MIN_VALUE : maybeP01;
+		final double p01 = (Math.abs(maybeP01) < 2 * Double.MIN_VALUE) ? Double.MIN_VALUE : maybeP01;
 
-		return new ColumnDescription(getName(), getLabel(), getDescription(), getType().toString(),
-									 (int) (getStatistics().getN() + getNulls().get()),
-									 getNulls().intValue(),
-									 getStatistics().getMean(),
-									 getStatistics().getPercentile(50d /*This is the median.*/),
-									 getStatistics().getStandardDeviation(),
-									 (int) getStatistics().getMin(),
-									 (int) getStatistics().getMax(),
+		return new ColumnDescription(getName(), getLabel(), getDescription(), getType().toString(), (int) (getStatistics().getN() + getNulls().intValue()), getNulls().intValue(), getStatistics().getMean(), getStatistics().getPercentile(50d /*This is the median.*/), getStatistics().getStandardDeviation(), (int) getStatistics().getMin(), (int) getStatistics().getMax(),
 									 // We cull extremes, as that can cause distortions when displayed.
 									 samples.stream().filter(val -> val.doubleValue() >= p01 && val.doubleValue() <= p99).sorted(comparator).toList()
 		);
