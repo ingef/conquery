d6552ded7bb87452e09c990bb3c7b95d07daf5d5
Marco Korinth <marco@bizfactory.tech>
2023-12-13 10:59:27 +0100

    feat: add custom render for types in preview table

--- frontend/src/js/preview-v2/util.ts
+++ frontend/src/js/preview-v2/util.ts
@@ -1,20 +1,39 @@
+import { t } from "i18next";
 import {
   DateStatistics,
   NumberStatistics,
   PreviewStatistics,
   StringStatistics,
 } from "../api/types";
+import { parseDate } from "../common/helpers/dateHelper";
 
-const DIGITS_OF_PRECISION = 3;
-export function formatNumber(num: number): string {
-  if (num > 100) {
-    return num.toFixed(0).replace(".", ",");
+export const NUMBER_TYPES = [
+  "NUMBER",
+  "INTEGER",
+  "REAL",
+  "DECIMAL",
+];
+
+export const NUMBER_STATISTICS_TYPES = [
+  ...NUMBER_TYPES,
+  "MONEY",
+];
+
+export function formatNumber(num: number, precision = 3): string {
+  const precisionMultiplier = 10 * precision;
+  return (
+    Math.round(num * precisionMultiplier) / precisionMultiplier
+  ).toLocaleString("de-de");
+}
+
+export function formatDate(date: string | undefined) {
+  if (date) {
+    return (
+      parseDate(date, "yyyy-MM-dd")?.toLocaleDateString("de-de") ??
+      t("preview.dateError")
+    );
   }
-
-  return num
-    .toPrecision(DIGITS_OF_PRECISION)
-    .toLocaleString()
-    .replace(".", ",");
+  return t("preview.dateError");
 }
 
 export function previewStatsIsStringStats(
