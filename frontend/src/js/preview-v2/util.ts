72881ec5bdc64fa82959b77c68772eb45802f241
Fabian Blank <fabian.blank@bizfactory.tech>
2023-12-14 09:55:31 +0100

    use chart types

--- frontend/src/js/preview-v2/util.ts
+++ frontend/src/js/preview-v2/util.ts
@@ -39,17 +39,17 @@ export function formatDate(date: string | undefined) {
 export function previewStatsIsStringStats(
   stats: PreviewStatistics,
 ): stats is StringStatistics {
-  return stats.type === "STRING";
+  return stats.chart === "HISTO";
 }
 
 export function previewStatsIsNumberStats(
   stats: PreviewStatistics,
 ): stats is NumberStatistics {
-  return "mean" in stats;
+  return stats.chart === "DESCRIPTIVE";
 }
 
 export function previewStatsIsDateStats(
   stats: PreviewStatistics,
 ): stats is DateStatistics {
-  return stats.type === "DATE" || stats.type === "DATE_RANGE";
+  return stats.chart === "DATES";
 }
