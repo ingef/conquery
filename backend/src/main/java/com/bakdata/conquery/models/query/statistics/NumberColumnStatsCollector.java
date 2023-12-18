aa24a29d4c96d8c9d50ea987c2afbc6efc96d7be
awildturtok <1553491+awildturtok@users.noreply.github.com>
2023-12-18 14:50:58 +0100

    create histogram for NumberColumnStatsCollector as well

--- backend/src/main/java/com/bakdata/conquery/models/query/statistics/NumberColumnStatsCollector.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/statistics/NumberColumnStatsCollector.java
@@ -1,16 +1,24 @@
 package com.bakdata.conquery.models.query.statistics;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Formatter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.BooleanSupplier;
 
 import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.models.i18n.I18n;
 import com.bakdata.conquery.models.query.PrintSettings;
 import com.bakdata.conquery.models.types.ResultType;
+import com.dynatrace.dynahist.Histogram;
+import com.dynatrace.dynahist.bin.Bin;
+import com.dynatrace.dynahist.layout.LogLinearLayout;
 import lombok.Getter;
 import lombok.ToString;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
@@ -80,15 +88,39 @@ public class NumberColumnStatsCollector<TYPE extends Number & Comparable<TYPE>> 
 			);
 		}
 
+		//TODO pull from config?
+		final Histogram histogram = Histogram.createDynamic(LogLinearLayout.create(5, 5, getStatistics().getMin(), getStatistics().getMax()));
+
+		Arrays.stream(getStatistics().getValues()).forEach(histogram::addValue);
+
+		final Map<String, Long> bins = new HashMap<>();
+		final Formatter formatter = new Formatter(I18n.LOCALE.get());
+
+
+		for (Bin bin : histogram.nonEmptyBinsAscending()) {
+			//TODO handle under/overflow, although they should not happen, given we are providing min/max properly
+
+			final String binLabel = formatter.format("%f.1f - %f.1f", bin.getLowerBound(), bin.getUpperBound()).toString();
+
+			bins.put(binLabel, bin.getBinCount());
+
+			formatter.flush();
+		}
+
 		final double p99 = getStatistics().getPercentile(99d);
 		final double maybeP01 = getStatistics().getPercentile(1d);
 
 		// If min is basically 0, we don't prune for it, as those are usually relevant values.
 		final double p01 = (Math.abs(maybeP01) < 2 * Double.MIN_VALUE) ? Double.MIN_VALUE : maybeP01;
 
-		return new ColumnDescription(getName(), getLabel(), getDescription(), getType().toString(), (int) (getStatistics().getN() + getNulls().intValue()), getNulls().intValue(), getStatistics().getMean(), getStatistics().getPercentile(50d /*This is the median.*/), getStatistics().getStandardDeviation(), (int) getStatistics().getMin(), (int) getStatistics().getMax(),
-									 // We cull extremes, as that can cause distortions when displayed.
-									 getStatistics().getSum(), samples.stream().filter(val -> val.doubleValue() >= p01 && val.doubleValue() <= p99).sorted(comparator).toList()
+		//TODO properly implement number value histogram.
+//		return new ColumnDescription(getName(), getLabel(), getDescription(), getType().toString(), (int) (getStatistics().getN() + getNulls().intValue()), getNulls().intValue(), getStatistics().getMean(), getStatistics().getPercentile(50d /*This is the median.*/), getStatistics().getStandardDeviation(), (int) getStatistics().getMin(), (int) getStatistics().getMax(),
+//									 // We cull extremes, as that can cause distortions when displayed.
+//									 getStatistics().getSum(), samples.stream().filter(val -> val.doubleValue() >= p01 && val.doubleValue() <= p99).sorted(comparator).toList()
+//		);
+
+		return new StringColumnStatsCollector.ColumnDescription(
+				getName(), getLabel(), getDescription(), bins
 		);
 	}
 
