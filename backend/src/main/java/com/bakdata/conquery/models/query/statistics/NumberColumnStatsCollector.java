845026290f8bd5453d351815b91f0f8c398d8a8e
awildturtok <1553491+awildturtok@users.noreply.github.com>
2024-01-02 17:37:30 +0100

    fix layout for integers, also merge small bins

--- backend/src/main/java/com/bakdata/conquery/models/query/statistics/NumberColumnStatsCollector.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/statistics/NumberColumnStatsCollector.java
@@ -10,6 +10,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.DoubleStream;
 
 import c10n.C10N;
 import com.bakdata.conquery.models.i18n.I18n;
@@ -17,6 +18,7 @@ import com.bakdata.conquery.models.query.PrintSettings;
 import com.bakdata.conquery.models.types.ResultType;
 import com.dynatrace.dynahist.Histogram;
 import com.dynatrace.dynahist.bin.Bin;
+import com.dynatrace.dynahist.layout.CustomLayout;
 import com.dynatrace.dynahist.layout.Layout;
 import com.dynatrace.dynahist.layout.LogLinearLayout;
 import lombok.Getter;
@@ -90,39 +92,36 @@ public class NumberColumnStatsCollector<TYPE extends Number & Comparable<TYPE>> 
 			return new StringColumnStatsCollector.ColumnDescription(getName(), getLabel(), getDescription(), Collections.emptyList(), Collections.emptyMap());
 		}
 
-		final List<StringColumnStatsCollector.ColumnDescription.Entry> bins = createBins();
+		final List<StringColumnStatsCollector.ColumnDescription.Entry> bins = createBins(getStatistics().getN(), 15d);
 		final Map<String, String> extras = getExtras();
 
 		return new StringColumnStatsCollector.ColumnDescription(getName(), getLabel(), getDescription(), bins, extras);
 	}
 
 	@NotNull
-	private List<StringColumnStatsCollector.ColumnDescription.Entry> createBins() {
-		//TODO create logic for integral bins
-		//TODO if we have a long tail of small bins, consider merging them
-
-		Layout layout = LogLinearLayout.create(getStatistics().getStandardDeviation() / 2, 1 / 10d, getStatistics().getMin(), getStatistics().getMax());
-
+	private List<StringColumnStatsCollector.ColumnDescription.Entry> createBins(long total, double expectedBins) {
+		final Layout layout = getLayout();
 
 		final Histogram histogram = Histogram.createDynamic(layout);
 
 		Arrays.stream(getStatistics().getValues()).forEach(histogram::addValue);
 
+		final List<Count> bins = postProcessBins((double) total, expectedBins, histogram);
 
-		final List<StringColumnStatsCollector.ColumnDescription.Entry> bins = new ArrayList<>();
 
-		for (Bin bin : histogram.nonEmptyBinsAscending()) {
-			//TODO Do we need to handle under/overflow?
+		final List<StringColumnStatsCollector.ColumnDescription.Entry> entries = new ArrayList<>();
 
-			final String lower = printValue(bin.getLowerBound());
-			final String upper = printValue(bin.getUpperBound());
+
+		for (Count bin : bins) {
+			final String lower = printValue(bin.min());
+			final String upper = printValue(bin.max());
 
 			final String binLabel = String.format("%s - %s", lower, upper);
 
 
-			bins.add(new StringColumnStatsCollector.ColumnDescription.Entry(binLabel, bin.getBinCount()));
+			entries.add(new StringColumnStatsCollector.ColumnDescription.Entry(binLabel, bin.count()));
 		}
-		return bins;
+		return entries;
 	}
 
 	@NotNull
@@ -149,8 +148,78 @@ public class NumberColumnStatsCollector<TYPE extends Number & Comparable<TYPE>> 
 		return out;
 	}
 
+	@NotNull
+	private Layout getLayout() {
+		if (getType() instanceof ResultType.IntegerT) {
+			return getIntegerLayout();
+		}
+
+		final Layout initial = LogLinearLayout.create(getStatistics().getStandardDeviation() / 2, 1 / 10d, getStatistics().getMin(), getStatistics().getMax());
+
+
+		return initial;
+	}
+
+	/**
+	 * Merge small bins from left to right order
+	 */
+	@NotNull
+	private static List<Count> postProcessBins(double total, double expectedBins, Histogram histogram) {
+		final List<Count> bins = new ArrayList<>();
+
+		Count prior = null;
+
+		for (Bin bin : histogram.nonEmptyBinsDescending()) {
+			final Count current = Count.fromBin(bin);
+
+			if (prior == null) {
+				prior = current;
+				continue;
+			}
+
+			// If the bin is too small, we merge-left
+			if ((double) prior.count() / total <= (1 / expectedBins)) {
+				prior = prior.merge(current);
+				continue;
+			}
+
+			// Only emit bin, if we cannot merge left.
+			bins.add(prior);
+			prior = null;
+		}
+
+		bins.add(prior);
+		return bins;
+	}
+
 	private String printValue(Number value) {
 		return formatter.format(value.doubleValue());
+	}
+
+	private Layout getIntegerLayout() {
+		final int expectedBins = 15;
+
+		final int min = (int) Math.round(getStatistics().getMin());
+		final int max = (int) Math.round(getStatistics().getMin());
+
+		final int nBins = Math.min(1 + min - max, expectedBins);
+
+		final int width = (min - max) / nBins;
+
+
+		return CustomLayout.create(DoubleStream.iterate(min, cur -> cur <= max, cur -> cur + width).toArray());
+	}
+
+	private record Count(double min, double max, long count) {
+
+		public static Count fromBin(Bin bin) {
+			return new Count(bin.getLowerBound(), bin.getUpperBound(), bin.getBinCount());
+		}
+
+		public Count merge(Count other) {
+			return new Count(Math.min(min(), other.min()), Math.max(max(), other.max()), count() + other.count());
+		}
+
 	}
 
 }
