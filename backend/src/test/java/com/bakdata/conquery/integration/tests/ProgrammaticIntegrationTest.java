5123c581153512d7c10a9b3e7025c103fdfecd2c
Jonas Arnhold <jonasarnhold@web.de>
2024-01-04 22:20:06 +0100

    Rework test infrastructure to easily switch between Worker and SQL mode

--- backend/src/test/java/com/bakdata/conquery/integration/tests/ProgrammaticIntegrationTest.java
+++ backend/src/test/java/com/bakdata/conquery/integration/tests/ProgrammaticIntegrationTest.java
@@ -1,7 +1,22 @@
 package com.bakdata.conquery.integration.tests;
 
 import com.bakdata.conquery.integration.IntegrationTest;
+import com.bakdata.conquery.util.support.StandaloneSupport;
 
 public interface ProgrammaticIntegrationTest extends IntegrationTest {
-	
+
+	default boolean isEnabled(StandaloneSupport.Mode mode) {
+		return switch (mode) {
+			case WORKER -> true;
+			case SQL -> isSqlReady();
+		};
+	}
+
+	/**
+	 * @return True if this programmatic test can run in SQL mode.
+	 */
+	default boolean isSqlReady() {
+		return false;
+	}
+
 }
