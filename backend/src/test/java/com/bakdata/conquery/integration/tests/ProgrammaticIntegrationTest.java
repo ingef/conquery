37c4e9142dd5a8c4cb0d7cbe0113def3169eab2f
Jonas Arnhold <jonasarnhold@web.de>
2024-01-08 13:30:17 +0100

    add forModes to enable tests for a Set of modes

--- backend/src/test/java/com/bakdata/conquery/integration/tests/ProgrammaticIntegrationTest.java
+++ backend/src/test/java/com/bakdata/conquery/integration/tests/ProgrammaticIntegrationTest.java
@@ -1,22 +1,19 @@
 package com.bakdata.conquery.integration.tests;
 
+import java.util.Set;
+
 import com.bakdata.conquery.integration.IntegrationTest;
 import com.bakdata.conquery.util.support.StandaloneSupport;
 
 public interface ProgrammaticIntegrationTest extends IntegrationTest {
 
 	default boolean isEnabled(StandaloneSupport.Mode mode) {
-		return switch (mode) {
-			case WORKER -> true;
-			case SQL -> isSqlReady();
-		};
+		return forModes().contains(mode);
 	}
 
-	/**
-	 * @return True if this programmatic test can run in SQL mode.
-	 */
-	default boolean isSqlReady() {
-		return false;
+	default Set<StandaloneSupport.Mode> forModes() {
+		// Worker mode is standard until SQL connector has full feature support
+		return Set.of(StandaloneSupport.Mode.WORKER);
 	}
 
 }
