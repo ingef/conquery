5028400fa0f2da94f4a25fee0dd8ad22aa09bd36
awildturtok <1553491+awildturtok@users.noreply.github.com>
2024-01-22 14:43:08 +0100

    rudimentary tests for histogram

--- backend/src/test/java/com/bakdata/conquery/models/query/statistics/HistogramTest.java
+++ backend/src/test/java/com/bakdata/conquery/models/query/statistics/HistogramTest.java
@@ -1,42 +1,57 @@
 package com.bakdata.conquery.models.query.statistics;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 import java.util.Random;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 class HistogramTest {
 
 	public static final int SEED = 0xD00F;
-	private final int max = 250;
 
 	@Test
 	void plain() {
-		Histogram histogram = Histogram.create(0, max, 15);
+
+		final Histogram histogram = Histogram.create(0, 10, 10);
 
 		final Random random = new Random(SEED);
 
-		for (int val = 0; val < 1000; val++) {
-			histogram.add(random.nextDouble(0, max));
+		for (int it = 0; it < 100; it++) {
+			histogram.add(random.nextDouble(-2, -1));
 		}
 
-		for (int val = 0; val < 100; val++) {
-			histogram.add(random.nextDouble(0, max / 5d));
+
+		for (int it = 0; it < 100; it++) {
+			histogram.add(random.nextDouble(0, 5));
 		}
 
-		for (int val = 0; val < 100; val++) {
-			histogram.add(random.nextDouble(max / 2d, max / 2d + max / 5d));
+		for (int it = 0; it < 100; it++) {
+			histogram.add(random.nextDouble(7, 10));
 		}
 
-		histogram.add(max);
+		for (int it = 0; it < 100; it++) {
+			histogram.add(random.nextDouble(11, 15));
+		}
 
+		final List<Histogram.Node> balanced = histogram.nodes();
 
-		List<Histogram.Node> balanced = histogram.nodes();
-		log.info("{}", balanced);
+		assertThat(balanced).hasSize(8); // gap between 7-8
+
+		final Histogram.Node first = balanced.get(0);
+
+		assertThat(first.getMin()).isLessThanOrEqualTo(-1);
+
+		final Histogram.Node last = balanced.get(7);
+
+		assertThat(last.getMin()).isCloseTo(9d, Offset.offset(0.2d));
+		assertThat(last.getMax()).isGreaterThanOrEqualTo(11);
+
 	}
-
 
 
 }
