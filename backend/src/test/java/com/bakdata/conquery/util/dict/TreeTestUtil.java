24333d678006f5551f035785d2c63d6a5a2b0df4
awildturtok <1553491+awildturtok@users.noreply.github.com>
2024-02-15 11:10:59 +0100

    use ints for sorting

--- backend/src/test/java/com/bakdata/conquery/util/dict/TreeTestUtil.java
+++ backend/src/test/java/com/bakdata/conquery/util/dict/TreeTestUtil.java
@@ -1,22 +1,17 @@
 package com.bakdata.conquery.util.dict;
 
 import java.util.Comparator;
-import java.util.IdentityHashMap;
-import java.util.Map;
 import java.util.Random;
-import java.util.UUID;
+
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 
 public class TreeTestUtil {
 
 	public static <T> Comparator<T> shuffle(Random random) {
-		final Map<Object, UUID> uniqueIds = new IdentityHashMap<>();
+		final Object2IntMap<T> uniqueIds = new Object2IntOpenHashMap<>();
 
-		return Comparator.comparing(e -> uniqueIds.computeIfAbsent(e, ignored -> {
-			final byte[] randomBytes = new byte[16];
-			random.nextBytes(randomBytes);
-
-			return UUID.nameUUIDFromBytes(randomBytes);
-		}));
+		return Comparator.comparing(e -> uniqueIds.computeIfAbsent(e, ignored -> random.nextInt()));
 	}
 
 }
