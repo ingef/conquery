da796b9b724982137a5e97a3cdfcb936593dfab2
awildturtok <1553491+awildturtok@users.noreply.github.com>
2024-02-27 12:26:13 +0100

    calculate descriptions in parallel

--- backend/src/main/java/com/bakdata/conquery/models/query/statistics/ResultStatistics.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/statistics/ResultStatistics.java
@@ -2,96 +2,101 @@ package com.bakdata.conquery.models.query.statistics;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import com.bakdata.conquery.models.common.Range;
 import com.bakdata.conquery.models.common.daterange.CDateRange;
 import com.bakdata.conquery.models.config.ConqueryConfig;
 import com.bakdata.conquery.models.query.ManagedQuery;
 import com.bakdata.conquery.models.query.PrintSettings;
+import com.bakdata.conquery.models.query.SingleTableResult;
 import com.bakdata.conquery.models.query.resultinfo.ResultInfo;
 import com.bakdata.conquery.models.query.resultinfo.UniqueNamer;
 import com.bakdata.conquery.models.query.results.EntityResult;
 import com.bakdata.conquery.models.types.ResultType;
-import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
-import it.unimi.dsi.fastutil.ints.IntSet;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
 
 public record ResultStatistics(int entities, int total, List<ColumnStatsCollector.ResultColumnStatistics> statistics, Range<LocalDate> dateRange) {
+	@SneakyThrows
 	@NotNull
 	public static ResultStatistics collectResultStatistics(ManagedQuery managedQuery, List<ResultInfo> resultInfos, Optional<ResultInfo> dateInfo, int dateIndex, PrintSettings printSettings, UniqueNamer uniqueNamer, ConqueryConfig conqueryConfig) {
 
-		final List<ColumnStatsCollector>
-				statsCollectors =
-				resultInfos.stream()
-						   .map(info -> ColumnStatsCollector.getStatsCollector(info, printSettings, info.getType(), uniqueNamer, conqueryConfig.getFrontend()))
-						   .collect(Collectors.toList());
+		final ListeningExecutorService executorService = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(5));
 
-		final IntSet entities = new IntOpenHashSet();
-		final AtomicInteger lines = new AtomicInteger();
+		// Yes, we are actually iterating the result for every job.
 
+		// Span date-column
+		final ListenableFuture<Range<LocalDate>> futureSpan = executorService.submit(() -> calculateDateSpan(managedQuery, dateInfo, dateIndex));
+
+		// Count result lines (may differ in case of form or SecondaryIdQuery)
+		final ListenableFuture<Integer> futureLines = executorService.submit(() -> managedQuery.streamResults().mapToInt(result -> result.listResultLines().size()).sum());
+
+		// compute ResultColumnStatistics for each column
+		final List<ListenableFuture<ColumnStatsCollector.ResultColumnStatistics>>
+				futureDescriptions =
+				IntStream.range(0, resultInfos.size()).mapToObj(col -> (Callable<ColumnStatsCollector.ResultColumnStatistics>) () -> {
+					final ResultInfo info = resultInfos.get(col);
+					final ColumnStatsCollector
+							statsCollector =
+							ColumnStatsCollector.getStatsCollector(info, printSettings, info.getType(), uniqueNamer, conqueryConfig.getFrontend());
+
+					managedQuery.streamResults().map(EntityResult::listResultLines).flatMap(List::stream).forEach(line -> statsCollector.consume(line[col]));
+
+					return statsCollector.describe();
+				})
+						 .map(executorService::submit)
+						 .toList();
+
+		final Range<LocalDate> span = futureSpan.get();
+		final List<ColumnStatsCollector.ResultColumnStatistics> descriptions = Futures.allAsList(futureDescriptions).get();
+		final int lines = futureLines.get();
+		return new ResultStatistics(managedQuery.getLastResultCount().intValue(), lines, descriptions, span);
+	}
+
+	private static Range<LocalDate> calculateDateSpan(SingleTableResult managedQuery, Optional<ResultInfo> dateInfo, int dateIndex) {
+		if (dateInfo.isEmpty()) {
+			return CDateRange.all().toSimpleRange();
+		}
 
 		final AtomicReference<CDateRange> spanRef = new AtomicReference<>(null);
-		final Consumer<Object[]> dateAggregator = getDateSpanner(dateInfo, dateIndex, spanRef);
+		final Consumer<Object[]> dateAggregator = getDateSpanner(dateInfo.get(), dateIndex, spanRef);
 
-		//TODO This might be really slow, we can handle this in parallel - thread per statsCollector - quite well
-		managedQuery.streamResults()
-					.peek(result -> entities.add(result.getEntityId()))
-					.map(EntityResult::listResultLines)
-					.flatMap(List::stream)
-					.forEach(line -> {
+		managedQuery.streamResults().flatMap(EntityResult::streamValues).forEach(dateAggregator);
 
-						dateAggregator.accept(line);
+		final CDateRange span = spanRef.get();
 
-						lines.incrementAndGet();
+		if (span == null) {
+			return CDateRange.all().toSimpleRange();
+		}
 
-						for (int col = 0; col < line.length; col++) {
-							final ColumnStatsCollector collector = statsCollectors.get(col);
-							if (collector == null) {
-								continue;
-							}
-
-							collector.consume(line[col]);
-						}
-					});
-
-		final List<ColumnStatsCollector.ResultColumnStatistics> columnStats = statsCollectors.stream()
-																							 .filter(Objects::nonNull) // Not all columns produces stats
-																							 .map(ColumnStatsCollector::describe)
-																							 .toList();
-
-
-		final Range<LocalDate> span = dateInfo.map(ignored -> spanRef.get().toSimpleRange()).orElse(CDateRange.all().toSimpleRange());
-		return new ResultStatistics(entities.size(), lines.get(), columnStats, span);
+		return span.toSimpleRange();
 	}
 
 	/**
 	 * If not dateInfo is given, don't try to span values. otherwise takes values from line at dateIndex, and handles them according to dateInfo.
 	 */
-	private static Consumer<Object[]> getDateSpanner(Optional<ResultInfo> dateInfo, int dateIndex, AtomicReference<CDateRange> spanRef) {
-		if (dateInfo.isEmpty()) {
-			return ignored -> {
-			};
-		}
-
-		final ResultInfo info = dateInfo.get();
+	private static Consumer<Object[]> getDateSpanner(ResultInfo dateInfo, int dateIndex, AtomicReference<CDateRange> spanRef) {
 
 		final Consumer<CDateRange> spanner = date -> spanRef.getAndAccumulate(date, (old, incoming) -> incoming.spanClosed(old));
 
-		final BiConsumer<Object, Consumer<CDateRange>> extractor = validityDateAggregator(info.getType());
-
+		final BiConsumer<Object, Consumer<CDateRange>> extractor = validityDateExtractor(dateInfo.getType());
 
 		return line -> extractor.accept(line[dateIndex], spanner);
 
 	}
 
-	public static BiConsumer<Object, Consumer<CDateRange>> validityDateAggregator(ResultType dateType) {
+	public static BiConsumer<Object, Consumer<CDateRange>> validityDateExtractor(ResultType dateType) {
 		if (dateType instanceof ResultType.DateRangeT) {
 			return (obj, con) -> con.accept(CDateRange.fromList((List<? extends Number>) obj));
 		}
@@ -102,7 +107,7 @@ public record ResultStatistics(int entities, int total, List<ColumnStatsCollecto
 		}
 
 		if (dateType instanceof ResultType.ListT listT) {
-			final BiConsumer<Object, Consumer<CDateRange>> extractor = validityDateAggregator(listT.getElementType());
+			final BiConsumer<Object, Consumer<CDateRange>> extractor = validityDateExtractor(listT.getElementType());
 			return (obj, con) -> ((List<?>) obj).forEach(date -> extractor.accept(date, con));
 		}
 
