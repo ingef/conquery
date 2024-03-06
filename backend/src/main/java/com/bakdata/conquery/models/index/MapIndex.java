20f50206921f138d21ba9539873a1e35ab5de44c
awildturtok <1553491+awildturtok@users.noreply.github.com>
2024-03-06 13:33:03 +0100

    forfeit inheritance toward delegation

--- backend/src/main/java/com/bakdata/conquery/models/index/MapIndex.java
+++ backend/src/main/java/com/bakdata/conquery/models/index/MapIndex.java
@@ -8,20 +8,31 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
-public class MapIndex extends HashMap<String, String> implements Index<MapIndexKey> {
+public class MapIndex implements Index<MapIndexKey> {
 
 	private final String externalTemplate;
 
+	private final HashMap<String, String> mappings = new HashMap<>();
+
 	@Override
 	public void put(String key, Map<String, String> templateToConcrete) {
-		if (containsKey(key)) {
+		if (mappings.containsKey(key)) {
 			throw new IllegalArgumentException("The key '" + key + "' already exists in the index. Cannot map '" + key + "' -> '" + templateToConcrete + "'.");
 		}
-		super.put(key, templateToConcrete.get(externalTemplate));
+		mappings.put(key, templateToConcrete.get(externalTemplate));
+	}
+
+	@Override
+	public int size() {
+		return mappings.size();
 	}
 
 	@Override
 	public void finalizer() {
 		// Nothing to finalize
+	}
+
+	public String get(String internalValue, String defaultValue) {
+		return mappings.getOrDefault(internalValue, defaultValue);
 	}
 }
