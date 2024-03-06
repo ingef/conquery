e820b9e4532c17bbf00ced36dab7e8baa15dbfeb
awildturtok <1553491+awildturtok@users.noreply.github.com>
2024-03-06 16:36:13 +0100

    cleanup file

--- backend/src/main/java/com/bakdata/conquery/models/auth/oidc/JwtPkceVerifyingRealm.java
+++ backend/src/main/java/com/bakdata/conquery/models/auth/oidc/JwtPkceVerifyingRealm.java
@@ -13,6 +13,7 @@ import com.bakdata.conquery.models.auth.entities.User;
 import com.bakdata.conquery.models.auth.util.SkippingCredentialsMatcher;
 import com.bakdata.conquery.models.config.auth.JwtPkceVerifyingRealmFactory;
 import com.bakdata.conquery.models.identifiable.ids.specific.UserId;
+import lombok.Data;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authc.AuthenticationException;
@@ -33,47 +34,48 @@ import org.keycloak.representations.JsonWebToken;
  * the authenticated user from it.
  */
 @Slf4j
+@Data
 public class JwtPkceVerifyingRealm extends AuthenticatingRealm implements ConqueryAuthenticationRealm {
 
 	private static final Class<? extends AuthenticationToken> TOKEN_CLASS = BearerToken.class;
-
-	Supplier<Optional<JwtPkceVerifyingRealmFactory.IdpConfiguration>> idpConfigurationSupplier;
 	private final String[] allowedAudience;
 	private final TokenVerifier.Predicate<JsonWebToken>[] tokenChecks;
 	private final List<String> alternativeIdClaims;
 	private final ActiveWithLeewayVerifier activeVerifier;
 	private final MetaStorage storage;
+	Supplier<Optional<JwtPkceVerifyingRealmFactory.IdpConfiguration>> idpConfigurationSupplier;
 
-	public JwtPkceVerifyingRealm(@NonNull Supplier<Optional<JwtPkceVerifyingRealmFactory.IdpConfiguration>> idpConfigurationSupplier,
-								 @NonNull String allowedAudience,
-								 List<TokenVerifier.Predicate<AccessToken>> additionalTokenChecks,
-								 List<String> alternativeIdClaims,
-								 MetaStorage storage,
-								 int tokenLeeway) {
+	public JwtPkceVerifyingRealm(@NonNull Supplier<Optional<JwtPkceVerifyingRealmFactory.IdpConfiguration>> idpConfigurationSupplier, @NonNull String allowedAudience, List<TokenVerifier.Predicate<AccessToken>> additionalTokenChecks, List<String> alternativeIdClaims, MetaStorage storage, int tokenLeeway) {
 		this.storage = storage;
 		this.idpConfigurationSupplier = idpConfigurationSupplier;
 		this.allowedAudience = new String[]{allowedAudience};
-		this.tokenChecks = additionalTokenChecks.toArray((TokenVerifier.Predicate<JsonWebToken>[]) Array.newInstance(TokenVerifier.Predicate.class, 0));
+		tokenChecks = additionalTokenChecks.toArray((TokenVerifier.Predicate<JsonWebToken>[]) Array.newInstance(TokenVerifier.Predicate.class, 0));
 		this.alternativeIdClaims = alternativeIdClaims;
-		this.setCredentialsMatcher(SkippingCredentialsMatcher.INSTANCE);
-		this.setAuthenticationTokenClass(TOKEN_CLASS);
-		this.activeVerifier = new ActiveWithLeewayVerifier(tokenLeeway);
+		setCredentialsMatcher(SkippingCredentialsMatcher.INSTANCE);
+		setAuthenticationTokenClass(TOKEN_CLASS);
+		activeVerifier = new ActiveWithLeewayVerifier(tokenLeeway);
 	}
 
 
 	@Override
 	public ConqueryAuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-		Optional<JwtPkceVerifyingRealmFactory.IdpConfiguration> idpConfigurationOpt = idpConfigurationSupplier.get();
+
+		final Optional<JwtPkceVerifyingRealmFactory.IdpConfiguration> idpConfigurationOpt = idpConfigurationSupplier.get();
+
 		if (idpConfigurationOpt.isEmpty()) {
 			log.warn("Unable to start authentication, because idp configuration is not available.");
 			return null;
 		}
-		JwtPkceVerifyingRealmFactory.IdpConfiguration idpConfiguration = idpConfigurationOpt.get();
+
+		final JwtPkceVerifyingRealmFactory.IdpConfiguration idpConfiguration = idpConfigurationOpt.get();
 		final BearerToken bearerToken = (BearerToken) token;
 
 		log.trace("Parsing token ({}) to extract key id from header", bearerToken.getToken());
+
 		final String keyId = JOSEParser.parse(bearerToken.getToken()).getHeader().getKeyId();
+
 		log.trace("Key id of token signer: {}", keyId);
+
 		final PublicKey publicKey = idpConfiguration.signingKeys().get(keyId);
 
 		if (publicKey == null) {
@@ -81,13 +83,16 @@ public class JwtPkceVerifyingRealm extends AuthenticatingRealm implements Conque
 		}
 
 		log.trace("Creating token verifier");
-		TokenVerifier<AccessToken> verifier = TokenVerifier.create(bearerToken.getToken(), AccessToken.class)
-														   .withChecks(new TokenVerifier.RealmUrlCheck(idpConfiguration.issuer()), TokenVerifier.SUBJECT_EXISTS_CHECK, activeVerifier)
-														   .withChecks(tokenChecks)
-														   .publicKey(publicKey)
-														   .audience(allowedAudience);
+		final TokenVerifier<AccessToken>
+				verifier =
+				TokenVerifier.create(bearerToken.getToken(), AccessToken.class)
+							 .withChecks(new TokenVerifier.RealmUrlCheck(idpConfiguration.issuer()), TokenVerifier.SUBJECT_EXISTS_CHECK, activeVerifier)
+							 .withChecks(tokenChecks)
+							 .publicKey(publicKey)
+							 .audience(allowedAudience);
 
 		log.trace("Verifying token");
+
 		final AccessToken accessToken;
 		try {
 			verifier.verify();
@@ -97,6 +102,7 @@ public class JwtPkceVerifyingRealm extends AuthenticatingRealm implements Conque
 			log.trace("Verification failed", e);
 			throw new IncorrectCredentialsException(e);
 		}
+
 		final String subject = accessToken.getSubject();
 
 		if (subject == null) {
@@ -109,6 +115,7 @@ public class JwtPkceVerifyingRealm extends AuthenticatingRealm implements Conque
 
 		UserId userId = new UserId(subject);
 		User user = storage.getUser(userId);
+
 		if (user != null) {
 			log.trace("Successfully authenticated user {}", userId);
 			return new ConqueryAuthenticationInfo(user, token, this, true);
@@ -116,13 +123,17 @@ public class JwtPkceVerifyingRealm extends AuthenticatingRealm implements Conque
 
 		// Try alternative ids
 		for (String alternativeIdClaim : alternativeIdClaims) {
-			Object altId = accessToken.getOtherClaims().get(alternativeIdClaim);
+
+			final Object altId = accessToken.getOtherClaims().get(alternativeIdClaim);
+
 			if (!(altId instanceof String)) {
 				log.trace("Found no value for alternative id claim {}", alternativeIdClaim);
 				continue;
 			}
+
 			userId = new UserId((String) altId);
 			user = storage.getUser(userId);
+
 			if (user != null) {
 				log.trace("Successfully mapped subject {} using user id {}", subject, userId);
 				return new ConqueryAuthenticationInfo(user, token, this, true);
