d6e85687510d9cc950fa2bb4fa3ad16a03165799
Kai Rollmann <kai.rollmann@gmail.com>
2024-03-18 12:54:19 +0100

    Fix types

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -24,11 +24,11 @@ export default defineConfig({
     open: true,
   },
   define: {
-    __BUILD_GIT_DESCRIBE__: fs.existsSync("./git_describe.txt")
-      ? fs.readFileSync("./git_describe.txt", "utf-8").trim()
-      : "__BUILD_GIT_DESCRIBE__",
     __BUILD_TIMESTAMP__: JSON.stringify(
       new Date().toISOString().split(".")[0].split("T").join(" "),
     ),
+    __BUILD_GIT_DESCRIBE__: fs.existsSync("./git_describe.txt")
+      ? fs.readFileSync("./git_describe.txt", "utf-8").trim()
+      : '"__BUILD_GIT_DESCRIBE__"',
   },
 });
