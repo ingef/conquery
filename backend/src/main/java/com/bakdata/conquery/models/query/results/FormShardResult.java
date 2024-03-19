740ac3838bec39f26b2939165b8c0ae7adbda4a0
awildturtok <1553491+awildturtok@users.noreply.github.com>
2024-03-19 15:36:55 +0100

    inline CollectQueryResult into ShardResults

--- backend/src/main/java/com/bakdata/conquery/models/query/results/FormShardResult.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/results/FormShardResult.java
@@ -5,12 +5,13 @@ import com.bakdata.conquery.models.execution.ExecutionState;
 import com.bakdata.conquery.models.forms.managed.ManagedInternalForm;
 import com.bakdata.conquery.models.identifiable.ids.specific.ManagedExecutionId;
 import com.bakdata.conquery.models.identifiable.ids.specific.WorkerId;
+import com.bakdata.conquery.models.messages.namespaces.NamespacedMessage;
 import com.bakdata.conquery.models.query.DistributedExecutionManager;
 import com.bakdata.conquery.models.query.ManagedQuery;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
-@CPSType(id = "FORM_SHARD_RESULT", base = ShardResult.class)
+@CPSType(id = "FORM_SHARD_RESULT", base = NamespacedMessage.class)
 @EqualsAndHashCode(callSuper = true)
 @Getter
 public class FormShardResult extends ShardResult {
