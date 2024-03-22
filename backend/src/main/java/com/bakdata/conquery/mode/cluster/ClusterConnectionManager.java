aa214f8c39d229f59d8794298201b71064697c53
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2024-03-22 17:08:02 +0100

    close all managed sessions
    Signed-off-by: Max Thonagel <12283268+thoniTUB@users.noreply.github.com>

--- backend/src/main/java/com/bakdata/conquery/mode/cluster/ClusterConnectionManager.java
+++ backend/src/main/java/com/bakdata/conquery/mode/cluster/ClusterConnectionManager.java
@@ -115,6 +115,10 @@ public class ClusterConnectionManager extends IoHandlerAdapter {
 		clusterState.getShardNodes().forEach(((socketAddress, shardNodeInformation) -> shardNodeInformation.send(new ShutdownShard())));
 
 		try {
+			for (IoSession value : acceptor.getManagedSessions().values()) {
+				log.info("Closing session: {}", value);
+				value.closeNow().awaitUninterruptibly();
+			}
 			log.info("Disposing NioSocketAcceptor: {}", acceptor);
 			acceptor.dispose(true);
 			log.info("Disposed NioSocketAcceptor: {}", acceptor);
