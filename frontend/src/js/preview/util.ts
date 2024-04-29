460b37020f8174764980cf2d640112127b21ca33
Fabian Kovacs <Fabian.Kovacs@Ingef.de>
2024-04-29 12:39:52 +0200

    fix calculation using quarters where none are provided

--- frontend/src/js/preview/util.ts
+++ frontend/src/js/preview/util.ts
@@ -40,24 +40,24 @@ export function useDateTickHandler(stats: PreviewStatistics) {
   let shouldTickRender: (date: Date) => boolean = () => false;
 
   if (previewStatsIsDateStats(stats)) {
-    const quarterCount = Object.keys(stats.quarterCounts).length;
+    const monthCount = Object.keys(stats.monthCounts).length;
 
     // default -> months
     shouldTickRender = () => true;
     // > 12 months -> quarters
-    if (quarterCount > 4) {
+    if (monthCount > 12) {
       shouldTickRender = (date: Date) => date.getMonth() % 3 === 0;
     }
     // > 12 quarters -> halfyears
-    if (quarterCount > 12) {
+    if (monthCount > 4 * 12) {
       shouldTickRender = (date: Date) => date.getMonth() % 6 === 0;
     }
     // > 12 halfyears -> years
-    if (quarterCount > 24) {
+    if (monthCount > 6 * 12) {
       shouldTickRender = (date: Date) => date.getMonth() === 0;
     }
     // > 12 years -> decades
-    if (quarterCount > 48) {
+    if (monthCount > 12 * 12) {
       shouldTickRender = (date: Date) =>
         date.getFullYear() % 10 === 0 && date.getMonth() === 0;
     }
