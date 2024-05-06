b3de5376eb7aa7d45c75d93886260354f17e56bd
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2024-05-06 14:45:33 +0200

    fix ignore unused categorical member
    Signed-off-by: Max Thonagel <12283268+thoniTUB@users.noreply.github.com>

--- backend/src/main/java/com/bakdata/conquery/models/datasets/concepts/select/connector/specific/DateUnionSelect.java
+++ backend/src/main/java/com/bakdata/conquery/models/datasets/concepts/select/connector/specific/DateUnionSelect.java
@@ -12,6 +12,7 @@ import com.bakdata.conquery.models.datasets.concepts.select.Select;
 import com.bakdata.conquery.models.query.queryplan.aggregators.Aggregator;
 import com.bakdata.conquery.models.query.queryplan.aggregators.specific.DateUnionAggregator;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,6 +21,7 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor(onConstructor_ = @JsonCreator)
 @CPSType(id = "DATE_UNION", base = Select.class)
+@JsonIgnoreProperties("categorical")
 public class DateUnionSelect extends Select implements DaterangeSelect {
 
 	@NsIdRef
