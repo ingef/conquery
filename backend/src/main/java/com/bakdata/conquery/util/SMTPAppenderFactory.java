6b8b473218449875f429c62417088a5d7e027920
awildturtok <1553491+awildturtok@users.noreply.github.com>
2024-05-06 14:17:05 +0200

    copy config

--- backend/src/main/java/com/bakdata/conquery/util/SMTPAppenderFactory.java
+++ backend/src/main/java/com/bakdata/conquery/util/SMTPAppenderFactory.java
@@ -1,28 +1,57 @@
 package com.bakdata.conquery.util;
 
+import java.net.URI;
+
 import ch.qos.logback.access.net.SMTPAppender;
 import ch.qos.logback.access.spi.IAccessEvent;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.Appender;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.google.common.base.Strings;
 import io.dropwizard.logging.AbstractAppenderFactory;
 import io.dropwizard.logging.async.AsyncAppenderFactory;
 import io.dropwizard.logging.filter.LevelFilterFactory;
 import io.dropwizard.logging.layout.LayoutFactory;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @JsonTypeName("SMTP")
+@Data
 public class SMTPAppenderFactory extends AbstractAppenderFactory<IAccessEvent> {
+
+	private String from;
+	private String[] to;
+	private URI server;
+	private String subject;
+
 
 	@Override
 	public Appender<IAccessEvent> build(LoggerContext context, String applicationName, LayoutFactory<IAccessEvent> layoutFactory, LevelFilterFactory<IAccessEvent> levelFilterFactory, AsyncAppenderFactory<IAccessEvent> asyncAppenderFactory) {
 		final SMTPAppender appender = new SMTPAppender();
-		appender.setName("cq-smtp-appender");
+		appender.setName("smtp-appender");
 
-		//TODO configure
+		appender.setFrom(getFrom());
+		appender.setSmtpHost(server.getHost());
+		appender.setSMTPPort(server.getPort());
+
+		if (!Strings.isNullOrEmpty(getServer().getUserInfo())) {
+			final String[] userInfo = getServer().getUserInfo().split(":");
+			final String userName = userInfo[0];
+			final String password = userInfo.length > 1 ? userInfo[1] : null;
+
+			appender.setPassword(password);
+			appender.setUsername(userName);
+		}
+
+		appender.setSubject(getSubject());
+
+		for (String target : getTo()) {
+			appender.addTo(target);
+		}
 
 		appender.setContext(context);
+		appender.setAsynchronousSending(true);
 
 		appender.setLayout(buildLayout(context, layoutFactory));
 		appender.addFilter(levelFilterFactory.build(threshold));
