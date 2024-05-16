4725afa7da8ad9c024382296d820a8d6a3b98280
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2024-05-16 09:35:52 +0200

    remove cache from serdes as it is to early
    Signed-off-by: Max Thonagel <12283268+thoniTUB@users.noreply.github.com>

--- backend/src/main/java/com/bakdata/conquery/io/jackson/MetaIdRefParamConverter.java
+++ backend/src/main/java/com/bakdata/conquery/io/jackson/MetaIdRefParamConverter.java
@@ -1,18 +1,12 @@
 package com.bakdata.conquery.io.jackson;
 
-import java.util.concurrent.TimeUnit;
-
 import com.bakdata.conquery.io.storage.MetaStorage;
 import com.bakdata.conquery.models.identifiable.Identifiable;
 import com.bakdata.conquery.models.identifiable.ids.Id;
 import com.bakdata.conquery.models.identifiable.ids.IdUtil;
-import com.github.benmanes.caffeine.cache.CacheLoader;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.LoadingCache;
 import jakarta.ws.rs.ext.ParamConverter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 @RequiredArgsConstructor
 public class MetaIdRefParamConverter<ID extends Id<VALUE>, VALUE extends Identifiable<ID>> implements ParamConverter<VALUE> {
@@ -21,20 +15,11 @@ public class MetaIdRefParamConverter<ID extends Id<VALUE>, VALUE extends Identif
 	@NonNull
 	private final MetaStorage storage;
 
-	LoadingCache<ID, VALUE> cache = Caffeine.newBuilder()
-											.expireAfterAccess(10, TimeUnit.MINUTES)
-											.build(new CacheLoader<>() {
-												@Override
-												public @Nullable VALUE load(ID id) {
-													return storage.get(id);
-												}
-											});
-
 	@Override
 	public VALUE fromString(String value) {
 		final ID id = idParser.parse(value);
 
-		return cache.get(id);
+		return storage.get(id);
 	}
 
 	@Override
