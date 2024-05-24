994361c63aa850eeb7034298264489596306ed84
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2024-05-24 16:33:49 +0200

    create dataset for new test
    Signed-off-by: Max Thonagel <12283268+thoniTUB@users.noreply.github.com>

--- cypress/e2e/backend-admin-ui/test_3_indexService.cy.js
+++ cypress/e2e/backend-admin-ui/test_3_indexService.cy.js
@@ -2,11 +2,25 @@
 import { visitAdminUI } from "../../integration-helpers/visitAdminUI";
 
 context("Visit Index Service Page", () => {
+    const testDSLabel = "TestIndexDataset";
+    const testDSID = "TestIndexDatasetID";
+
+
+    describe("Create a new dataset", () => {
+        it("Can create a new dataset", () => {
+          visitAdminUI("datasets");
+          cy.get('[data-test-id="entity-name"]').type(testDSLabel);
+          cy.get('[data-test-id="entity-id"]').type(testDSID);
+          cy.get('[data-test-id="create-dataset-btn"]').click().as("createDataset");
+          cy.contains(testDSID);
+        });
+      });
+
     describe("Perform Update Matching Stats", () => {
 
         it("Start Update Matching Stats", () => {
             visitAdminUI("datasets");
-            cy.get('[data-cy="datasets"]').contains('dataset1').click();
+            cy.get('[data-cy="datasets"]').contains(testDSID).click();
             cy.get('[data-cy="update-matching-stats"]').click();
         })
 
