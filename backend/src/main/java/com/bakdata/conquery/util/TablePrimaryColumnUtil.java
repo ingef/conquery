54836221b3add000d643657a5b6cd99da5a05646
Jonas Arnhold <jonasarnhold@web.de>
2024-05-29 18:22:13 +0200

    Fix typo in Table#primaryColumn

--- backend/src/main/java/com/bakdata/conquery/util/TablePrimaryColumnUtil.java
+++ backend/src/main/java/com/bakdata/conquery/util/TablePrimaryColumnUtil.java
@@ -2,15 +2,17 @@ package com.bakdata.conquery.util;
 
 import com.bakdata.conquery.models.config.DatabaseConfig;
 import com.bakdata.conquery.models.datasets.Table;
+import lombok.extern.slf4j.Slf4j;
 import org.jooq.Field;
 import org.jooq.impl.DSL;
 
+@Slf4j
 public class TablePrimaryColumnUtil {
 
-	public static Field<Object> findPrimaryColumn(Table table, DatabaseConfig sqlConfig) {
-		String primaryColumnName = table.getPrimaryColum() == null
-								   ? sqlConfig.getPrimaryColumn()
-								   : table.getPrimaryColum().getName();
+	public static Field<Object> findPrimaryColumn(Table table, DatabaseConfig databaseConfig) {
+		String primaryColumnName = table.getPrimaryColumn() == null
+								   ? databaseConfig.getPrimaryColumn()
+								   : table.getPrimaryColumn().getName();
 		return DSL.field(DSL.name(table.getName(), primaryColumnName));
 	}
 
