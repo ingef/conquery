38d39923a3efe6fe80d781c411712ee54712d509
Jonas Arnhold <jonasarnhold@web.de>
2024-05-30 13:42:01 +0200

    Remove logger

--- backend/src/main/java/com/bakdata/conquery/util/TablePrimaryColumnUtil.java
+++ backend/src/main/java/com/bakdata/conquery/util/TablePrimaryColumnUtil.java
@@ -2,11 +2,9 @@ package com.bakdata.conquery.util;
 
 import com.bakdata.conquery.models.config.DatabaseConfig;
 import com.bakdata.conquery.models.datasets.Table;
-import lombok.extern.slf4j.Slf4j;
 import org.jooq.Field;
 import org.jooq.impl.DSL;
 
-@Slf4j
 public class TablePrimaryColumnUtil {
 
 	public static Field<Object> findPrimaryColumn(Table table, DatabaseConfig databaseConfig) {
