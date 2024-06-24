4acdf8693994caeb39c6ca254ab58f3a1e84ce3f
awildturtok <1553491+awildturtok@users.noreply.github.com>
2024-06-24 11:38:04 +0200

    fix CPSBase of StartCalculateCblocks

--- backend/src/main/java/com/bakdata/conquery/models/messages/namespaces/specific/StartCalculateCblocks.java
+++ backend/src/main/java/com/bakdata/conquery/models/messages/namespaces/specific/StartCalculateCblocks.java
@@ -1,10 +1,11 @@
 package com.bakdata.conquery.models.messages.namespaces.specific;
 
 import com.bakdata.conquery.io.cps.CPSType;
+import com.bakdata.conquery.models.messages.namespaces.NamespacedMessage;
 import com.bakdata.conquery.models.messages.namespaces.WorkerMessage;
 import com.bakdata.conquery.models.worker.Worker;
 
-@CPSType(id = "CALCULATE_CBLOCKS", base = WorkerMessage.class)
+@CPSType(id = "CALCULATE_CBLOCKS", base = NamespacedMessage.class)
 public class StartCalculateCblocks extends WorkerMessage {
 	@Override
 	public void react(Worker context) throws Exception {
