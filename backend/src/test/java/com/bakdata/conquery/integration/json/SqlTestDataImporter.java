60f2a94858ef270b13548175993bcc272ea37791
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2024-07-19 10:49:04 +0200

    import data for sql test via http endpoint where possible so that resolving works

--- backend/src/test/java/com/bakdata/conquery/integration/json/SqlTestDataImporter.java
+++ backend/src/test/java/com/bakdata/conquery/integration/json/SqlTestDataImporter.java
@@ -1,21 +1,12 @@
 package com.bakdata.conquery.integration.json;
 
-import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
 
-import com.bakdata.conquery.integration.common.LoadingUtil;
 import com.bakdata.conquery.integration.common.RequiredData;
-import com.bakdata.conquery.integration.common.RequiredSecondaryId;
 import com.bakdata.conquery.integration.common.RequiredTable;
 import com.bakdata.conquery.integration.json.filter.FilterTest;
 import com.bakdata.conquery.integration.sql.CsvTableImporter;
-import com.bakdata.conquery.models.datasets.SecondaryIdDescription;
-import com.bakdata.conquery.models.datasets.Table;
-import com.bakdata.conquery.models.datasets.concepts.Concept;
-import com.bakdata.conquery.models.exceptions.JSONException;
 import com.bakdata.conquery.util.support.StandaloneSupport;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -32,6 +23,9 @@ public class SqlTestDataImporter implements TestDataImporter {
 		importTables(support, content.getTables(), true);
 		importConcepts(support, test.getRawConcepts());
 		importTableContents(support, content.getTables());
+
+		importSearchIndexes(support, test.getSearchIndexes());
+		importIdMapping(support, content);
 	}
 
 	@Override
@@ -41,39 +35,13 @@ public class SqlTestDataImporter implements TestDataImporter {
 		importTables(support, content.getTables(), true);
 		importConcepts(support, test.getRawConcepts());
 		importTableContents(support, content.getTables());
-		waitUntilDone(support, () -> LoadingUtil.importIdMapping(support, content));
-		waitUntilDone(support, () -> LoadingUtil.importPreviousQueries(support, content, support.getTestUser()));
+		importIdMapping(support, content);
+		importPreviousQueries(support, content);
 	}
 
 	@Override
 	public void importFilterTestData(StandaloneSupport support, FilterTest filterTest) throws Exception {
 		throw new UnsupportedOperationException("Not implemented yet.");
-	}
-
-	@Override
-	public void importSecondaryIds(StandaloneSupport support, List<RequiredSecondaryId> secondaryIds) {
-		for (RequiredSecondaryId required : secondaryIds) {
-			final SecondaryIdDescription description =
-					required.toSecondaryId(support.getDataset());
-			support.getAdminDatasetsProcessor().addSecondaryId(support.getNamespace(), description);
-		}
-	}
-
-	@Override
-	public void importTables(StandaloneSupport support, List<RequiredTable> tables, boolean autoConcept) throws JSONException {
-		for (RequiredTable requiredTable : tables) {
-			final Table table = requiredTable.toTable(support.getDataset(), support.getNamespaceStorage());
-			support.getNamespaceStorage().addTable(table);
-		}
-	}
-
-	@Override
-	public void importConcepts(StandaloneSupport support, ArrayNode rawConcepts) throws JSONException, IOException {
-		List<Concept<?>> concepts =
-				ConqueryTestSpec.parseSubTreeList(support, rawConcepts, Concept.class, concept -> concept.setDataset(support.getDataset().getId()));
-		for (Concept<?> concept : concepts) {
-			support.getNamespaceStorage().updateConcept(concept);
-		}
 	}
 
 	@Override
