69778bd950cabf4b419ed12c40f971d954499d84
awildturtok <1553491+awildturtok@users.noreply.github.com>
2024-07-25 15:44:06 +0200

    dont use config override in Conquery

--- backend/src/test/java/com/bakdata/conquery/integration/json/ConqueryTestSpec.java
+++ backend/src/test/java/com/bakdata/conquery/integration/json/ConqueryTestSpec.java
@@ -3,17 +3,14 @@ package com.bakdata.conquery.integration.json;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 import java.util.function.Consumer;
+
 import javax.annotation.Nullable;
 
 import com.bakdata.conquery.integration.IntegrationTest;
 import com.bakdata.conquery.io.cps.CPSBase;
 import com.bakdata.conquery.io.jackson.Jackson;
 import com.bakdata.conquery.io.jackson.View;
-import com.bakdata.conquery.io.storage.MetaStorage;
-import com.bakdata.conquery.models.config.ColumnConfig;
 import com.bakdata.conquery.models.config.ConqueryConfig;
 import com.bakdata.conquery.models.config.Dialect;
 import com.bakdata.conquery.models.config.IdColumnConfig;
@@ -56,17 +53,7 @@ public abstract class ConqueryTestSpec {
 	SqlSpec sqlSpec;
 
 	// default IdColumnConfig for SQL mode
-	private IdColumnConfig idColumns = new IdColumnConfig(
-			"entities",
-			List.of(
-					ColumnConfig.builder()
-								.name("ID")
-								.field("pid")
-								.primaryId(true)
-								.label(Map.of(Locale.ROOT, "result"))
-								.build()
-			)
-	);
+	private IdColumnConfig idColumns = null;
 
 	public ConqueryConfig overrideConfig(ConqueryConfig config) {
 
