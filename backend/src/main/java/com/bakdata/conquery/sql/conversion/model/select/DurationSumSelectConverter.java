3667d795ade6a79b0998a777b62e0e59c35bdc67
Jonas Arnhold <jonas.arnhold@bakdata.com>
2024-07-29 16:04:56 +0200

    Fix ConceptColumnSelect conversion if there is only 1 connector (#3495)

--- backend/src/main/java/com/bakdata/conquery/sql/conversion/model/select/DurationSumSelectConverter.java
+++ backend/src/main/java/com/bakdata/conquery/sql/conversion/model/select/DurationSumSelectConverter.java
@@ -1,6 +1,5 @@
 package com.bakdata.conquery.sql.conversion.model.select;
 
-import com.bakdata.conquery.models.datasets.concepts.Connector;
 import com.bakdata.conquery.models.datasets.concepts.select.connector.specific.DurationSumSelect;
 import com.bakdata.conquery.sql.conversion.cqelement.concept.ConnectorSqlTables;
 import com.bakdata.conquery.sql.conversion.model.ColumnDateRange;
@@ -8,7 +7,7 @@ import com.bakdata.conquery.sql.conversion.model.ColumnDateRange;
 public class DurationSumSelectConverter implements SelectConverter<DurationSumSelect> {
 
 	@Override
-	public ConnectorSqlSelects connectorSelect(DurationSumSelect select, SelectContext<Connector, ConnectorSqlTables> selectContext) {
+	public ConnectorSqlSelects connectorSelect(DurationSumSelect select, SelectContext<ConnectorSqlTables> selectContext) {
 		return DaterangeSelectUtil.createConnectorSqlSelects(
 				select,
 				(daterange, alias, functionProvider) -> {
--- backend/src/main/java/com/bakdata/conquery/sql/conversion/model/select/ExistsSelectConverter.java
+++ backend/src/main/java/com/bakdata/conquery/sql/conversion/model/select/ExistsSelectConverter.java
@@ -1,15 +1,13 @@
 package com.bakdata.conquery.sql.conversion.model.select;
 
-import com.bakdata.conquery.models.datasets.concepts.Connector;
 import com.bakdata.conquery.models.datasets.concepts.select.concept.specific.ExistsSelect;
-import com.bakdata.conquery.models.datasets.concepts.tree.TreeConcept;
 import com.bakdata.conquery.sql.conversion.cqelement.concept.ConceptSqlTables;
 import com.bakdata.conquery.sql.conversion.cqelement.concept.ConnectorSqlTables;
 
 public class ExistsSelectConverter implements SelectConverter<ExistsSelect> {
 
 	@Override
-	public ConnectorSqlSelects connectorSelect(ExistsSelect select, SelectContext<Connector, ConnectorSqlTables> selectContext) {
+	public ConnectorSqlSelects connectorSelect(ExistsSelect select, SelectContext<ConnectorSqlTables> selectContext) {
 		ExistsSqlSelect existsSqlSelect = createExistsSelect(select, selectContext);
 		return ConnectorSqlSelects.builder()
 								  .finalSelect(existsSqlSelect)
@@ -17,14 +15,14 @@ public class ExistsSelectConverter implements SelectConverter<ExistsSelect> {
 	}
 
 	@Override
-	public ConceptSqlSelects conceptSelect(ExistsSelect select, SelectContext<TreeConcept, ConceptSqlTables> selectContext) {
+	public ConceptSqlSelects conceptSelect(ExistsSelect select, SelectContext<ConceptSqlTables> selectContext) {
 		ExistsSqlSelect existsSqlSelect = createExistsSelect(select, selectContext);
 		return ConceptSqlSelects.builder()
 								.finalSelect(existsSqlSelect)
 								.build();
 	}
 
-	private static ExistsSqlSelect createExistsSelect(ExistsSelect select, SelectContext<?, ?> selectContext) {
+	private static ExistsSqlSelect createExistsSelect(ExistsSelect select, SelectContext<?> selectContext) {
 		String alias = selectContext.getNameGenerator().selectName(select);
 		return new ExistsSqlSelect(alias);
 	}
