a9a97d0ac11cde75bb66a36613527c5874cd1786
MT <12283268+thoniTUB@users.noreply.github.com>
2024-10-07 17:40:31 +0200

    submit ForwardToNamespace messages to the job manager of the namespace (#3580)
    * submit ForwardToNamespace messages to the job manager of the namespace
    * unify usage of NetworkSession in NetworkMessageContext
    
    ---------
    
    Co-authored-by: awildturtok <1553491+awildturtok@users.noreply.github.com>

--- backend/src/main/java/com/bakdata/conquery/models/messages/network/specific/ForwardToNamespace.java
+++ backend/src/main/java/com/bakdata/conquery/models/messages/network/specific/ForwardToNamespace.java
@@ -30,11 +30,17 @@ public class ForwardToNamespace extends MessageToManagerNode implements SlowMess
 
 		DistributedNamespace ns = Objects.requireNonNull(context.getDatasetRegistry().get(datasetId), () -> String.format("Missing dataset `%s`", datasetId));
 		ConqueryMDC.setLocation(ns.getStorage().getDataset().toString());
+
 		message.react(ns);
 
 		if (message instanceof ReactionMessage reactionMessage) {
 			ns.getWorkerHandler().handleReactionMessage(reactionMessage);
 		}
+	}
+
+	@Override
+	public String toString() {
+		return message.toString() + " for dataset " + datasetId;
 	}
 
 	@Override
@@ -45,10 +51,5 @@ public class ForwardToNamespace extends MessageToManagerNode implements SlowMess
 	@Override
 	public void setProgressReporter(ProgressReporter progressReporter) {
 		((SlowMessage) message).setProgressReporter(progressReporter);
-	}
-
-	@Override
-	public String toString() {
-		return message.toString() + " for dataset " + datasetId;
 	}
 }
