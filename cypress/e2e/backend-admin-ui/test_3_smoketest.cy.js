ea907e0052c627e1f287be65b445a9920da94043
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2024-10-14 11:08:54 +0200

    adds test to spot render error

--- cypress/e2e/backend-admin-ui/test_3_smoketest.cy.js
+++ cypress/e2e/backend-admin-ui/test_3_smoketest.cy.js
@@ -22,4 +22,50 @@ context("Simplest Smoke Tests", () => {
         cy.root().should('not.contain.text', 'FreeMarker template error')
       });
     });
+
+    describe("Dataset pages render", () => {
+      
+      it("Datasets", () => {
+        
+        visitAdminUI("datasets");
+
+        cy.root().should('not.contain.text', 'FreeMarker template error')
+
+        cy.get('[data-cy=datasets-dataset1]')
+          .find('a')
+          .contains('dataset1')
+          .click()
+
+
+        cy.get('[data-test-id="accordion-Mappings"]').click()
+        cy.get('[data-test-id="accordion-SearchIndices"]').click()
+        cy.get('[data-test-id="accordion-Tables"]').click()
+        cy.get('[data-test-id="accordion-Concepts"]').click()
+        cy.get('[data-test-id="accordion-SecondaryIds"]').click()
+        
+        cy.root().should('not.contain.text', 'FreeMarker template error')
+        
+        // Table page
+        cy.get('[data-test-id="accordion-Tables"]')
+          .find('a')
+          .contains('table')
+          .click()
+
+        cy.get('[data-test-id="accordion-Tags"]').click()
+        cy.get('[data-test-id="accordion-Concepts"]').click()
+        cy.get('[data-test-id="accordion-Columns"]').click()
+        
+        cy.root().should('not.contain.text', 'FreeMarker template error')
+
+        // Import page
+        cy.get('[data-test-id="accordion-Tags"]')
+          .find('a')
+          .contains('table')
+          .click()
+          
+          cy.root().should('not.contain.text', 'FreeMarker template error')
+
+
+      });
+    });
 })  
