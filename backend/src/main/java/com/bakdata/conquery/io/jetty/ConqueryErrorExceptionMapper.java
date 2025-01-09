4754f71caa3d83125d9d542adb5c0b82f8b324e7
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2025-01-09 11:07:16 +0100

    use plain object for exception mapping

--- backend/src/main/java/com/bakdata/conquery/io/jetty/ConqueryErrorExceptionMapper.java
+++ backend/src/main/java/com/bakdata/conquery/io/jetty/ConqueryErrorExceptionMapper.java
@@ -1,6 +1,8 @@
 package com.bakdata.conquery.io.jetty;
 
 import com.bakdata.conquery.models.error.ConqueryError;
+import com.bakdata.conquery.models.error.SimpleErrorInfo;
+
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
@@ -8,9 +10,11 @@ import jakarta.ws.rs.ext.ExceptionMapper;
 public class ConqueryErrorExceptionMapper implements ExceptionMapper<ConqueryError> {
     @Override
     public Response toResponse(ConqueryError exception) {
-        return Response.status(Response.Status.BAD_REQUEST.getStatusCode())
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(exception)
-                .build();
+		SimpleErrorInfo plain = exception.asPlain();
+
+		return Response.status(Response.Status.BAD_REQUEST.getStatusCode())
+					   .type(MediaType.APPLICATION_JSON_TYPE)
+					   .entity(plain)
+					   .build();
     }
 }
