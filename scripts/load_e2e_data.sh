8e057e9961de3063fb57de086221c2b07f869b93
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2025-01-13 16:34:56 +0100

    remove unnecessary comment

--- scripts/load_e2e_data.sh
+++ scripts/load_e2e_data.sh
@@ -37,7 +37,6 @@ echo "Creating secondary ids"
 curl --fail -X POST  "$admin_api/datasets/dataset1/secondaryId" -H "$h_ct" -H "$h_auth" -d "@./cypress/support/test_data/sid.secondaryId.json"
 sleep 1
 
- # TODO secondary ID
 echo "Creating tables"
 for table_json in `ls ./cypress/support/test_data/*.table.json`
 do
