55f5d04f8f4f8e5ef2578bb879277e9c080297d3
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2025-02-26 08:30:38 +0100

    fix npe on validity date aggregation check

--- backend/src/main/java/com/bakdata/conquery/models/query/queryplan/QueryPlan.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/queryplan/QueryPlan.java
@@ -7,7 +7,6 @@ import com.bakdata.conquery.models.query.QueryExecutionContext;
 import com.bakdata.conquery.models.query.entity.Entity;
 import com.bakdata.conquery.models.query.queryplan.aggregators.Aggregator;
 import com.bakdata.conquery.models.query.results.EntityResult;
-import org.jetbrains.annotations.NotNull;
 
 public interface QueryPlan<RESULT extends EntityResult> {
 
@@ -17,6 +16,5 @@ public interface QueryPlan<RESULT extends EntityResult> {
 
 	boolean isOfInterest(Entity entity);
 
-	@NotNull
 	Optional<Aggregator<CDateSet>> getValidityDateAggregator();
 }
