7920007a2d939d70ab7db8d34d782592b2d6d282
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2025-03-10 12:07:36 +0100

    adds busy and script endpoints to openapi-spec

--- backend/src/main/java/com/bakdata/conquery/resources/admin/rest/AdminApiResource.java
+++ backend/src/main/java/com/bakdata/conquery/resources/admin/rest/AdminApiResource.java
@@ -2,20 +2,32 @@ package com.bakdata.conquery.resources.admin.rest;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
 
 import com.bakdata.conquery.commands.ManagerNode;
+import com.bakdata.conquery.models.api.openapi.BusyApi;
 import com.bakdata.conquery.models.api.openapi.ConfigApi;
+import com.bakdata.conquery.models.api.openapi.ScriptApi;
 import com.bakdata.conquery.models.config.ConqueryConfig;
 import com.bakdata.conquery.models.config.FormBackendConfig;
 import com.bakdata.conquery.models.config.PluginConfig;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(onConstructor_ = @Inject)
-public class ConfigApiResource implements ConfigApi {
+public class AdminApiResource implements ConfigApi, BusyApi, ScriptApi {
 
+
+	private final AdminProcessor processor;
 	private final ManagerNode managerNode;
 	private final ConqueryConfig config;
+
+
+	@HeaderParam(HttpHeaders.ACCEPT)
+	private String acceptHeader;
 
 	@Override
 	public String deleteFormBackend(String configId) {
@@ -53,5 +65,19 @@ public class ConfigApiResource implements ConfigApi {
 		managerNode.getFormScanner().execute(null, null);
 
 		return pluginConfig.getId();
+	}
+
+	@Override
+	public Boolean busyGet() {
+		return processor.isBusy();
+	}
+
+	@Override
+	public Object executeScript(String script) {
+		return switch(acceptHeader) {
+			case MediaType.TEXT_PLAIN -> Objects.toString(processor.executeScript(script));
+			case MediaType.APPLICATION_JSON -> processor.executeScript(script);
+			default -> throw new IllegalArgumentException("Unexpected value: " + acceptHeader);
+		};
 	}
 }
