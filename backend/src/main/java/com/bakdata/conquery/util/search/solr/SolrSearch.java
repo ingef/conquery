3ae2f3a38e261b5c9f5be7247ef3d4369a1901f9
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2025-03-26 08:51:42 +0100

    revert checking for duplicates

--- backend/src/main/java/com/bakdata/conquery/util/search/solr/SolrSearch.java
+++ backend/src/main/java/com/bakdata/conquery/util/search/solr/SolrSearch.java
@@ -4,19 +4,17 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.bakdata.conquery.apiv1.frontend.FrontendValue;
 import com.bakdata.conquery.models.datasets.concepts.Searchable;
 import com.bakdata.conquery.util.search.Search;
 import com.bakdata.conquery.util.search.solr.entities.SolrFrontendValue;
+import com.google.common.base.Stopwatch;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.common.SolrDocumentList;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -71,17 +69,10 @@ public class SolrSearch extends Search<FrontendValue> {
 	public void registerValues(Collection<String> values) {
 		try {
 
-			// Use searchable's id to for collection.
-			List<SolrFrontendValue> solrFrontendValues = values.stream().map(value -> new SolrFrontendValue(searchable, value, value, null)).toList();
-			List<String> ids = solrFrontendValues.stream().map(SolrFrontendValue::getId).toList();
-
-			SolrDocumentList existingDocs = solrClient.getById(ids);
-			Set<String> existingIds = existingDocs.stream().map(doc -> doc.getFieldValue(SolrFrontendValue.Fields.id)).map(String.class::cast).collect(Collectors.toSet());
-
-
-			log.info("Received {} values, solr knows {} of them. Registering {} new documents", solrFrontendValues.size(), existingIds.size(), solrFrontendValues.size() - existingIds.size());
-
-			solrClient.addBeans(solrFrontendValues.stream().filter(val -> !existingIds.contains(val)).iterator());
+			Stopwatch stopwatch = Stopwatch.createStarted();
+			log.info("BEGIN registering {} values to {} for {} {}", values.size(), solrClient.getDefaultCollection(), searchable.getClass().getSimpleName(), searchable.getId());
+			solrClient.addBeans(values);
+			log.info("FINISHED registering {} values to {} for {} {} in {}", values.size(), solrClient.getDefaultCollection(), searchable.getClass().getSimpleName(), searchable.getId(), stopwatch);
 		}
 		catch (SolrServerException | IOException e) {
 			throw new IllegalStateException("Unable to register values for searchable '%s'".formatted(searchable), e);
