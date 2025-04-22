054c761bf85d3eddea4767ca184099056665c2c6
awildturtok <1553491+awildturtok@users.noreply.github.com>
2025-04-22 12:18:23 +0200

    apply reversion of infinity signs to tests

--- backend/src/test/java/com/bakdata/conquery/execution/DefaultSqlCDateSetParserTest.java
+++ backend/src/test/java/com/bakdata/conquery/execution/DefaultSqlCDateSetParserTest.java
@@ -32,9 +32,9 @@ class DefaultSqlCDateSetParserTest {
 	public static Stream<Arguments> testToEpochDayRangeListProvider() {
 		return Stream.of(
 				Arguments.of("{}", "{}", "Empty datemultirange"),
-				Arguments.of("{[-∞,∞]}", "{-inf/+inf}", "Infinity datemultirange"),
+				Arguments.of("{[-∞,∞]}", "{-∞/+∞}", "Infinity datemultirange"),
 				Arguments.of("{[2012-01-01,2013-01-01)}", "{2012-01-01/2012-12-31}", "datemultirange with 1 daterange"),
-				Arguments.of("{[-∞,2013-01-01),[2015-01-01,∞]}", "{-inf/2012-12-31,2015-01-01/+inf}", "datemultirange with multiple ranges and infinity start and end value"),
+				Arguments.of("{[-∞,2013-01-01),[2015-01-01,∞]}", "{-∞/2012-12-31,2015-01-01/+∞}", "datemultirange with multiple ranges and infinity start and end value"),
 				Arguments.of("{[2014-01-01,2015-01-01),[2015-06-01,2016-01-01),[2017-01-01,2018-01-01)}", "{2014-01-01/2014-12-31,2015-06-01/2015-12-31,2017-01-01/2017-12-31}", "datemultirange with multiple ranges")
 		);
 	}
