22e915aa8faad6bcf91cd9f1ca01ba754d7c340c
awildturtok <1553491+awildturtok@users.noreply.github.com>
2025-06-12 13:35:47 +0200

    Substring Filters and Aggregators (#3748)
    Implements substring logic for SelectFilter(s) and MappableSingleColumnSelect derivates
    also merges SingleSelectFilter logic to use MultiSelectFilterNode under the hood with Singleton-Set, as that is probably exactly as fast in our applications.

--- backend/src/main/java/com/bakdata/conquery/models/query/filter/event/SubstringMultiSelectFilterNode.java
+++ backend/src/main/java/com/bakdata/conquery/models/query/filter/event/SubstringMultiSelectFilterNode.java
@@ -3,10 +3,12 @@ package com.bakdata.conquery.models.query.filter.event;
 import java.util.Set;
 import jakarta.validation.constraints.NotNull;
 
+import com.bakdata.conquery.models.common.Range;
 import com.bakdata.conquery.models.datasets.Column;
 import com.bakdata.conquery.models.datasets.Table;
 import com.bakdata.conquery.models.events.Bucket;
 import com.bakdata.conquery.models.query.QueryExecutionContext;
+import com.bakdata.conquery.models.query.StringUtils;
 import com.bakdata.conquery.models.query.entity.Entity;
 import com.bakdata.conquery.models.query.queryplan.filter.EventFilterNode;
 import lombok.Getter;
@@ -19,19 +21,21 @@ import org.apache.logging.log4j.util.Strings;
  */
 
 @ToString(callSuper = true, of = "column")
-public class MultiSelectFilterNode extends EventFilterNode<Set<String>> {
+public class SubstringMultiSelectFilterNode extends EventFilterNode<Set<String>> {
 
+	private final Range.IntegerRange range;
 	@NotNull
 	@Getter
 	@Setter
 	private Column column;
-
 	private final boolean empty;
-	
 
-	public MultiSelectFilterNode(Column column, Set<String> filterValue) {
+
+	public SubstringMultiSelectFilterNode(Column column, Set<String> filterValue, Range.IntegerRange range) {
 		super(filterValue);
 		this.column = column;
+		this.range = range;
+
 		empty = filterValue.stream().anyMatch(Strings::isEmpty);
 	}
 
@@ -51,18 +55,16 @@ public class MultiSelectFilterNode extends EventFilterNode<Set<String>> {
 			return empty;
 		}
 
-		final String stringToken = bucket.getString(event, getColumn());
+		final String string = bucket.getString(event, getColumn());
 
-		return getFilterValue().contains(stringToken);
+		final String substr = StringUtils.getSubstringFromRange(string, range);
+
+		return getFilterValue().contains(substr);
 	}
 
-	@Override
-	public boolean isOfInterest(Bucket bucket) {
-		return true;
-	}
 
 	@Override
 	public void collectRequiredTables(Set<Table> requiredTables) {
 		requiredTables.add(column.getTable());
 	}
-	}
+}
