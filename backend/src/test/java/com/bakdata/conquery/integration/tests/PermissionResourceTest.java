77d29c90acb6efc586b14cb54f5fac19d293f5bc
MT <12283268+thoniTUB@users.noreply.github.com>
2026-04-20 12:57:00 +0200

    add test case

--- backend/src/test/java/com/bakdata/conquery/integration/tests/PermissionResourceTest.java
+++ backend/src/test/java/com/bakdata/conquery/integration/tests/PermissionResourceTest.java
@@ -37,6 +37,7 @@ public class PermissionResourceTest {
 				Arguments.of("domain", 204),
 				Arguments.of("domain:operation", 204),
 				Arguments.of("domain:operation:instance", 204),
+				Arguments.of("domain:operation1,:instance", 204),
 				Arguments.of("domain:operation1,operation2:instance", 204),
 				Arguments.of("", 422),
 				Arguments.of("domain:operation:instance:too_much", 422),
