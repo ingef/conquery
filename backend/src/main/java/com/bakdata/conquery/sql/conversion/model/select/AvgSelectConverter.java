83376ae08004d622b037fb51f93740c4354f0881
Georg Schwarzkopf <georg.schwarzkopf@bakdata.com>
2026-06-15 11:11:38 +0200

    feat: implement convertToSqlFilter

--- backend/src/main/java/com/bakdata/conquery/sql/conversion/model/select/AvgSelectConverter.java
+++ backend/src/main/java/com/bakdata/conquery/sql/conversion/model/select/AvgSelectConverter.java
@@ -1,15 +1,18 @@
 package com.bakdata.conquery.sql.conversion.model.select;
 
+import com.bakdata.conquery.models.common.IRange;
 import com.bakdata.conquery.models.datasets.Column;
 import com.bakdata.conquery.models.datasets.concepts.filters.specific.AvgFilter;
 import com.bakdata.conquery.models.datasets.concepts.select.connector.specific.AvgSelect;
 import com.bakdata.conquery.sql.conversion.cqelement.concept.ConceptCteStep;
 import com.bakdata.conquery.sql.conversion.cqelement.concept.ConnectorSqlTables;
+import com.bakdata.conquery.sql.conversion.cqelement.concept.FilterContext;
 import com.bakdata.conquery.sql.conversion.model.NumberMapUtil;
-import com.bakdata.conquery.sql.conversion.model.aggregator.SqlAggregator;
-import com.bakdata.conquery.sql.conversion.model.filter.FilterConverter;
-import com.bakdata.conquery.sql.conversion.model.filter.RangeCondition;
+import com.bakdata.conquery.sql.conversion.model.filter.*;
+
 import java.math.BigDecimal;
+
+import org.jooq.Condition;
 import org.jooq.Field;
 import org.jooq.impl.DSL;
 
@@ -31,7 +34,11 @@ import org.jooq.impl.DSL;
  * converting filters or can use one generic class for this purpose. In the future, I'd like to reduce complexity in
  * this repository, and getting rid of superfluous classes would be a first step.
  */
-public class AvgSelectConverter implements SelectConverter<AvgSelect> {
+public class AvgSelectConverter<RANGE extends IRange<? extends Number, ?>> implements
+// Move to a different package?
+//        SqlAggregator,
+        SelectConverter<AvgSelect>,
+        FilterConverter<AvgFilter<RANGE>, RANGE> {
 
     @Override
     public ConnectorSqlSelects connectorSelect(final AvgSelect select, final SelectContext<ConnectorSqlTables> selectContext) {
@@ -44,7 +51,6 @@ public class AvgSelectConverter implements SelectConverter<AvgSelect> {
 
         ExtractingSqlSelect<? extends Number> rootSelect = new ExtractingSqlSelect<>(tables.getRootTable(), avgColumn.getName(), numberClass);
 
-//      value: concept_concept_connector-0-event_filter
         String eventFilterCte = tables.cteName(ConceptCteStep.EVENT_FILTER);
 
         Field<? extends Number> avgField = rootSelect
@@ -56,11 +62,62 @@ public class AvgSelectConverter implements SelectConverter<AvgSelect> {
         String finalPredecessor = tables.getPredecessor(ConceptCteStep.AGGREGATION_FILTER);
         ExtractingSqlSelect<BigDecimal> finalSelect = avgGroupBy.qualify(finalPredecessor);
 
-        return ConnectorSqlSelects.builder()
+        return  ConnectorSqlSelects.builder()
                 .preprocessingSelect(rootSelect)
                 .aggregationSelect(avgGroupBy)
                 .finalSelect(finalSelect)
                 .build();
+    }
+
+// TODO: Use CommonAggregationSelect?
+    @Override
+    public SqlFilters convertToSqlFilter(AvgFilter<RANGE> filter, FilterContext<RANGE> filterContext) {
+
+        Column column = filter.getColumn().resolve();
+        String alias = filterContext.getNameGenerator().selectName(filter); // always selectname?
+        ConnectorSqlTables tables = filterContext.getTables();
+
+        Class<? extends Number> numberClass = NumberMapUtil.getType(column);
+
+        ExtractingSqlSelect<? extends Number> rootSelect = new ExtractingSqlSelect<>(tables.getRootTable(), column.getName(), numberClass);
+
+
+        String eventFilterCte = tables.cteName(ConceptCteStep.EVENT_FILTER);
+        Field<? extends Number> avgField = rootSelect.qualify(eventFilterCte).select();
+
+        // TODO: Use the correct type
+        FieldWrapper<?> avgGroupBy = new FieldWrapper<>(DSL.avg(avgField).as(alias), column.getName());
+
+
+       ConnectorSqlSelects selects = ConnectorSqlSelects.builder()
+               .preprocessingSelect(rootSelect)
+               .aggregationSelect(avgGroupBy)
+               .build();
+
+
+
+        String aggSelectPredecessor = tables.getPredecessor(ConceptCteStep.AGGREGATION_FILTER);
+        // TODO: Use the correct type
+        Field<?> qualifiedAvgSelect = avgGroupBy.qualify(aggSelectPredecessor).select();
+
+        // TODO: Replace CountCondition
+        CountCondition condition = new CountCondition(qualifiedAvgSelect, filterContext.getValue());
+
+        WhereClauses whereClauses = WhereClauses.builder()
+                .groupFilter(condition)
+                .build();
+
+
+
+        return new SqlFilters(
+                selects,
+                whereClauses
+        );
+    }
+
+    @Override
+    public Condition convertForTableExport(AvgFilter<RANGE> filter, FilterContext<RANGE> filterContext) {
+        throw new UnsupportedOperationException("Not implemented");
     }
 
 }
