b297f821ba7071c28de35d9c66b1f3956711dce7
Georg Schwarzkopf <georg.schwarzkopf@bakdata.com>
2026-06-17 14:40:21 +0200

    refactor: use CommonAggregator in AvgSqlAggregator

--- backend/src/main/java/com/bakdata/conquery/sql/conversion/model/aggregator/AvgSqlAggregator.java
+++ backend/src/main/java/com/bakdata/conquery/sql/conversion/model/aggregator/AvgSqlAggregator.java
@@ -17,24 +17,6 @@ import org.jooq.Condition;
 import org.jooq.Field;
 import org.jooq.impl.DSL;
 
-/**
- * Next steps:
- * - See the new test case in src/test/resources/tests/aggregator/AVG_AGGREGATOR/filter/AVG_FILTER.test.json. Implement
- * a conversion for the {@link AvgFilter}. Therefore, turn this {@link SelectConverter} into a {@link SqlAggregator}
- * and implement {@link FilterConverter}.
- * <p>
- * Hints:
- * - The filter is applied onto the aggregated avg value. Have a look at the select converter test case you already
- * implemented, and at the filter value of the new test case. Every row, whose avg value is not in the applied
- * filter range will be filtered.
- * - Implementation is similar to COUNT/SUM filter conversion.
- * - You do not need to implement FilterConverter::convertForTableExport(). Throw an {@link UnsupportedOperationException}.
- * <p>
- * Bonus:
- * - After finishing the implementation, check if we actually need individual {@link RangeCondition} classes when
- * converting filters or can use one generic class for this purpose. In the future, I'd like to reduce complexity in
- * this repository, and getting rid of superfluous classes would be a first step.
- */
 public class AvgSqlAggregator<RANGE extends IRange<? extends Number, ?>> implements
         SqlAggregator,
         SelectConverter<AvgSelect>,
@@ -45,68 +27,41 @@ public class AvgSqlAggregator<RANGE extends IRange<? extends Number, ?>> impleme
 
         String alias = selectContext.getNameGenerator().selectName(select);
         ConnectorSqlTables tables = selectContext.getTables();
-        Column avgColumn = select.getColumn().resolve();
+        Column column = select.getColumn().resolve();
 
-        Class<? extends Number> numberClass = NumberMapUtil.getType(avgColumn);
-
-        ExtractingSqlSelect<? extends Number> rootSelect = new ExtractingSqlSelect<>(tables.getRootTable(), avgColumn.getName(), numberClass);
-
-        String eventFilterCte = tables.cteName(ConceptCteStep.EVENT_FILTER);
-
-        Field<? extends Number> avgField = rootSelect
-                .qualify(eventFilterCte) // change column from root table to event_filter
-                .select();
-
-        FieldWrapper<BigDecimal> avgGroupBy = new FieldWrapper<>(DSL.avg(avgField).as(alias), avgColumn.getName());
+        CommonAggregationSelect<BigDecimal> commonAggSelect = createCommonAggregationselect(column, alias, tables);
 
         String finalPredecessor = tables.getPredecessor(ConceptCteStep.AGGREGATION_FILTER);
-        ExtractingSqlSelect<BigDecimal> finalSelect = avgGroupBy.qualify(finalPredecessor);
+        ExtractingSqlSelect<BigDecimal> finalSelect = commonAggSelect.getGroupBy().qualify(finalPredecessor);
 
-        return  ConnectorSqlSelects.builder()
-                .preprocessingSelect(rootSelect)
-                .aggregationSelect(avgGroupBy)
+        return ConnectorSqlSelects.builder()
+                .preprocessingSelects(commonAggSelect.getRootSelects())
+                .aggregationSelect(commonAggSelect.getGroupBy())
                 .finalSelect(finalSelect)
                 .build();
     }
 
-// TODO: Use CommonAggregationSelect?
     @Override
     public SqlFilters convertToSqlFilter(AvgFilter<RANGE> filter, FilterContext<RANGE> filterContext) {
 
         Column column = filter.getColumn().resolve();
-        String alias = filterContext.getNameGenerator().selectName(filter); // always selectname?
+        String alias = filterContext.getNameGenerator().selectName(filter);
         ConnectorSqlTables tables = filterContext.getTables();
 
-        Class<? extends Number> numberClass = NumberMapUtil.getType(column);
+        CommonAggregationSelect<BigDecimal> commonAggSelect = createCommonAggregationselect(column, alias, tables);
 
-        ExtractingSqlSelect<? extends Number> rootSelect = new ExtractingSqlSelect<>(tables.getRootTable(), column.getName(), numberClass);
-
-
-        String eventFilterCte = tables.cteName(ConceptCteStep.EVENT_FILTER);
-        Field<? extends Number> avgField = rootSelect.qualify(eventFilterCte).select();
-
-        // TODO: Use the correct type
-        FieldWrapper<?> avgGroupBy = new FieldWrapper<>(DSL.avg(avgField).as(alias), column.getName());
-
-
-       ConnectorSqlSelects selects = ConnectorSqlSelects.builder()
-               .preprocessingSelect(rootSelect)
-               .aggregationSelect(avgGroupBy)
-               .build();
-
-
+        ConnectorSqlSelects selects = ConnectorSqlSelects.builder()
+                .preprocessingSelects(commonAggSelect.getRootSelects())
+                .aggregationSelect(commonAggSelect.getGroupBy())
+                .build();
 
         String aggSelectPredecessor = tables.getPredecessor(ConceptCteStep.AGGREGATION_FILTER);
-        // TODO: Use the correct type
-        Field<?> qualifiedAvgSelect = avgGroupBy.qualify(aggSelectPredecessor).select();
-
+        Field<BigDecimal> qualifiedAvgSelect = commonAggSelect.getGroupBy().qualify(aggSelectPredecessor).select();
         RangeCondition condition = new RangeCondition(qualifiedAvgSelect, filterContext.getValue());
 
         WhereClauses whereClauses = WhereClauses.builder()
                 .groupFilter(condition)
                 .build();
-
-
 
         return new SqlFilters(
                 selects,
@@ -117,6 +72,25 @@ public class AvgSqlAggregator<RANGE extends IRange<? extends Number, ?>> impleme
     @Override
     public Condition convertForTableExport(AvgFilter<RANGE> filter, FilterContext<RANGE> filterContext) {
         throw new UnsupportedOperationException("Not implemented");
+    }
+
+    private CommonAggregationSelect<BigDecimal> createCommonAggregationselect(
+            Column column,
+			String alias,
+			ConnectorSqlTables tables) {
+
+        Class<? extends Number> numberClass = NumberMapUtil.getType(column);
+
+        ExtractingSqlSelect<? extends Number> rootSelect = new ExtractingSqlSelect<>(tables.getRootTable(), column.getName(), numberClass);
+
+        String eventFilterCte = tables.cteName(ConceptCteStep.EVENT_FILTER);
+        Field<? extends Number> avgField = rootSelect.qualify(eventFilterCte).select();
+        FieldWrapper<BigDecimal> avgGroupBy = new FieldWrapper<>(DSL.avg(avgField).as(alias), column.getName());
+
+        return CommonAggregationSelect.<BigDecimal>builder()
+                .rootSelect(rootSelect)
+                .groupBy(avgGroupBy)
+                .build();
     }
 
 }
