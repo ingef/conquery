5a677a50b5f8c18d5d6c167c0622c32e4ede187f
Fabian Kovacs <Fabian.Kovacs@Ingef.de>
2026-07-23 15:46:13 +0200

    Fixes a bug where empty-Value selection alone causes faulty SQL generation.

--- backend/src/main/java/com/bakdata/conquery/sql/conversion/model/filter/MultiSelectCondition.java
+++ backend/src/main/java/com/bakdata/conquery/sql/conversion/model/filter/MultiSelectCondition.java
@@ -1,13 +1,14 @@
 package com.bakdata.conquery.sql.conversion.model.filter;
 
-import java.util.Arrays;
-
 import com.bakdata.conquery.sql.conversion.dialect.SqlFunctionProvider;
 import com.google.common.base.Strings;
 import lombok.RequiredArgsConstructor;
 import org.jooq.Condition;
 import org.jooq.Field;
-import org.jooq.impl.DSL;
+
+import java.util.Arrays;
+
+import static org.jooq.impl.DSL.*;
 
 @RequiredArgsConstructor
 public class MultiSelectCondition implements WhereCondition {
@@ -21,7 +22,7 @@ public class MultiSelectCondition implements WhereCondition {
 		// we want all entries that don't satisfy a condition - because in SQL a comparison with NULL equals UNKNOWN and not FALSE,
 		// we need to check if the entry is NULL or does not fulfil the condition
 		Condition valueIsNull = column.isNull();
-		Condition notOrNull = DSL.not(condition()).or(valueIsNull);
+		Condition notOrNull = not(condition()).or(valueIsNull);
 		return ConditionUtil.wrap(notOrNull);
 	}
 
@@ -30,14 +31,29 @@ public class MultiSelectCondition implements WhereCondition {
 
 		// values can contain empty or null Strings
 		String[] valuesWithoutNull = Arrays.stream(values)
-										   .filter(value -> !Strings.isNullOrEmpty(value))
-										   .toArray(String[]::new);
-		Condition inCondition = this.functionProvider.in(column, valuesWithoutNull);
+				.filter(value -> !Strings.isNullOrEmpty(value))
+				.toArray(String[]::new);
 
-		if (valuesWithoutNull.length < values.length) {
-			return inCondition.or(DSL.field(column).isNull());
+
+		Condition inCondition;
+
+		if (valuesWithoutNull.length == 0) {
+			inCondition = noCondition();
+		} else if (valuesWithoutNull.length == 1) {
+			inCondition = field(column).eq(val(valuesWithoutNull[0]));
+		} else {
+			inCondition = functionProvider.in(column, valuesWithoutNull);
 		}
-		return inCondition;
+
+		Condition emptyCondition;
+
+		if (Arrays.stream(values).anyMatch(Strings::isNullOrEmpty)) {
+			emptyCondition = field(column).isNull();
+		} else {
+			emptyCondition = noCondition();
+		}
+
+		return inCondition.or(emptyCondition);
 	}
 
 
