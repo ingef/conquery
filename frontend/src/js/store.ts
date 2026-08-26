1537ef6255ca7efd070b280d77de8a37905bc26a
Kai Rollmann <kai.rollmann@gmail.com>
2026-08-26 15:07:49 +0200

    chore(frontend): upgrade redux 5, react-redux 9, redux-devtools 4
    No RTK here, so createStore stays via the legacy_createStore alias.

--- frontend/src/js/store.ts
+++ frontend/src/js/store.ts
@@ -1,5 +1,5 @@
 import { composeWithDevTools } from "@redux-devtools/extension";
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
 
 import buildAppReducer from "./app/reducers";
 
